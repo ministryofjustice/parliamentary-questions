02775ef74cbcd3e96d762631479e592fdaa88052
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-06-19 16:31:42 +0100

    trim link tests started

--- spec/models/trim_link_spec.rb
+++ spec/models/trim_link_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe TrimLink do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:trim) {build(:trim_link)}
+
+  	it 'should pass factory build' do
+  		expect(trim).to be_valid
+  	end
+
+  	it 'should have binary data' 
+
+  	it 'should be linked to a pq'
 end
