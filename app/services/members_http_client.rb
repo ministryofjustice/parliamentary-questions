9d8e22cba13302bf4a0e4ad7793d4cd43a472714
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-06-20 12:38:04 +0100

    basic selection of the ministers members_id from the parliamentary API

--- app/services/members_http_client.rb
+++ app/services/members_http_client.rb
@@ -5,13 +5,34 @@ class MembersHttpClient
     @client = HTTPClient.new
   end
 
-  def members(name)
+  def members_xml(name)
     param = URI.escape("name*#{name}")
+
+    # Example
+    # http://data.parliament.uk/membersdataplatform/services/mnis/members/query/name*Diane%20Abbott
     endpoint = URI::join(@base_url, "/membersdataplatform/services/mnis/members/query/#{param}/")
-    puts endpoint
+
     response = @client.get(endpoint)
     response.content
   end
-  #http://data.parliament.uk/membersdataplatform/services/mnis/members/query/name*Diane%20Abbott
+
+  def members(name)
+    parse_members_xml(members_xml(name))
+  end
+
+
+  def parse_members_xml(response)
+    xml  = Nokogiri::XML(response)
+    xml.remove_namespaces! # easy to parse if we are only using one namespace
+    member_xml = xml.xpath('//Member')
+
+    members = Array.new
+
+    member_xml.each do |q|
+      item = Hash.from_xml(q.to_xml)
+      members.push(item["Member"])
+    end
+    members
+  end
 
 end
