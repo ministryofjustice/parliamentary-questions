3418a4f09db917461ac2333a0b835272bc5b2322
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2014-06-24 09:39:21 +0100

    Added more Question Status Progresses.

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -20,6 +20,31 @@ class Progress < ActiveRecord::Base
     find_by_status(self.UNALLOCATED)
   end
 
+  def self.Rejected
+    find_by_status(self.REJECTED)
+  end
+  def self.Transfer
+    find_by_status(self.TRANSFER)
+  end
+  def self.pod_waiting
+    find_by_status(self.POD_WAITING)
+  end
+  def self.pod_query
+    find_by_status(self.POD_QUERY)
+  end
+  def self.pod_cleared
+    find_by_status(self.POD_CLEARED)
+  end
+  def self.minister_waiting
+    find_by_status(self.MINISTER_WAITING)
+  end
+  def self.minister_query
+    find_by_status(self.MINISTER_QUERY)
+  end
+  def self.minister_cleared
+    find_by_status(self.MINISTER_CLEARED)
+  end
+
 
   # status constants
 
@@ -34,5 +59,39 @@ class Progress < ActiveRecord::Base
   def self.UNALLOCATED
     'Unallocated'
   end
+
+  def self.REJECTED
+    'Rejected'
+  end
+
+  def self.TRANSFER
+    'Transfer'
+  end
+
+  def self.POD_WAITING
+    'Pod Waiting'
+  end
+
+  def self.POD_QUERY
+    'Pod Query'
+  end
+
+  def self.POD_CLEARED
+    'Pod Cleared'
+  end
+
+  def self.MINISTER_WAITING
+    'Minister Waiting'
+  end
+
+  def self.MINISTER_QUERY
+    'Minister Query'
+  end
+
+  def self.MINISTER_CLEARED
+    'Minister Cleared'
+  end
+
+
 
 end
