ff1c50d90f37f3f2d9a4f0dafdb415ed1adaa01a
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-07-03 16:28:59 +0100

    reminder email for action officers

---
+++ app/mailers/action_officer_reminder_mailer.rb
@@ -0,0 +1,15 @@
+class ActionOfficerReminderMailer < ActionMailer::Base
+  default from: Settings.mail_from
+  def remind_accept_reject_email(template_params)
+
+    @template_params = template_params
+
+    #template[:name] = ao.name
+    #template[:email] = ao.email
+    #template[:uin] = pq.uin
+    #template[:question] = pq.question
+
+    mail(to: @template_params[:email], subject: "[Parliamentary-Questions] URGENT: #{@template_params[:uin]} | You have to Accept or Reject the question")
+  end
+
+end
