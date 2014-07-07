ac7d47af465394e1302a8d5c71f939354a1b1e5c
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2014-07-07 15:24:56 +0100

    Added basic question fields to Transferred PQ

--- app/controllers/transferred_controller.rb
+++ app/controllers/transferred_controller.rb
@@ -9,17 +9,24 @@ class TransferredController < ApplicationController
     @pq = PQ.new(pq_params)
     @pq.transferred = true
     @pq.raising_member_id = '0'
-    @pq.question = '<< This question is created as a transferred, the text of the question it should appear once the transfer question is received >>'
-    @pq.save
+    @pq.progress_id = Progress.UNALLOCATED
 
-    flash[notice] = 'Transferred PQ was successfully created.'
-    redirect_to controller: 'dashboard', action: 'index'
+
+    if @pq.save
+      flash[notice] = 'Transferred PQ was successfully created.'
+      redirect_to controller: 'dashboard', action: 'index'
+    else
+      flash[notice] = 'There was an error creating the transfer PQ.'
+      render :new
+
+
+    end
   end
 
   private
 
   def pq_params
-    params.require(:pq).permit(:uin)
+    params.require(:pq).permit(:uin, :question, :house_name, :member_name, :member_constituency, :date_for_answer, :registered_interest, :type_of_question )
   end
 
 
