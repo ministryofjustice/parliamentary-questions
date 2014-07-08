46b07bfc22c9dd71d32ca74cf18e0dd62ebe4a11
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-07-08 15:16:07 +0100

    more reliable reject, avoiding to be Progress.accepted if you change your mind from accept to reject

--- app/models/action_officers_pq.rb
+++ app/models/action_officers_pq.rb
@@ -2,8 +2,12 @@ class ActionOfficersPq < ActiveRecord::Base
   belongs_to :pq
 	belongs_to :action_officer
 
-
   def self.rejected()
     where('reject = ?', true)
   end
+
+  def self.accepted()
+    where('accept = ?', true)
+  end
+
 end
