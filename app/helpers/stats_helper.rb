e3861d613e3b63974895f594ee199008aa4bd3c4
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-07-10 10:30:04 +0100

    Added StatsHelper to centrally manage metrics naming

---
+++ app/helpers/stats_helper.rb
@@ -0,0 +1,11 @@
+module StatsHelper
+
+  METRIC_DASHBOARD_HITS = "dashboard.hits"
+  METRIC_MAIL_SENT = "mails.sent"
+  METRIC_MAIL_FAIL = "mails.failed"
+
+  MAIL_TIMING   = "mails.timing"
+  MAIL_SUCCESS  = "mails.success"
+  MAIL_FAILURE  = "mails.failed"
+
+end
