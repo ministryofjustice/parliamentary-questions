f0fc9c57264850fda6825cc761ff9a3e924674c6
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-07-10 10:32:18 +0100

    Removed unused metric constants

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,8 +1,9 @@
+# This module contains constants for use in metrics generation to allow the naming 
+# convention to be managed centrally
+
 module StatsHelper
 
   METRIC_DASHBOARD_HITS = "dashboard.hits"
-  METRIC_MAIL_SENT = "mails.sent"
-  METRIC_MAIL_FAIL = "mails.failed"
 
   MAIL_TIMING   = "mails.timing"
   MAIL_SUCCESS  = "mails.success"
