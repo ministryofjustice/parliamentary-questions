5d92d3f13359a64dd6bc71bf2e2886193d7bc48c
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-07-10 10:31:16 +0100

    Added project specific BaseMailer to encapsulate logging/metrics

---
+++ app/mailers/pq_base_mailer.rb
@@ -0,0 +1,30 @@
+
+class PqBaseMailer < ActionMailer::Base
+
+  def mail(opts, &block)
+    m = super
+
+    class << m
+      attr_accessor :logger
+
+      # Re-open the message object and wrap the deliver method since that is when the actual error is thrown.
+      def deliver( )
+        $statsd.time(StatsHelper::MAIL_TIMING) do 
+          begin
+            super
+            logger.warn "Sent mail with subject '#{self[:subject]}'"
+            $statsd.increment StatsHelper::MAIL_SUCCESS
+          rescue => e
+            logger.warn "Problem sending mail with subject '#{self[:subject]}': #{e.inspect}"
+            $statsd.increment StatsHelper::MAIL_FAILURE
+            raise e
+          end
+        end
+      end
+    end
+
+    m.logger = logger
+    m
+  end
+end
+
