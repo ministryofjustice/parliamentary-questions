c633aea4d35620e4b5def4f5e7a40270d93b7274
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-07-14 11:29:02 +0100

    silly error :S

--- spec/services/pq_progress_changer_service_spec.rb
+++ spec/services/pq_progress_changer_service_spec.rb
@@ -7,25 +7,22 @@ describe 'PQProgressChangerService' do
   let(:pq_2) { create(:PQ, uin: 'HL710', question: 'test question?') }
   progress_seed
 
+
+
+#  Initial state 'Draft Pending' (done by the import process)
 #
-#  Show Finance Check - If no finance view
-#  Show PQ Commission - If finance have viewed
-#  Show PQ draft - If AO has accepted
-#  Show POD Check - If date set to pod complete
-#  Show Minister Check -  If date cleared by POD complete
-#  Show Answer - if date cleared by minister is complete
+#  POD Waiting - if date 'draft_answer_received'is completed  && 'Draft Pending'
 #
-#  Show in Draft Pending - if - Allocated accepted  lower than at the beginning of the day.
-#  Show in POD Waiting - If - ‘Date returned to action officer’ is completed - & remove from Draft Pending
-#  Show in POD Query - if - POD query is selected - & remove from POD Waiting
-#  Show in POD cleared - if - POD cleared’ is completed - & remove from POD Query
+#  POD Query - if 'pod_query_flag == true' && 'POD Waiting'
+#
+#  POD Cleared - if - POD cleared’ is completed - & remove from POD Query
 #  Show in Minister Waiting - If - 'Date sent to minister’ is completed - & remove from POD cleared (Both ministers if there are two)
 #  Show in Minister Query - If - ‘Minister query’ is selected - & remove from Minister Waiting - (both ministers need to be completed if there are two)
 #  Show in Minister Cleared - If - ‘Date cleared by minister’ is completed - & remove from Minister Query (Both ministers need to be cleared if there are two)
 #
 #  *Remove from dashboard - if - 'Date answer submitted to Q&A tool’ is completed - or- ‘Date PQ withdrawn’ is selected*
 
-                                                                                                                                                                   Show in I Will Write - If - 'i will write’ is selected. - & maintain all other states.
+# Show in I Will Write - If - 'i will write’ is selected. - & maintain all other states.
 
 
 
