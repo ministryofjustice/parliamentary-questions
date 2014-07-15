61f307c72accf00b60cb244cc5da3b29386091f9
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-07-15 10:55:26 +0100

    removed death code

--- spec/services/pq_progress_changer_service_spec.rb
+++ spec/services/pq_progress_changer_service_spec.rb
@@ -15,7 +15,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from DRAFT_PENDING to POD_WAITING if draft_answer_received is completed' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.draft_pending.id)
-      pq_before = pq.dup
+      
       pq.update(draft_answer_received: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -27,7 +27,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question to POD_WAITING if the progress is not DRAFT_PENDING' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.allocated_accepted.id)
-      pq_before = pq.dup
+      
       pq.update(draft_answer_received: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -42,7 +42,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from POD_WAITING to POD_QUERY if pod_query_flag is true' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_waiting.id)
-      pq_before = pq.dup
+      
       pq.update(pod_query_flag: true)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -54,7 +54,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question to POD_QUERY if the progress is not POD_WAITING' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.draft_pending.id)
-      pq_before = pq.dup
+      
       pq.update(pod_query_flag: true)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -69,7 +69,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from POD_WAITING to POD_CLEARED if pod_clearance completed' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_waiting.id)
-      pq_before = pq.dup
+      
       pq.update(pod_clearance: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -81,7 +81,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from POD_QUERY to POD_CLEARED if pod_clearance completed' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_query.id)
-      pq_before = pq.dup
+      
       pq.update(pod_clearance: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -94,7 +94,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question to POD_CLEARED if the progress is not POD_QUERY or POD_WAITING' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.draft_pending.id)
-      pq_before = pq.dup
+      
       pq.update(pod_clearance: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -109,7 +109,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from POD_CLEARED to MINISTER_WAITING if sent_to_answering_minister completed and there is no policy minister' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_cleared.id)
-      pq_before = pq.dup
+      
       pq.update(sent_to_answering_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -121,7 +121,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from POD_CLEARED to MINISTER_WAITING if sent_to_answering_minister and sent_to_policy_minister is completed when policy minister is set' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_cleared.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(sent_to_answering_minister: DateTime.now, sent_to_policy_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -133,7 +133,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question from POD_CLEARED to MINISTER_WAITING if sent_to_answering_minister completed and sent_to_policy_minister is NOT completed when policy minister is set' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_cleared.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(sent_to_answering_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -145,7 +145,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question to MINISTER_WAITING if the progress is not POD_CLEARED' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_waiting.id)
-      pq_before = pq.dup
+      
       pq.update(sent_to_answering_minister: DateTime.now, sent_to_policy_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -160,7 +160,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_WAITING to MINISTER_QUERY if answering_minister_query true and there is no policy minister' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_waiting.id)
-      pq_before = pq.dup
+      
       pq.update(answering_minister_query: true)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -172,7 +172,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_WAITING to MINISTER_QUERY if policy_minister_query true when policy minister is set' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_waiting.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(policy_minister_query: true)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -184,7 +184,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_WAITING to MINISTER_QUERY if answering_minister_query true when policy minister is set' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_waiting.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(answering_minister_query: true)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -197,7 +197,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question to MINISTER_QUERY if the progress is not MINISTER_WAITING' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_waiting.id)
-      pq_before = pq.dup
+      
       pq.update(answering_minister_query: true)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -211,7 +211,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_QUERY to MINISTER_CLEARED if cleared_by_answering_minister completed and there is no policy minister' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_query.id)
-      pq_before = pq.dup
+      
       pq.update(cleared_by_answering_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -223,7 +223,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_WAITING to MINISTER_CLEARED if cleared_by_answering_minister completed and there is no policy minister' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_waiting.id)
-      pq_before = pq.dup
+      
       pq.update(cleared_by_answering_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -236,7 +236,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_QUERY to MINISTER_CLEARED if cleared_by_answering_minister and cleared_by_policy_minister is completed when policy minister is set' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_query.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(cleared_by_answering_minister: DateTime.now, cleared_by_policy_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -248,7 +248,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_WAITING to MINISTER_CLEARED if cleared_by_answering_minister and cleared_by_policy_minister is completed when policy minister is set' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_waiting.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(cleared_by_answering_minister: DateTime.now, cleared_by_policy_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -260,7 +260,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question from MINISTER_QUERY to MINISTER_CLEARED if cleared_by_answering_minister completed and cleared_by_policy_minister is NOT completed when policy minister is set' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_query.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(cleared_by_answering_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -272,7 +272,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question to MINISTER_CLEARED if the progress is not MINISTER_QUERY or MINISTER_WAITING' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_waiting.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(cleared_by_answering_minister: DateTime.now, cleared_by_policy_minister: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -288,7 +288,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_CLEARED to ANSWERED if answer_submitted completed' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_cleared.id)
-      pq_before = pq.dup
+      
       pq.update(answer_submitted: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -300,7 +300,7 @@ describe 'PQProgressChangerService' do
     it 'should move the question from MINISTER_CLEARED to ANSWERED if pq_withdrawn set' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_cleared.id)
-      pq_before = pq.dup
+      
       pq.update(pq_withdrawn: DateTime.now)
 
       @pq_progress_changer_service.update_progress(pq)
@@ -312,7 +312,7 @@ describe 'PQProgressChangerService' do
     it 'should not move the question from MINISTER_CLEARED to ANSWERED if pq_withdrawn or answer_submitted not set ' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.minister_cleared.id)
-      pq_before = pq.dup
+      
 
       @pq_progress_changer_service.update_progress(pq)
 
@@ -324,7 +324,7 @@ describe 'PQProgressChangerService' do
     it 'should NOT move the question to ANSWERED if the progress is not MINISTER_CLEARED' do
       uin = 'TEST1'
       pq = create(:PQ, uin: uin, question: 'test question?', progress_id: Progress.pod_waiting.id, policy_minister_id: minister_1.id)
-      pq_before = pq.dup
+      
       pq.update(pq_withdrawn: true)
 
       @pq_progress_changer_service.update_progress(pq)
