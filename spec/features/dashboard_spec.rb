231a1aed1bea083b106ef65b86f321b2366524c2
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-07-16 11:14:27 +0100

    fixed the failing spec

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -13,7 +13,7 @@ feature 'Visit the dashboard an show the questions for the day' do
     context 'view' do
       scenario "can view the questions tabled for today" do
         visit '/dashboard'
-        expect(page).to have_content('Parliamentary Questions')
+        expect(page).to have_content('PQ Tool')
       end
     end
 
