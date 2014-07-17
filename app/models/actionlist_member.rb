e3b2a1ac1bd57241090a89c1fca91c71398a61a0
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-07-17 17:01:23 +0100

    Action list model, controller and views

---
+++ app/models/actionlist_member.rb
@@ -0,0 +1,17 @@
+class ActionlistMember < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: true, on: :create
+  	validates_format_of :email,:with => Devise::email_regexp
+  
+	before_validation :strip_whitespace
+	after_initialize :init
+
+    def init
+      self.deleted  ||= false           #will set the default value only if it's nil
+    end
+    
+	def strip_whitespace
+		self.name = self.name.strip unless self.name.nil?
+		self.email = self.email.strip unless self.email.nil?
+	end
+end
