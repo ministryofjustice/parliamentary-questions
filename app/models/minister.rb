a3e7d8425c177c9bdf038c02cda1ba9f226c6d16
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-07-24 12:16:51 +0100

    first version of the questions  report

--- app/models/minister.rb
+++ app/models/minister.rb
@@ -8,4 +8,9 @@ class Minister < ActiveRecord::Base
     self.name = self.name.strip unless self.name.nil?
     self.email = self.email.strip unless self.email.nil?
   end
+
+  def self.all_active
+    Minister.where(deleted: false).all
+  end
+
 end
