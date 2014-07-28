c9b6fc4e2d626305a08601499931eb55e1312555
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-07-28 15:01:53 +0100

    removed minister email from seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,12 +34,12 @@ progresses = Progress.create([
   {name: Progress.TRANSFERRED_OUT}])
   
 ministers = Minister.create!([
-	{name: 'Chris Grayling', email:'no-emails-1@please.com', title: 'Secretary of State and Lord High Chancellor of Great Britain', deleted: false},
-	{name: 'Damian Green (MP)', email:'no-emails-3@please.com', title: 'Minister of State', deleted: false},
-	{name: 'Jeremy Wright (MP)', email:'no-emails-5@please.com', title: 'Parliamentary Under-Secretary of State; Minister for Prisons and Rehabilitation', deleted: false},
-	{name: 'Shailesh Vara (MP)', email:'no-emails-7@please.com', title: 'Parliamentary Under-Secretary of State', deleted: false},
-	{name: 'Simon Hughes (MP)', email:'no-emails-9@please.com', title: 'Minister of State for Justice & Civil Liberties', deleted: false},
-	{name: 'Lord Faulks QC', email:'no-emails-10@please.com', title: 'Lord Faulks QC, Minister of State', deleted: false}])
+	{name: 'Chris Grayling', title: 'Secretary of State and Lord High Chancellor of Great Britain', deleted: false},
+	{name: 'Damian Green (MP)', title: 'Minister of State', deleted: false},
+	{name: 'Jeremy Wright (MP)', title: 'Parliamentary Under-Secretary of State; Minister for Prisons and Rehabilitation', deleted: false},
+	{name: 'Shailesh Vara (MP)', title: 'Parliamentary Under-Secretary of State', deleted: false},
+	{name: 'Simon Hughes (MP)',  title: 'Minister of State for Justice & Civil Liberties', deleted: false},
+	{name: 'Lord Faulks QC',  title: 'Lord Faulks QC, Minister of State', deleted: false}])
 
 directorates = Directorate.create!([
 	{name: 'Finance Assurance and Commercial', deleted: false},
