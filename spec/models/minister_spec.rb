7fe32c5401652d902125e7be68fd770c217a6350
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-07-28 16:19:36 +0100

    updated minister tests

--- spec/models/minister_spec.rb
+++ spec/models/minister_spec.rb
@@ -7,12 +7,6 @@ describe Minister do
 		expect(minister).to be_valid
 	end
 
-	it 'should have a unique email' do
-		minister = create(:minister, name: 'MP one', email: 'first.name@mp.com')
-		duplicate = build(:minister, name: 'MP two', email: 'first.name@mp.com')
-		expect(duplicate).to be_invalid
-	end
-
 	describe "associations" do
 		it 'should have a collection of PQs' do
 			@pqs = minister.should respond_to(:pqs)
