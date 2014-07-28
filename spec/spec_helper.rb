45cc91f2df99697be227f10196cf6ff87c5eb8f1
David Hernandez <david.hernandez@digital.justice.gov.uk>
2014-07-28 16:51:12 +0100

    warning fixing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/email/rspec'
 require 'headless'
@@ -46,6 +45,10 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
