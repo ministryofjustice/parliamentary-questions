a375f1d2c18265e32dfa54d06adc5d7530f33435
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-08-06 09:58:29 +0100

    PT#76298746 Acceptance reminder email

--- app/mailers/action_officer_reminder_mailer.rb
+++ app/mailers/action_officer_reminder_mailer.rb
@@ -1,15 +1,13 @@
 class ActionOfficerReminderMailer < PQBaseMailer
   default from: Settings.mail_from
   def remind_accept_reject_email(template_params)
-
     @template_params = template_params
 
     #template[:name] = ao.name
     #template[:email] = ao.email
     #template[:uin] = pq.uin
     #template[:question] = pq.question
-
-    mail(to: @template_params[:email], subject: "[Parliamentary-Questions] URGENT: #{@template_params[:uin]} | You have to Accept or Reject the question")
+    mail(to: @template_params[:email], subject: "#{@template_params[:uin]} | URGENT: you need to accept or reject this PQ")
   end
 
 end
--- spec/mailers/action_officer_reminder_mailer_spec.rb
+++ spec/mailers/action_officer_reminder_mailer_spec.rb
@@ -1,9 +1,40 @@
-require 'rspec'
+require 'spec_helper'
 
-describe deliver do
+describe 'ActionOfficerReminderMailer' do
 
-  it 'should include house and member name' do
+  let(:ao) { create(:action_officer,  name: 'ao name 1', email: 'ao@ao.gov') }
+  let(:minister_1) { create(:minister, name: 'Mr Name1 for Test') }
 
-    true.should == false
+  before(:each) do
+    @pq = create(:Pq, uin: 'HL789', question: 'test question?', member_name:'Asking MP', minister_id: minister_1.id, house_name: 'House of Lords')
+    @ao_pq = ActionOfficersPq.new(action_officer_id: ao.id, pq_id: @pq.id)
+
+    @template = Hash.new
+    @template[:name] = ao.name
+    @template[:email] = ao.emails
+    @template[:uin] = @pq.uin
+    @template[:question] = @pq.question
+    @template[:member_name] = @pq.member_name
+    @template[:house] = @pq.house_name
+  end
+
+  describe 'draft reminder' do
+    describe 'deliver' do
+      it 'should include house, member name and uin from PQ and AO name' do
+        ActionOfficerReminderMailer.remind_accept_reject_email(@template).deliver
+
+        mail = ActionMailer::Base.deliveries.first
+
+        mail.html_part.body.should include @pq.member_name
+        mail.html_part.body.should include @pq.house_name
+        mail.html_part.body.should include @pq.uin
+        mail.html_part.body.should include ao.name
+
+        mail.text_part.body.should include @pq.member_name
+        mail.text_part.body.should include @pq.house_name
+        mail.text_part.body.should include @pq.uin
+        mail.text_part.body.should include ao.name
+      end
+    end
   end
 end
