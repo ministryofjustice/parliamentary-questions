417959c861fd82d6751d2895bd83e7f602e107d6
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-08-13 12:36:36 +0100

    Removed outdated filters

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -35,62 +35,6 @@ class ReportsController < ApplicationController
     end
   end
 
-  def ministers_filter
-    minister_id = params[:minister_id]
-    progress_id = params[:progress_id]
-    @progresses = []
-
-    if minister_id.nil? || minister_id.empty?
-      return render action: 'ministers_filter', minister_id: minister_id, progress_id: progress_id
-    end
-
-    progress_counters = PQ_by_minister(minister_id).group('progress_id').count
-
-    Progress.all.each do |it|
-      count = progress_counters[it.id] || 0
-      @progresses.push({id: it.id, name: it.name, count: count})
-    end
-
-    if !progress_id.nil? && !progress_id.empty?
-      pqs = PQ_by_minister(minister_id).where('progress_id = ? ', progress_id)
-    else
-      pqs = PQ_by_minister(minister_id)
-    end
-
-    @questions_count = pqs.count
-    @questions = pqs.paginate(:page => params[:page], :per_page => @@per_page).order(:internal_deadline).load
-
-    render action: 'ministers_filter', minister_id: minister_id, progress_id: progress_id
-  end
-
-  def press_desk_filter
-    press_desk_id = params[:press_desk_id]
-    progress_id = params[:progress_id]
-    @progresses = []
-
-    if press_desk_id.nil? || press_desk_id.empty?
-      return render action: 'press_desk_filter', press_desk_id: press_desk_id, progress_id: progress_id
-    end
-
-    aos = PressDesk.find(press_desk_id).action_officers.collect{|it| it.id}
-
-    Progress.all.each do |it|
-      count = PQ_by_press_desk(aos).where('progress_id = ?', it.id).count
-      @progresses.push({id: it.id, name: it.name, count: count})
-    end
-
-    if !progress_id.nil? && !progress_id.empty?
-      pqs = PQ_by_press_desk(aos).where('progress_id = ?', progress_id)
-    else
-      pqs = PQ_by_press_desk(aos)
-    end
-
-    @questions_count = pqs.count
-    @questions = pqs.paginate(:page => params[:page], :per_page => @@per_page).order(:internal_deadline).load
-
-    render action: 'press_desk_filter', press_desk_id: press_desk_id, progress_id: progress_id
-  end
-
   def filter_all
     minister_id = params[:minister_id]
     press_desk_id = params[:press_desk_id]
