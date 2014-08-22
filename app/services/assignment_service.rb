a2b7838dba4078a1caf3408c4950cb6ed114da86
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-08-22 13:06:22 +0100

    Refactored Assignment Service

--- app/services/assignment_service.rb
+++ app/services/assignment_service.rb
@@ -7,8 +7,8 @@ class AssignmentService
     pq = Pq.find_by(id: assignment.pq_id)
 
 
-    div = ao.deputy_director.division.id if ao.deputy_director && ao.deputy_director.division
-    dir = ao.deputy_director.division.directorate.id if ao.deputy_director && ao.deputy_director.division && ao.deputy_director.division.directorate
+    div = get_dep_director_for_ao(ao)
+    dir = get_directorate_for_ao(ao)
 
     pro = Progress.accepted
     pq.update progress_id: pro.id, at_acceptance_directorate_id: dir, at_acceptance_division_id: div
@@ -23,17 +23,19 @@ class AssignmentService
 
     if pq.action_officers_pq.rejected.size==pq.action_officers_pq.size
       pro = Progress.rejected
-      pq.update progress_id: pro.id
-      return
-    end
-
-    if pq.action_officers_pq.accepted.size >=1
+    elsif pq.action_officers_pq.accepted.size >=1
       pro = Progress.accepted
-      pq.update progress_id: pro.id
-      return
+    else
+      pro = Progress.no_response
     end
-
-    pro = Progress.no_response
     pq.update progress_id: pro.id
+  end
+
+  private
+  def get_dep_director_for_ao(ao)
+    ao.deputy_director.division.id if ao.deputy_director && ao.deputy_director.division
+  end
+  def get_directorate_for_ao(ao)
+    ao.deputy_director.division.directorate.id if ao.deputy_director && ao.deputy_director.division && ao.deputy_director.division.directorate
   end
 end
