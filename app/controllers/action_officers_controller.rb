8de9c3ebce713164d41e8be70963a0cf5068023a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-08-22 11:58:50 +0100

    Added error handling for AO/DD constraint exceptions

--- app/controllers/action_officers_controller.rb
+++ app/controllers/action_officers_controller.rb
@@ -42,9 +42,18 @@ class ActionOfficersController < ApplicationController
 
   # PATCH/PUT /action_officers/1
   def update
+    begin
     if @action_officer.update(action_officer_params)
       redirect_to @action_officer, notice: 'Action officer was successfully updated.'
     else
+      render action: 'edit'
+    end
+    rescue => err
+      if err.message.include?('index_action_officers_on_email_and_deputy_director_id')
+        @action_officer.errors[:base] << "This email address(#{@action_officer.email}) is already assigned to #{@action_officer.deputy_director.name}"
+      else
+        @action_officer.errors[:base] << err
+      end
       render action: 'edit'
     end
   end
