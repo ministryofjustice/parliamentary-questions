da64e88f7c9af1f7a6df20d399afb9d696f7f483
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2014-09-04 14:19:25 +0100

    Amended Watchlist emailer to be in One Single Mail, rather than a mail per member. Amended expiry of watchlist link to 3 days Amended Watchlist email text Amended Subject of mail produced from the mailto link in the acceptance email

--- spec/services/watchlist_report_service_spec.rb
+++ spec/services/watchlist_report_service_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'WatchlistReportService' do
 
+
   let!(:watchlist_one) { create(:watchlist_member, name: 'member 1', email: 'm1@ao.gov',  deleted: false) }
   let!(:watchlist_two) { create(:watchlist_member, name: 'member 2', email: 'm2@ao.gov', deleted: false) }
   let!(:watchlist_deleted) { create(:watchlist_member, name: 'member 3', email: 'm3@ao.gov', deleted: true) }
@@ -16,22 +17,22 @@ describe 'WatchlistReportService' do
     @report_service.send()
 
     # first watchlist member
-    token = Token.where(entity: "watchlist:#{watchlist_one.id}", path: '/watchlist/dashboard').first
+    token = Token.where(entity: "watchlist-" + DateTime.now.to_s, path: '/watchlist/dashboard').first
     token.should_not be nil
     token.id.should_not be nil
     token.token_digest.should_not be nil
 
-    end_of_day = DateTime.now.end_of_day.change({:offset => 0})
+    end_of_day = DateTime.now.end_of_day.change({:offset => 0})+3
     token.expire.should eq(end_of_day)
 
 
     # second watchlist member
-    token = Token.where(entity: "watchlist:#{watchlist_two.id}", path: '/watchlist/dashboard').first
+    token = Token.where(entity: "watchlist-" + DateTime.now.to_s, path: '/watchlist/dashboard').first
     token.should_not be nil
     token.id.should_not be nil
     token.token_digest.should_not be nil
 
-    end_of_day = DateTime.now.end_of_day.change({:offset => 0})
+    end_of_day = DateTime.now.end_of_day.change({:offset => 0})+3
     token.expire.should eq(end_of_day)
 
 
@@ -41,6 +42,31 @@ describe 'WatchlistReportService' do
   end
 
   it 'should send an email with the right data' do
+    pqtest_mail ='pqtest@digital.justice.gov.uk'
+
+    result = @report_service.send()
+
+    # first email for the first member
+    mail = ActionMailer::Base.deliveries.first
+    #sentToken = result[watchlist_one.id]
+    sentToken = result[pqtest_mail]
+    token_param = {token: sentToken}.to_query
+    entity = {entity: entity = "watchlist-" + DateTime.now.to_s}.to_query
+    url = '/watchlist/dashboard'
+
+    mail.html_part.body.should include url
+    mail.html_part.body.should include token_param
+    mail.html_part.body.should include entity
+
+
+    mail.text_part.body.should include url
+    mail.text_part.body.should include token_param
+    mail.text_part.body.should include entity
+
+    mail.to.should include pqtest_mail
+
+  end
+  it 'should add the people from the Watchlist to the CC' do
 
     result = @report_service.send()
 
@@ -48,42 +74,15 @@ describe 'WatchlistReportService' do
     mail = ActionMailer::Base.deliveries.first
     sentToken = result[watchlist_one.id]
     token_param = {token: sentToken}.to_query
-    entity = {entity: "watchlist:#{watchlist_one.id}"}.to_query
+    entity = {entity: entity = "watchlist-" + DateTime.now.to_s}.to_query
     url = '/watchlist/dashboard'
 
-    mail.html_part.body.should include url
-    mail.html_part.body.should include token_param
-    mail.html_part.body.should include entity
+    mail.cc.should include watchlist_one.email
+    mail.cc.should include watchlist_two.email
 
-
-    mail.text_part.body.should include url
-    mail.text_part.body.should include token_param
-    mail.text_part.body.should include entity
-
-    mail.to.should include watchlist_one.email
-
-
-    # second email for the second member
-    mail = ActionMailer::Base.deliveries[1]
-    sentToken = result[watchlist_two.id]
-    token_param = {token: sentToken}.to_query
-    entity = {entity: "watchlist:#{watchlist_two.id}"}.to_query
-    url = '/watchlist/dashboard'
-
-    mail.html_part.body.should include url
-    mail.html_part.body.should include token_param
-    mail.html_part.body.should include entity
-
-
-    mail.text_part.body.should include url
-    mail.text_part.body.should include token_param
-    mail.text_part.body.should include entity
-
-    mail.to.should include watchlist_two.email
 
 
   end
-
 
 
 end
