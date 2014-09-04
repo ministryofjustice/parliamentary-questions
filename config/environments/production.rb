85f571419521e2cccd06e17ee107ccb1f17119a1
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-09-04 12:50:34 +0100

    Disable production debug logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,9 +42,6 @@ ParliamentaryQuestions::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Set to :debug to see everything in the log.
-  config.log_level = :debug
-
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
