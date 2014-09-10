1a11e5029fa1d284f1b4f8edf31a0d095d3b0a92
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-09-10 16:50:33 +0100

    refactor pq mailer for cc

--- app/mailers/pq_mailer.rb
+++ app/mailers/pq_mailer.rb
@@ -17,26 +17,7 @@ class PqMailer < PQBaseMailer
 
   def acceptance_email(pq, ao, urgent = false)
 
-    @template_params = Hash.new
-    @template_params[:ao_name] = ao.name
-    @template_params[:email] = ao.emails
-    @template_params[:uin] = pq.uin
-    @template_params[:question] = pq.question
-    @template_params[:mpname] = pq.minister.name unless pq.minister.nil?
-    @template_params[:mpemail] = pq.minister.email unless pq.minister.nil?
-    @template_params[:policy_mpname] = pq.policy_minister.name unless pq.policy_minister.nil?
-    @template_params[:policy_mpemail] = pq.policy_minister.email unless pq.policy_minister.nil?
-    @template_params[:press_email] = ao.press_desk.email_output unless ao.press_desk.nil?
-    @template_params[:member_name] = pq.member_name unless pq.member_name.nil?
-    @template_params[:house_name] = pq.house_name unless pq.house_name.nil?
-    @template_params[:internal_deadline] = "#{pq.internal_deadline.strftime('%d/%m/%Y') } - 10am " unless pq.internal_deadline.nil?
-
-
-    mp_cc_list = [
-        'Christopher.Beal@justice.gsi.gov.uk',
-        'Nicola.Calderhead@justice.gsi.gov.uk',
-        'thomas.murphy@JUSTICE.gsi.gov.uk'
-    ]
+    @template_params = build_primary_hash(pq, ao)
 
     cc_list = [
         @template_params[:mpemail],
@@ -44,28 +25,18 @@ class PqMailer < PQBaseMailer
         @template_params[:press_email]
     ]
 
-    # add the mp_cc_list if the minister is 'Simon Hughes'
-    if !pq.minister.nil? && pq.minister.name == 'Simon Hughes'
-      cc_list.append(mp_cc_list)
-    end
+    cc_list.append(get_mp_office_email(pq, 'Simon Hughes (MP)'))
 
     # add the people from the Actionlist
-    action_list_emails = ActionlistMember.where('deleted = false').collect{|it| it.email}
-    cc_list.append(action_list_emails)
+    cc_list.append(get_action_list_mails)
 
     #add the Deputy director of the AO if they exist and have a mail.
-    if !ao.deputy_director.nil?
-      if !ao.deputy_director.email.nil?
-       cc_list.append(ao.deputy_director.email)
-      end
-    end
+    cc_list.append(get_dd_email(ao))
 
-    if pq.finance_interest
-      finance_users_emails = User.where("roles = 'FINANCE'").where('is_active = TRUE').collect{|it| it.email}
-      cc_list.append(finance_users_emails)
-    end
+    # get the finance emails
+    cc_list.append(finance_users_emails) if pq.finance_interest
 
-    @template_params[:cc_list] = cc_list.join(';')
+    @template_params[:cc_list] = cc_list.reject(&:blank?).join(';')
 
     if urgent
       subject = "URGENT : please send your draft response for PQ #{@template_params[:uin]}"
@@ -94,5 +65,47 @@ class PqMailer < PQBaseMailer
     date = Date.today.strftime('%d/%m/%Y')
     @template_params[:date] = date
     mail(to: @template_params[:email], cc: @template_params[:cc], subject: 'PQs allocated today')
+  end
+
+  private
+  def build_primary_hash(pq,ao)
+    {
+      :ao_name => ao.name,
+      :email => ao.emails,
+      :uin => pq.uin,
+      :question => pq.question,
+      :mpname => pq.minister.nil? ? '' : pq.minister.name ,
+      :mpemail => pq.minister.nil? ? '' : pq.minister.email,
+      :policy_mpname => pq.policy_minister.nil? ? '' :  pq.policy_minister.name,
+      :policy_mpemail => pq.policy_minister.nil? ? '' :  pq.policy_minister.email,
+      :press_email => ao.press_desk.nil? ? '' : ao.press_desk.email_output,
+      :member_name => pq.member_name.nil? ? '' : pq.member_name,
+      :house_name => pq.house_name.nil? ? '' : pq.house_name,
+      :internal_deadline => pq.internal_deadline.nil? ? '' : "#{pq.internal_deadline.strftime('%d/%m/%Y') } - 10am "
+    }
+  end
+
+  def get_mp_office_email(pq, mp_name)
+    # add the mp_cc_list if the minister is 'Simon Hughes'
+    if !pq.minister.nil? && pq.minister.name == mp_name
+      return [
+          'Christopher.Beal@justice.gsi.gov.uk',
+          'Nicola.Calderhead@justice.gsi.gov.uk',
+          'thomas.murphy@JUSTICE.gsi.gov.uk'
+      ]
+    end
+  end
+  def get_action_list_mails
+    ActionlistMember.where('deleted = false').collect{|it| it.email}
+  end
+  def finance_users_emails
+    User.where("roles = 'FINANCE'").where('is_active = TRUE').collect{|it| it.email}
+  end
+  def get_dd_email(ao)
+    if !ao.deputy_director.nil?
+      if !ao.deputy_director.email.nil?
+        return ao.deputy_director.email
+      end
+    end
   end
 end
