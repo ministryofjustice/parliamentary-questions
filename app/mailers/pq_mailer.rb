ab83d5561392899c37bbef2b64b61baa4a824053
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-09-11 10:33:13 +0100

    interim changes to pq mailer and accept reject changes

--- app/mailers/pq_mailer.rb
+++ app/mailers/pq_mailer.rb
@@ -60,10 +60,16 @@ class PqMailer < PQBaseMailer
       end
     end
 
+<<<<<<< Updated upstream
     if pq.finance_interest
       finance_users_emails = User.where("roles = 'FINANCE'").where('is_active = TRUE').collect{|it| it.email}
       cc_list.append(finance_users_emails)
     end
+=======
+    # get the finance emails
+    @template_params[:finance_users_emails] = finance_users_emails(pq)
+    cc_list.append(@template_params['finance_users_emails'])
+>>>>>>> Stashed changes
 
     @template_params[:cc_list] = cc_list.join(';')
 
@@ -95,4 +101,70 @@ class PqMailer < PQBaseMailer
     @template_params[:date] = date
     mail(to: @template_params[:email], cc: @template_params[:cc], subject: 'PQs allocated today')
   end
+<<<<<<< Updated upstream
+=======
+
+  private
+  def build_primary_hash(pq,ao)
+    {
+      :ao_name => ao.name,
+      :email => ao.emails,
+      :uin => pq.uin,
+      :question => pq.question,
+      :mpname => get_minister_detail(pq,'name') ,
+      :mpemail => get_minister_email(pq.minister),
+      :policy_mpname => get_policy_minister_detail(pq, 'name'),
+      :policy_mpemail => get_minister_email(pq.policy_minister),
+      :press_email => ao.press_desk.nil? ? '' : ao.press_desk.email_output,
+      :member_name => get_pq_details(pq,'member_name'),
+      :house_name => get_pq_details(pq,'house_name'),
+      :internal_deadline => pq.internal_deadline.nil? ? '' : "#{pq.internal_deadline.strftime('%d/%m/%Y') } - 10am "
+    }
+  end
+
+  def get_minister_email(minister)
+    minister.nil? ? '' : minister.email
+  end
+  def get_minister_detail(pq, field)
+    if !pq.minister.nil?
+      return pq.minister[field]
+    else
+      return ''
+    end
+  end
+  def get_policy_minister_detail(pq, field)
+    if !pq.policy_minister.nil?
+      return pq.policy_minister[field]
+    else
+      return ''
+    end
+  end
+  def get_pq_details(pq, field)
+    pq[field].nil? ? '' : pq[field]
+  end
+  def get_mp_office_email(pq, mp_name)
+    # add the mp_cc_list if the minister is 'Simon Hughes'
+    if !pq.minister.nil? && pq.minister.name == mp_name
+      return [
+          'Christopher.Beal@justice.gsi.gov.uk',
+          'Nicola.Calderhead@justice.gsi.gov.uk',
+          'thomas.murphy@JUSTICE.gsi.gov.uk'
+      ]
+    end
+  end
+  def get_action_list_mails
+    ActionlistMember.where('deleted = false').collect{|it| it.email}
+  end
+  def finance_users_emails(pq)
+    result = User.where("roles = 'FINANCE'").where('is_active = TRUE').collect{|it| it.email}
+    return pq.finance_interest ? result : ''
+  end
+  def get_dd_email(ao)
+    if !ao.deputy_director.nil?
+      if !ao.deputy_director.email.nil?
+        return ao.deputy_director.email
+      end
+    end
+  end
+>>>>>>> Stashed changes
 end
