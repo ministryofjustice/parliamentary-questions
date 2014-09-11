badbb9fde25d2ca76a461335ee2c4c2ea0b88cf4
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-09-11 10:50:45 +0100

    pq mailer updates for finance emails

--- app/mailers/pq_mailer.rb
+++ app/mailers/pq_mailer.rb
@@ -17,26 +17,7 @@ class PqMailer < PQBaseMailer
 
   def acceptance_email(pq, ao, urgent = false)
 
-    @template_params = Hash.new
-    @template_params[:ao_name] = ao.name
-    @template_params[:email] = ao.emails
-    @template_params[:uin] = pq.uin
-    @template_params[:question] = pq.question
-    @template_params[:mpname] = pq.minister.name unless pq.minister.nil?
-    @template_params[:mpemail] = pq.minister.email unless pq.minister.nil?
-    @template_params[:policy_mpname] = pq.policy_minister.name unless pq.policy_minister.nil?
-    @template_params[:policy_mpemail] = pq.policy_minister.email unless pq.policy_minister.nil?
-    @template_params[:press_email] = ao.press_desk.email_output unless ao.press_desk.nil?
-    @template_params[:member_name] = pq.member_name unless pq.member_name.nil?
-    @template_params[:house_name] = pq.house_name unless pq.house_name.nil?
-    @template_params[:internal_deadline] = "#{pq.internal_deadline.strftime('%d/%m/%Y') } - 10am " unless pq.internal_deadline.nil?
-
-
-    mp_cc_list = [
-        'Christopher.Beal@justice.gsi.gov.uk',
-        'Nicola.Calderhead@justice.gsi.gov.uk',
-        'thomas.murphy@JUSTICE.gsi.gov.uk'
-    ]
+    @template_params = build_primary_hash(pq, ao)
 
     cc_list = [
         @template_params[:mpemail],
@@ -44,34 +25,20 @@ class PqMailer < PQBaseMailer
         @template_params[:press_email]
     ]
 
-    # add the mp_cc_list if the minister is 'Simon Hughes'
-    if !pq.minister.nil? && pq.minister.name == 'Simon Hughes'
-      cc_list.append(mp_cc_list)
-    end
+    cc_list.append(get_mp_office_email(pq, 'Simon Hughes (MP)'))
 
     # add the people from the Actionlist
-    action_list_emails = ActionlistMember.where('deleted = false').collect{|it| it.email}
-    cc_list.append(action_list_emails)
+    cc_list.append(get_action_list_mails)
 
     #add the Deputy director of the AO if they exist and have a mail.
-    if !ao.deputy_director.nil?
-      if !ao.deputy_director.email.nil?
-       cc_list.append(ao.deputy_director.email)
-      end
-    end
+    cc_list.append(get_dd_email(ao))
 
-<<<<<<< Updated upstream
-    if pq.finance_interest
-      finance_users_emails = User.where("roles = 'FINANCE'").where('is_active = TRUE').collect{|it| it.email}
-      cc_list.append(finance_users_emails)
-    end
-=======
     # get the finance emails
     @template_params[:finance_users_emails] = finance_users_emails(pq)
-    cc_list.append(@template_params['finance_users_emails'])
->>>>>>> Stashed changes
+    cc_list.append(@template_params[:finance_users_emails]) if !@template_params[:finance_users_emails].empty?
 
-    @template_params[:cc_list] = cc_list.join(';')
+    # merge cc_list and remove blanks
+    @template_params[:cc_list] = cc_list.reject(&:blank?).join(';')
 
     if urgent
       subject = "URGENT : please send your draft response for PQ #{@template_params[:uin]}"
@@ -101,8 +68,6 @@ class PqMailer < PQBaseMailer
     @template_params[:date] = date
     mail(to: @template_params[:email], cc: @template_params[:cc], subject: 'PQs allocated today')
   end
-<<<<<<< Updated upstream
-=======
 
   private
   def build_primary_hash(pq,ao)
@@ -157,7 +122,7 @@ class PqMailer < PQBaseMailer
   end
   def finance_users_emails(pq)
     result = User.where("roles = 'FINANCE'").where('is_active = TRUE').collect{|it| it.email}
-    return pq.finance_interest ? result : ''
+    return !pq.finance_interest ? result : ''
   end
   def get_dd_email(ao)
     if !ao.deputy_director.nil?
@@ -166,5 +131,4 @@ class PqMailer < PQBaseMailer
       end
     end
   end
->>>>>>> Stashed changes
 end
