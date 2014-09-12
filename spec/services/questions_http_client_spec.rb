65a4402464b62e3818f3c238d1b4b945a72ba653
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-09-12 17:27:59 +0100

    Switched off test that travis can not run

--- spec/services/questions_http_client_spec.rb
+++ spec/services/questions_http_client_spec.rb
@@ -10,8 +10,8 @@ describe 'QuestionsHttpClient' do
     expect{@http_client.questions('dateFrom' => 'Force error')}.to raise_error('API response non-valid')
   end
 
-  # Mark as a pending because it need the credentials for the API to pass
-  it 'should get an xml response from the PQ API' do
+  # Mark as a pending because it need the credentials for the API to pass on Travis
+  xit 'should get an xml response from the PQ API' do
     questions_xml = @http_client.questions('dateFrom' => '2014-04-17')
     questions_xml.should include('<Questions')
   end
