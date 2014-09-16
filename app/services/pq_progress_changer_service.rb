3e6fad66e757842660e91787366f10c49ed8f281
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2014-09-16 09:24:08 +0100

    Tidied

--- app/services/pq_progress_changer_service.rb
+++ app/services/pq_progress_changer_service.rb
@@ -2,37 +2,33 @@ class PQProgressChangerService
 
   def update_progress(pq)
 
-    @new_progress = Progress.draft_pending
+    @new_progress = pq.progress
     @progress_onwards = TRUE
 
 
     #  initial state 'Draft Pending' (done by the import process)
     with_pod_filter(pq) if @progress_onwards
-    puts "with_pod_filter ?         : " + @new_progress.name + "  Onwards?" + @progress_onwards.to_s
+        puts "with_pod_filter ?         : " + @new_progress.name + "  Onwards? " + @progress_onwards.to_s
     pod_query_filter(pq) if @progress_onwards
-    puts "pod_query_filter ?        : " + @new_progress.name + "  Onwards?" + @progress_onwards.to_s
+        puts "pod_query_filter ?        : " + @new_progress.name + "  Onwards? " + @progress_onwards.to_s
     pod_clearance_filter(pq) if @progress_onwards
-    puts "pod_clearance_filter ?    : " + @new_progress.name + "  Onwards?" + @progress_onwards.to_s
+        puts "pod_clearance_filter ?    : " + @new_progress.name + "  Onwards? " + @progress_onwards.to_s
     with_minister_filter(pq) if @progress_onwards
-    puts "with_minister_filter ?    : " + @new_progress.name + "  Onwards?" + @progress_onwards.to_s
+        puts "with_minister_filter ?    : " + @new_progress.name + "  Onwards? " + @progress_onwards.to_s
     minister_query_filter(pq) if @progress_onwards
-    puts "minister_query_filter ?   : " + @new_progress.name + "  Onwards?" + @progress_onwards.to_s
+        puts "minister_query_filter ?   : " + @new_progress.name + "  Onwards? " + @progress_onwards.to_s
     minister_cleared_filter(pq) if @progress_onwards
-    puts "minister_cleared_filter ? : " + @new_progress.name + "  Onwards?" + @progress_onwards.to_s
+        puts "minister_cleared_filter ? : " + @new_progress.name + "  Onwards? " + @progress_onwards.to_s
     answered_filter(pq) if @progress_onwards
-    puts "answered_filter ?         : " + @new_progress.name + "  Onwards?" + @progress_onwards.to_s
+        puts "answered_filter ?         : " + @new_progress.name + "  Onwards? " + @progress_onwards.to_s
     transferred_out(pq) # Always check transferred out.
-    puts "transferred_out ?         : " + @new_progress.name + "  Onwards?" + @progress_onwards.to_s
+        puts "transferred_out ?         : " + @new_progress.name + "  Onwards? " + @progress_onwards.to_s
 
     update_pq(pq, @new_progress)
 
   end
 
   def with_pod_filter(pq)
-    # if !pq.is_in_progress?(Progress.draft_pending)
-    #   return
-    # end
-
     if !pq.draft_answer_received.nil?
       @new_progress =  Progress.with_pod
     else
@@ -42,10 +38,6 @@ class PQProgressChangerService
   end
 
   def pod_query_filter(pq)
-    # if !pq.is_in_progress?(Progress.with_pod)
-    #   return
-    # end
-
     if pq.pod_query_flag
       @new_progress =  Progress.pod_query
     # else
@@ -54,11 +46,6 @@ class PQProgressChangerService
   end
 
   def pod_clearance_filter(pq)
-    # not (with_pod || pod_query)
-    # if !(pq.is_in_progress?(Progress.with_pod) || pq.is_in_progress?(Progress.pod_query))
-    #   return
-    # end
-
     if !(!pq.draft_answer_received.nil? || pq.pod_query_flag)
       return
     end
@@ -72,10 +59,6 @@ class PQProgressChangerService
 
 
   def with_minister_filter(pq)
-    # if !pq.is_in_progress?(Progress.pod_cleared)
-    #   return
-    # end
-
 
     # does not have policy minister
     if pq.policy_minister.nil?
@@ -98,18 +81,12 @@ class PQProgressChangerService
   end
 
   def minister_query_filter(pq)
-    # if !pq.is_in_progress?(Progress.with_minister)
-    #   return
-    # end
-
 
     # does not have policy minister
     if pq.policy_minister.nil?
       if pq.answering_minister_query
         @new_progress =  Progress.ministerial_query
         return
-      # else
-      #   @progress_onwards = FALSE
       end
     end
 
@@ -118,17 +95,11 @@ class PQProgressChangerService
       @new_progress = Progress.ministerial_query
       progress_onwards = TRUE
       return
-    # else
-    #   @progress_onwards = FALSE
     end
   end
 
 
   def minister_cleared_filter(pq)
-    # not (with_minister || minister_query)
-    # if !(pq.is_in_progress?(Progress.with_minister) || pq.is_in_progress?(Progress.ministerial_query))
-    #   return
-    # end
 
     # does not have policy minister & cleared by minister
     # Or has been cleared policy minister and not cleared by answering minister
@@ -142,9 +113,6 @@ class PQProgressChangerService
 
 
   def answered_filter(pq)
-    # if !pq.is_in_progress?(Progress.minister_cleared)
-    #   return
-    # end
 
     if !pq.pq_withdrawn.nil?
       @new_progress =   Progress.answered
