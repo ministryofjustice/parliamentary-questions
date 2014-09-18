43628157876a345626a7289ed2b99dc830d19151
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-09-18 14:56:01 +0100

    removed original path to whitespace validator

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,6 @@ module ParliamentaryQuestions
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/scrapers)
-    config.autoload_paths += %W(#{Rails.root.join('lib')})
 
     config.encoding = 'utf-8'
 
