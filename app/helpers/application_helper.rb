23b60d9133aee68060137a3e19ccf99c241dea68
ColinBruce <colin.bruce@digital.justice.gov.uk>
2014-09-23 14:07:57 +0100

    first pass at creating w3c compliant gds checkbox

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,29 @@
 module ApplicationHelper
+  class ActionView::Helpers::FormBuilder
+    # http://stackoverflow.com/a/2625727/1935918
+    include ActionView::Helpers::TagHelper
+    include ActionView::Helpers::FormTagHelper
+    include ActionView::Helpers::FormOptionsHelper
+    include ActionView::Helpers::CaptureHelper
+    include ActionView::Helpers::AssetTagHelper
+    def gds_check_box(attribute_name, *args)
+      @template.content_tag 'div', class:'form-group wibble' do
+
+
+        @template.content_tag 'label', 'text', class: 'wibblelabel', for: 'a_control' do end
+
+
+        @template.content_tag 'label', class: 'block-label selected', for: "#{@object_name}[#{attribute_name}]" do
+          # options = args.extract_options!
+          # options[:include_hidden] = false
+          # args << options
+          @template.hidden_field_tag @object_name, attribute_name , value: '0'
+          check_box_tag("#{@object_name}[#{attribute_name}]") + @object.class.human_attribute_name(attribute_name)
+        end
+      end
+    end
+  end
+
   def flash_class_for flash_type
     case flash_type
       when :success
