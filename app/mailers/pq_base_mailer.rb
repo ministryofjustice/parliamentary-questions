9eb67bd6bea67a0ca43805b208ce315c339fcc22
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-09-24 11:59:07 +0100

    Try to add LogStuff to base mailer

--- app/mailers/pq_base_mailer.rb
+++ app/mailers/pq_base_mailer.rb
@@ -6,25 +6,25 @@ class PQBaseMailer < ActionMailer::Base
     m = super
 
     class << m
-      attr_accessor :logger
 
       # Re-open the message object and wrap the deliver method since that is when the actual error is thrown.
-      def deliver( )
+      def deliver
         $statsd.time(StatsHelper::MAIL_TIMING) do 
-          begin
-            LogStuff.info "Sending mail with subject '#{self[:subject]}'"
-            super
-            $statsd.increment StatsHelper::MAIL_SUCCESS
-          rescue => e
-            logger.warn "Problem sending mail with subject '#{self[:subject]}': #{e.inspect}"
-            $statsd.increment StatsHelper::MAIL_FAILURE
-            raise e
+          LogStuff.tag(:mail) do 
+            begin
+              LogStuff.info "Sending mail with subject '#{self[:subject]}'"
+              super
+              $statsd.increment StatsHelper::MAIL_SUCCESS
+            rescue => e
+              LogStuff.warn "Problem sending mail with subject '#{self[:subject]}': #{e.inspect}"
+              $statsd.increment StatsHelper::MAIL_FAILURE
+              raise e
+            end
           end
         end
       end
     end
 
-    m.logger = logger
     m
   end
 end
