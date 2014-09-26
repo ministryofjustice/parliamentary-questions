8e0b46ff0a3fbf2c18c23ddb2c277c48a6eab2fb
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-09-26 11:38:07 +0100

    Updated logging configuration for docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,10 +52,13 @@ ParliamentaryQuestions::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Logstasher configuration
+  config.log_level = :info
   config.logstasher.enabled = true
   config.logstasher.suppress_app_log = false
+  config.logstasher.log_level = Logger::INFO
+  config.logstasher.logger_path = "#{Rails.root}/log/logstash_#{Rails.env}.json"
   # This line is optional, it allows you to set a custom value for the @source field of the log event
-  # config.logstasher.source = 'your.arbitrary.source'
+  config.logstasher.source = 'logstasher'
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   if ENV['ASSET_HOST']
