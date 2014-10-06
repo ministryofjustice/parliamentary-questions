22df75add85181d64db417d6cdc11fff15933186
Jakub Novotny <novotny.jakub@gmail.com>
2014-10-06 12:36:09 +0100

    Remove unused controller actions for dates
    The functionality has now been merged to one 'commission' form. Date rules have been moved to the Pq model

--- config/routes.rb
+++ config/routes.rb
@@ -98,9 +98,6 @@ ParliamentaryQuestions::Application.routes.draw do
   get 'send_accept_reject_reminder/:id' => 'action_officer_reminder#accept_reject'
   get 'send_draft_reminder/:id' => 'action_officer_reminder#send_draft'
 
-  patch 'set_internal_deadline/:id' => 'pqs#set_internal_deadline'
-  patch 'set_date_for_answer/:id' => 'pqs#set_date_for_answer'
-
   match 'export/pq.csv' => 'export#csv', via: [:get, :post]
   get 'export' => 'export#index'
 
