f944144209b24e9db15b6f7238d9aa24f8107d7a
Jakub Novotny <novotny.jakub@gmail.com>
2014-10-08 15:00:21 +0100

    Seed progress data before suite, rather than using factories
    This also seem to speed up the whole test suite for about 30%

--- spec/support/progress_seed.rb
+++ spec/support/progress_seed.rb
@@ -1,15 +1,18 @@
 def progress_seed
-  let!(:unassigned) { create(:progress, name: Progress.UNASSIGNED) }
-  let!(:accepted) { create(:progress, name: Progress.ACCEPTED) }
-  let!(:no_response) { create(:progress, name: Progress.NO_RESPONSE) }
-  let!(:rejected) { create(:progress, name: Progress.REJECTED) }
-  let!(:draft_pending) { create(:progress, name: Progress.DRAFT_PENDING) }
-  let!(:with_pod) { create(:progress, name: Progress.WITH_POD) }
-  let!(:pod_query) { create(:progress, name: Progress.POD_QUERY) }
-  let!(:pod_cleared) { create(:progress, name: Progress.POD_CLEARED) }
-  let!(:with_minister) { create(:progress, name: Progress.WITH_MINISTER) }
-  let!(:ministerial_query) { create(:progress, name: Progress.MINISTERIAL_QUERY) }
-  let!(:minister_cleared) { create(:progress, name: Progress.MINISTER_CLEARED) }
-  let!(:answered) { create(:progress, name: Progress.ANSWERED) }
-  let!(:transferred_out) { create(:progress, name: Progress.TRANSFERRED_OUT) }
+  Progress.create(
+      [
+          {name: Progress.UNASSIGNED},
+          {name: Progress.NO_RESPONSE},
+          {name: Progress.ACCEPTED},
+          {name: Progress.REJECTED},
+          {name: Progress.DRAFT_PENDING},
+          {name: Progress.WITH_POD},
+          {name: Progress.POD_QUERY},
+          {name: Progress.POD_CLEARED},
+          {name: Progress.WITH_MINISTER},
+          {name: Progress.MINISTERIAL_QUERY},
+          {name: Progress.MINISTER_CLEARED},
+          {name: Progress.ANSWERED},
+          {name: Progress.TRANSFERRED_OUT}
+      ])
 end
