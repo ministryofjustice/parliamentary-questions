3afdb67e5319170117d8bbe05eecefa2a4bc0512
Jakub Novotny <novotny.jakub@gmail.com>
2014-10-10 16:59:04 +0100

    Improve the ministerSelect to comply to jQuery naming convention

--- app/assets/javascripts/ministerSelect.js
+++ app/assets/javascripts/ministerSelect.js
@@ -8,42 +8,48 @@
         }, options);
 
         return this.each(function () {
-            var dropdown = $(this);
-            var input = $('<input type="text" value="" class="' + settings['inputClass'] + '" />');
-            var lookup = [];
+            var $select = $(this),
+                $input = $('<input type="text" value="" class="' + settings['inputClass'] + '" />'),
+                lookup = [];
 
-            dropdown.find('option').each(function () {
-                var option = $(this);
+            // compile the list of auto suggestions from the select and pre-fill the input field
+            $select.find('option').each(function () {
+                var $option = $(this);
 
-                lookup.push(option.text());
-                if (option.is(':selected')) {
-                    input.val(option.text());
+                lookup.push($option.text());
+                if ($option.is(':selected')) {
+                    $input.val($option.text());
                 }
             });
 
-            input.autocomplete({
+            // construct the auto complete and only trigger change method on the input when user selects an option
+            $input.autocomplete({
                 lookup: lookup,
                 onSelect: function () {
                     $(this).trigger('change');
                 }
-            }).on('change', function () {
-                var text = $(this).val();
-                var options = dropdown.find('option').filter(function () {
-                    return $(this).text().toLowerCase().indexOf(text.toLowerCase()) !== -1;
-                });
-                if (options.length == 1) {
-                    var option = $(options[0]);
+            });
 
-                    dropdown.val(option.val());
-                    $(this).val(option.text());
+            // when the input changes, the relevant select option has to be chosen
+            $input.on('change', function () {
+                var text = $(this).val(),
+                    options = $select.find('option').filter(function () {
+                        return $(this).text().toLowerCase().indexOf(text.toLowerCase()) !== -1;
+                    });
+
+                if (options.length == 1) {
+                    var $option = $(options[0]);
+
+                    $select.val($option.val());
+                    $input.val($option.text());
                 } else {
-                    dropdown.val('');
-                    $(this).val('');
+                    $select.val('');
+                    $input.val('');
                 }
             });
 
-            dropdown.hide();
-            dropdown.before(input);
+            $select.hide();
+            $select.before($input);
         });
     }
 }(jQuery));
