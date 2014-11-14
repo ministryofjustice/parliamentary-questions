c59849e6eb3e717a0a06f27e218fb5795380f2b6
Phil Thompson <phil.thompson@digital.justice.gov.uk>
2014-11-14 08:16:15 +0000

    let webmock allow codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 Capybara.javascript_driver = :webkit
 Capybara.current_driver = :rack_test
 
+WebMock.disable_net_connect!(allow: "codeclimate.com")
 
 RSpec.configure do |config|
   # ## Mock Framework
