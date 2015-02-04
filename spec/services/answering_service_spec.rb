77d7a5b123cc73fd2c538c4a6d611384f98155de
Andrea Fiore <andrea.fiore@composable.io>
2015-02-04 11:35:44 +0000

    delete obsolete code and wire up new import logic to background worker

--- spec/services/answering_service_spec.rb
+++ spec/services/answering_service_spec.rb
@@ -1,66 +1,62 @@
 require 'spec_helper'
 
 describe 'AnsweringService' do
-  let!(:minister1) { create(:minister, member_id: 1) }
   let(:uin) { '183366' }
 
-  before(:each) do
-    @http_client = double('QuestionsHttpClient')
-    allow(@http_client).to receive(:answer) { {content: sample_answer, status: 200} }
-    allow(@http_client).to receive(:question) { sample_question_for_answer }
+  let(:minister) { double('minister', member_id: 1) }
 
-    @questions_service = QuestionsService.new(@http_client)
-    @answering_service = AnsweringService.new(@questions_service)
-    @import_service = ImportService.new(@questions_service)
+  let(:preview_url) {
+    'http://example.com/preview/url'
+  }
+
+  let(:answer_response) {
+    double('answer-response', preview_url: preview_url )
+  }
+
+  let(:pqa_service) {
+    srv = double('pqa-service')
+    allow(srv).to receive(:answer).with(uin, 1, 'Hello test', true) {
+      answer_response
+    }
+    srv
+  }
+
+  let(:pq) {
+    double('pq-stub', uin: uin, minister: minister)
+  }
+
+  before(:each) do
+    allow(PQAService).to receive(:from_settings) {
+      pqa_service
+    }
   end
 
   describe '#answer' do
-    it 'should insert the answer url in the pq' do
-      @import_service.questions_by_uin(uin)
+    it 'should insert the answer url in the pq table' do
+      service         = AnsweringService.new
+      expect(pq).to receive(:update).with(preview_url: preview_url)
 
-      pq = Pq.find_by(uin: uin)
-      pq.minister = minister1
-
-      @answering_service.answer(pq, {text: 'Hello test', is_holding_answer: true})
-
-      pq = Pq.find_by(uin: uin)
-      expect(pq.preview_url).to eq('https://wqatest.parliament.uk/Questions/Details/36527')
+      service.answer(pq, text: 'Hello test', is_holding_answer: true)
     end
 
-    it 'should raise an exception if the minister is nil' do
-      @import_service.questions_by_uin(uin)
+    context "when minister is nil" do
+      let(:minister) { nil }
 
-      pq = Pq.find_by(uin: uin)
-
-      expect {
-        @answering_service.answer(pq, {text: 'Hello test', is_holding_answer: true})
-      }.to raise_error 'Replying minister is not selected for the question'
+      it 'should raise an error' do
+        expect {
+          AnsweringService.new.answer(pq, text: 'Hello test', is_holding_answer: true)
+        }.to raise_error(RuntimeError, /minister is not selected/i)
+      end
     end
 
-    it 'should raise an exception if the member id is nil' do
-      @import_service.questions_by_uin(uin)
+    context "when member_id is nil" do
+      let(:minister) { double('minister', member_id: nil) }
 
-      pq = Pq.find_by(uin: uin)
-      pq.minister = minister1
-      pq.minister.member_id = nil
-
-      expect {
-        @answering_service.answer(pq, {text: 'Hello test', is_holding_answer: true})
-      }.to raise_error 'Replying minister has not member id, please update the member id of the minister'
-    end
-
-
-    it 'should raise an exception you have a error in questions_service' do
-      allow(@http_client).to receive(:answer) { {content: sample_answer_error, status: 403} }
-
-      @import_service.questions_by_uin(uin)
-
-      pq = Pq.find_by(uin: uin)
-      pq.minister = minister1
-
-      expect {
-        @answering_service.answer(pq, {text: 'Hello test', is_holding_answer: true})
-      }.to raise_error 'Validation failed on the request.'
+      it 'should raise an error' do
+        expect {
+          AnsweringService.new.answer(pq, text: 'Hello test', is_holding_answer: true)
+        }.to raise_error(RuntimeError, /minister is not selected/i)
+      end
     end
   end
 end
