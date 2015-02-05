27778bd15f9297143dc071e0b47c8c51d366e477
Andrea Fiore <andrea.fiore@composable.io>
2015-02-05 15:00:35 +0000

    WIP: add capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,21 +6,23 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
 require 'capybara/rspec'
+require 'capybara/poltergeist'
 require 'capybara/email/rspec'
-require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-Capybara.javascript_driver = :webkit
-Capybara.current_driver = :rack_test
+Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, {
+      phantomjs_logger: File.new('/dev/null', 'a')
+    })
+end
 
-WebMock.disable_net_connect!(allow: ["codeclimate.com", "localhost"])
+Capybara.javascript_driver = :poltergeist
 
 RSpec.configure do |config|
   mock_api_runner = PQA::MockApiServerRunner.new
-
 
   # ## Mock Framework
   #
