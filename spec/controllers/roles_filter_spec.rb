228c86693aeeb1d071a34e856734cb38d0d397b0
Andrea Fiore <andrea.fiore@composable.io>
2015-02-05 15:00:35 +0000

    WIP: add capybara

--- spec/controllers/roles_filter_spec.rb
+++ spec/controllers/roles_filter_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe 'roles filer' do
 
   let!(:pq_user) { create(:user, name: 'pquser', email:'pq@admin.com', password: 'password123') }
-  let!(:finance_user) { create(:user, name: 'finance', email:'f@admin.com', password: 'password123', roles: User.ROLE_FINANCE) }
+  let!(:finance_user) { create(:user, name: 'finance', email:'f@admin.com', password: 'password123', roles: User::ROLE_FINANCE) }
   let!(:fake_role_user) { create(:user, name: 'fake', email:'m@admin.com', password: 'password123', roles: 'BAD') }
 
   before(:each) do
--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 feature 'Visit the dashboard an show the questions for the day' do
-  scenario "can view the questions tabled for today" do
+  scenario "can view the questions tabled for today", js: true do
+    require 'pry'; binding.pry
     pq = create(:pq)
-    sign_in
     visit '/dashboard'
     expect(page).to have_content(pq.uin)
   end
