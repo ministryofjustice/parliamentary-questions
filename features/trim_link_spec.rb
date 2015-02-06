c7f437ae79697e7ea602d3a82ee7c0d7ca6acdf4
Andrea Fiore <andrea.fiore@composable.io>
2015-02-06 18:13:33 +0000

    WIP: add another scenario

--- features/trim_link_spec.rb
+++ features/trim_link_spec.rb
@@ -52,14 +52,14 @@ feature 'I want to manage trim links', js: true do
     expect(page).to have_link 'Open trim link'
   end
 
-  #scenario 'invalid trim link retains other changed fields and previous trim link' do
-  #  visit "/pqs/#{@pq_with_trim.uin}"
-  #  fill_in 'Date for answer back to Parliament', with: '01/01/2001'
-  #  click_link 'Trim link'
-  #  attach_file('pq[trim_link_attributes][file]', Rails.root.join('spec/fixtures/invalid_trimlink.tr5'))
-  #  click_button 'Save'
-
-  #  expect(page).to have_content 'Trim link data file empty'
-  #  expect(page).to have_field 'Date for answer back to Parliament', with: '01/01/2001'
-  #end
+  scenario 'invalid trim link retains other changed fields and previous trim link' do
+    visit "/pqs/#{@pq.uin}"
+    add_trim_link
+    fill_in 'Date for answer back to Parliament', with: '01/01/2001'
+    click_link 'Trim link'
+    attach_file('pq[trim_link_attributes][file]', Rails.root.join('spec/fixtures/invalid_trimlink.tr5'))
+    click_button 'Save'
+    expect(page).to have_content 'Trim link data file empty'
+    expect(page).to have_field 'Date for answer back to Parliament', with: '01/01/2001'
+  end
 end
