9d9805558e1e819ff1e36c235e99fec060fdd18d
Andrea Fiore <andrea.fiore@composable.io>
2015-02-09 15:25:39 +0000

    Get rid of obsolete gems

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,14 +4,6 @@
 # http://en.wikipedia.org/wiki/Cron
 # Learn more: http://github.com/javan/whenever
 
-
-# run on the root of the app to get the line that you have to put in the crontab
-# $ whenever
-
-#every 1.day, :at => '7:00 am' do
-#  runner 'ImportWorker.perform_async'
-#end
-
 set :output, 'log/schedule.log'
 job_type :runner,  "cd :path && bundle exec rails runner -e :environment ':task' :output"
 
