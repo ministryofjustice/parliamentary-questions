9b349788c49f70e955f9a1014e883af1421b2a51
mel pierre <mel.pierre@digital.justice.gov.uk>
2015-02-10 15:57:50 +0000

    added comments

--- features/finance_officer_spec.rb
+++ features/finance_officer_spec.rb
@@ -1,10 +1,22 @@
 require 'feature_helper'
 
+# Senario 1
+# pq user invites a new user as a finance officer in the PQ tool 
+
+# Senario 2 
+# user recieve an email and clicks on the accept the invitation link and sets their password, and see the list of questions
+
+# Senario 3
+# as a finance office i need to be able to log in and see the list of questions
+
+# Senario 4
 feature "As a finance officer, I want to be able to register interest in a question", js: true do
   before do
     @pqs = PQA::QuestionLoader.new.load_and_import(3)
     create_finance_session
   end
+
+# These tests are actioned on the following URL //finance/questions
 
   scenario "Finance officer tries to access the dashboard page" do
     visit '/dashboard'
@@ -23,3 +35,6 @@ feature "As a finance officer, I want to be able to register interest in a quest
     end
   end
 end
+
+# Senario 5
+# Finance officer I can sign out successfully of the PQ tool
