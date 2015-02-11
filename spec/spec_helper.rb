9e2efb7a921533a4468ae616bccceeb3ecc75aee
Andrea Fiore <andrea.fiore@composable.io>
2015-02-11 16:52:45 +0000

    minor tweak to fixture loading

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,20 +52,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
-    Progress.create!([
-      {name: Progress.UNASSIGNED},
-      {name: Progress.NO_RESPONSE},
-      {name: Progress.REJECTED},
-      {name: Progress.DRAFT_PENDING},
-      {name: Progress.WITH_POD},
-      {name: Progress.POD_QUERY},
-      {name: Progress.POD_CLEARED},
-      {name: Progress.WITH_MINISTER},
-      {name: Progress.MINISTERIAL_QUERY},
-      {name: Progress.MINISTER_CLEARED},
-      {name: Progress.ANSWERED},
-      {name: Progress.TRANSFERRED_OUT}
-    ])
+    DBHelpers.load_spec_seeds
   end
 
   config.before(:each) do
