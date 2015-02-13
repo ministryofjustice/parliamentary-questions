8ea301f6e25cebe32423efc69888c90c6a3995c3
Andrea Fiore <andrea.fiore@composable.io>
2015-02-13 16:05:00 +0000

    WIP: refactor commissioning service/controller

--- app/controllers/commission_controller.rb
+++ app/controllers/commission_controller.rb
@@ -2,28 +2,18 @@ class CommissionController < ApplicationController
   before_action :authenticate_user!, PQUserFilter
 
   def commission
-    form = CommissionForm.new(commission_form_params)
-
-    if form.valid?
-      pq = Pq.find(form.pq_id)
-      pq.minister_id = form.minister_id
-      pq.policy_minister_id = form.policy_minister_id
-      pq.date_for_answer = form.date_for_answer
-      pq.internal_deadline = form.internal_deadline
-      pq.save
-
-      begin
-        form.action_officer_id.each do |action_officer_id|
-          assign_one_action_officer(pq.id, action_officer_id)
-        end
-
-        render :partial => 'shared/question_assigned', :locals => {question: pq}
-      rescue Exception => e
-        render text: "Error when commissioning: #{e}", status: 400
+    form       = CommissionForm.new(commission_form_params)
+    pq, status =
+      if form.valid?
+        [CommissioningService.new.commission(form), 200]
+      else
+        flash.now[:error] = "Error in commissioning question"
+        [Pq.find_by(uin: params[:id]), 422]
       end
-    else
-      render text: form.errors.values.flatten.join('\n'), status: 422
-    end
+
+    render(partial: 'shared/question_assigned',
+           locals: { question: pq },
+           status: status)
   end
 
   def complete
@@ -31,22 +21,12 @@ class CommissionController < ApplicationController
     render :partial => 'shared/commissioned', :locals => {uin: @pq}
   end
 
-private
-
-  def assign_one_action_officer(pq_id, ao_id)
-    assignment = ActionOfficersPq.new(pq_id: pq_id, action_officer_id: ao_id)
-    comm_service = CommissioningService.new
-    result = comm_service.commission(assignment)
-    if result.nil?
-      flash.now[:error] = "Error in commissioning to #{assignment.action_officer.name}"
-      return render :partial => 'shared/question_assigned', :locals => {question: @pq}
-    else
-      comm_service.notify_dd(assignment)
-    end
-  end
+  private
 
   def commission_form_params
-    params.require(:commission_form).permit(
-      :pq_id, :minister_id, :policy_minister_id, {action_officer_id: []}, :date_for_answer, :internal_deadline)
+    params.require(:commission_form)
+      .permit(:pq_id, :minister_id, :policy_minister_id,
+             { action_officer_id: []},
+             :date_for_answer, :internal_deadline)
   end
 end
--- spec/services/commissioning_service_spec.rb
+++ spec/services/commissioning_service_spec.rb
@@ -1,136 +1,101 @@
 require 'spec_helper'
 
 describe CommissioningService do
-  let(:minister) {build(:minister)}
-  let(:deputy_director) { create(:deputy_director, name: 'dd name', email: 'dd@dd.gov', id: 1+rand(10))}
-  let(:action_officer) { create(:action_officer, name: 'ao name 1', email: 'ao@ao.gov', deputy_director_id: deputy_director.id) }
-  let(:pq) { create(:pq, uin: 'HL789', question: 'test question?', member_name: 'Henry Higgins', internal_deadline:'01/01/2014 10:30', minister:minister, house_name:'commons' ) }
+  let(:pq) {
+    DBHelpers.pqs.first
+  }
 
-  let(:deputy_director2) { create(:deputy_director, name: 'dd name', email: '', id: 1+rand(10))}
-  let(:action_officer2) { create(:action_officer, name: 'ao name 1', email: 'ao@ao.gov', deputy_director_id: deputy_director2.id) }
-  let(:assignment) { create(:action_officers_pq, action_officer: action_officer, pq: pq) }
+  let(:minister) {
+    DBHelpers.ministers[0]
+  }
+  let(:policy_minister) {
+    DBHelpers.ministers[1]
+  }
 
-  before(:each) do
-    ActionMailer::Base.deliveries = []
-  end
+  let(:ao1) {
+    DBHelpers.action_officers[0]
+  }
 
-  it 'updates progress' do
-    progress_service = double
-    expect(progress_service).to receive(:update_progress).with(pq)
-    expect(PQProgressChangerService).to receive(:new).and_return progress_service
+  let(:ao2) {
+    DBHelpers.action_officers[1]
+  }
 
-    subject.commission(assignment)
-  end
+  let(:form_params) {{
+    pq_id: pq.id,
+    minister_id: minister.id,
+    policy_minister_id: policy_minister.id,
+    action_officer_id: [ao1.id, ao2.id],
+    date_for_answer: Date.tomorrow,
+    internal_deadline: Date.today
+  }}
 
-  it 'should return the assignment id and inserts the data' do
-    result = subject.commission(assignment)
+  let(:invalid_form_params) {
+    form_params.merge(date_for_answer: nil)
+  }
 
-    expect(result).to_not be nil
-    expect(result[:assignment_id]).to_not be nil
+  let(:form) {
+    CommissionForm.new(form_params)
+  }
 
-    expect(ActionOfficersPq.where(action_officer_id: action_officer.id, pq_id: pq.id).first).to_not be nil
-  end
-
-  it 'should have generated a valid token' do
-    result = subject.commission(assignment)
-
-    token = Token.where(entity: "assignment:#{result[:assignment_id]}", path: '/assignment/HL789').first
-
-    expect(token).to_not be nil
-    expect(token.id).to_not be nil
-    expect(token.token_digest).to_not be nil
-
-    token_expires = DateTime.now.midnight.change({:offset => 0}) + 3.days
-    expect(token.expire).to eq(token_expires)
-  end
-
-  it 'should send an email with the right data' do
-    result = subject.commission(assignment)
-    sentToken = result[:token]
-
-    mail = ActionMailer::Base.deliveries.first
-
-    token_param = {token: sentToken}.to_query
-    entity = {entity: "assignment:#{result[:assignment_id]}"}.to_query
-    url = "/assignment/HL789"
-
-    expect(mail.html_part.body).to include pq.question
-    expect(mail.html_part.body).to include action_officer.name
-    expect(mail.html_part.body).to include url
-    expect(mail.html_part.body).to include token_param
-    expect(mail.html_part.body).to include entity
-
-    expect(mail.text_part.body).to include pq.question
-    expect(mail.text_part.body).to include action_officer.name
-    expect(mail.text_part.body).to include url
-    expect(mail.text_part.body).to include token_param
-    expect(mail.text_part.body).to include entity
-
-    expect(mail.to).to include action_officer.email
-  end
+  let(:invalid_form) {
+    CommissionForm.new(invalid_form_params)
+  }
 
 
-  it 'should set the progress to awaiting response' do
-    result = subject.commission(assignment)
+  describe "#commission" do
+    context "when the supplied form data is not valid" do
+      it "raises an error" do
+        expect {
+          invalid_form = CommissionForm.new(invalid_form_params)
+          CommissioningService.new.commission(invalid_form)
+        }.to raise_error(ArgumentError)
+      end
+    end
 
-    expect(result).to_not be nil
+    context "when the supplied data is valid" do
+      before do
+        valid_form = CommissionForm.new(form_params)
+        @pq = CommissioningService.new.commission(valid_form)
+      end
 
-    assignment_id = result[:assignment_id]
-    expect(assignment_id).to_not be nil
+      it "returns an updated PQ" do
+        expect(@pq).to be_valid
+        expect(@pq.minister).to eq(minister)
+        expect(@pq.policy_minister).to eq(policy_minister)
+        expect(@pq.internal_deadline).to eq(form.internal_deadline)
+        expect(@pq.date_for_answer).to eq(form.date_for_answer)
+      end
 
-    assignment = ActionOfficersPq.find(assignment_id)
+      it "sets the pqs' action officers" do
+        expect(@pq.action_officers).to eq([
+          ao1,
+          ao2
+        ])
+      end
 
-    pq = Pq.find(assignment.pq_id)
-    expect(pq.progress.name).to  eq(Progress.NO_RESPONSE)
-  end
+      it "notifies the action officers" do
+        ao1_mail, _,  ao2_mail, _ = ActionMailer::Base.deliveries
 
-  it 'should raise an error if the action_officer.id is null' do
-    assignment = ActionOfficersPq.new(pq_id: pq.id)
-    expect {
-      subject.commission(assignment)
-    }.to raise_error 'Action Officer is not selected'
-  end
+        expect(ao1_mail.to).to eq([ao1.email])
+        expect(ao1_mail.subject).to match(/you have been allocated PQ #{pq.uin}/i)
 
-  it 'should raise an error if the pq_id is null' do
-    assignment = ActionOfficersPq.new(action_officer_id: action_officer.id)
-    expect {
-      subject.commission(assignment)
-    }.to raise_error 'Question is not selected'
-  end
+        expect(ao2_mail.to).to eq([ao2.email])
+        expect(ao2_mail.subject).to match(/you have been allocated PQ #{pq.uin}/i)
+      end
 
-  it 'should send an email to the deputy director the right data' do
-    result = subject.notify_dd(assignment)
-    mail = ActionMailer::Base.deliveries.first
+      it "notifies the deputy director" do
+        _, dd1_mail, _, dd2_mail = ActionMailer::Base.deliveries
 
-    expect(mail.html_part.body).to include pq.uin
-    expect(mail.html_part.body).to include pq.question
-    expect(mail.html_part.body).to include pq.member_name
-    expect(mail.html_part.body).to include pq.internal_deadline.to_s(:date)
-    expect(mail.html_part.body).to include action_officer.name
-    expect(mail.html_part.body).to include deputy_director.name
+        expect(dd1_mail.to).to eq([ao1.deputy_director.email])
+        expect(dd1_mail.subject).to match(/#{ao1.name} has been allocated PQ #{pq.uin}/i)
 
-    expect(mail.text_part.body).to include pq.uin
-    expect(mail.text_part.body).to include pq.question
-    expect(mail.text_part.body).to include pq.member_name
-    expect(mail.text_part.body).to include pq.internal_deadline.to_s(:date)
-    expect(mail.text_part.body).to include action_officer.name
-    expect(mail.text_part.body).to include deputy_director.name
+        expect(dd2_mail.to).to eq([ao2.deputy_director.email])
+        expect(dd2_mail.subject).to match(/#{ao2.name} has been allocated PQ #{pq.uin}/i)
+      end
 
-    expect(mail.to).to include deputy_director.email
-  end
-
-  it 'should not send an email if the deputy director email does not exist' do
-    assignment = ActionOfficersPq.new(action_officer_id: action_officer2.id, pq_id: pq.id)
-    result = subject.notify_dd(assignment)
-    expect(result).to eq('Deputy Director has no email')
-  end
-
-  it 'should include "No deadline set" in email to DD if not set' do
-    new_pq = Pq.create(uin: 'HL999', question: 'test question?', raising_member_id: 0, member_name: 'Henry Higgins', internal_deadline: nil, minister:minister, house_name:'commons' )
-
-    assignment = ActionOfficersPq.new(action_officer_id: action_officer.id, pq_id: new_pq.id)
-    result = subject.notify_dd(assignment)
-    mail = ActionMailer::Base.deliveries.first
-    expect(mail.html_part.body).to include 'No deadline set'
+      it "sets the PQ state to 'no-response'" do
+        expect(@pq.progress).to eq(Progress.no_response)
+      end
+    end
   end
 end
