0598fc8c11677b9350616f432b1a211125d52cca
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-02-17 10:39:13 +0000

    refactored pq mailer

--- app/mailers/pq_mailer.rb
+++ app/mailers/pq_mailer.rb
@@ -13,29 +13,26 @@ class PqMailer < PQBaseMailer
   end
 
   def acceptance_email(pq, ao, urgent = false)
-
-    @template_params = build_primary_hash(pq, ao).merge({
-      date_to_parliament:    pq.date_for_answer.try(:to_s, :date),
-      finance_users_emails:  finance_users_emails(pq).join(';'),
-    })
-
     cc_list = [
-      ao.deputy_director && ao.deputy_director.email,
-      @template_params[:mpemail],
-      @template_params[:policy_mpemail],
-      @template_params[:press_email]
-    ].reject(&:blank?) + 
+      dd_email(ao),
+      mp_email(pq),
+      policy_mp_email(pq),
+      press_email(ao)
+    ] + 
     mp_office_emails(pq, 'Simon Hughes (MP)') + 
     action_list_emails +
     finance_users_emails(pq)
-        
-    @template_params[:cc_list] = cc_list.join(';')
 
+    @template_params = build_primary_hash(pq, ao).merge({
+      cc_list: cc_list.reject(&:blank?).join(';')
+    })
+        
     subject = if urgent
       "URGENT : please send your draft response for PQ #{@template_params[:uin]}"
     else
       "You have accepted PQ #{@template_params[:uin]}"
     end
+
     mail(to: @template_params[:email], subject: subject)
   end
 
@@ -58,20 +55,22 @@ class PqMailer < PQBaseMailer
 
 private
 
-  def build_primary_hash(pq,ao)
+  def build_primary_hash(pq, ao)
     {
-      :ao_name => ao.name,
-      :email => ao.emails,
-      :uin => pq.uin,
-      :question => pq.question,
-      :mpname => pq.minister && pq.minister.name,
-      :mpemail => pq.minister && pq.minister.email,
-      :policy_mpname => pq.policy_minister && pq.policy_minister.name,
-      :policy_mpemail => pq.policy_minister && pq.policy_minister.email,
-      :press_email => ao.press_desk && ao.press_desk.email_output,
-      :member_name => pq.member_name,
-      :house_name => pq.house_name,
-      :internal_deadline => pq.internal_deadline ? "#{pq.internal_deadline.to_s(:date) } - 10am " : ''
+      ao_name:              ao.name,
+      email:                ao.emails,
+      uin:                  pq.uin,
+      question:             pq.question,
+      mpname:               mp_name(pq),
+      mpemail:              mp_email(pq),
+      policy_mpname:        policy_mp_name(pq),
+      policy_mpemail:       policy_mp_email(pq),
+      press_email:          press_email(ao),
+      member_name:          pq.member_name,
+      house_name:           pq.house_name,
+      internal_deadline:    internal_deadline(pq),
+      date_to_parliament:   pq.date_for_answer.try(:to_s, :date),
+      finance_users_emails: finance_users_emails(pq).join(';'),
     }
   end
 
@@ -97,5 +96,33 @@ private
     else
       []
     end
+  end
+
+  def dd_email(ao)
+    ao.deputy_director && ao.deputy_director.email
+  end
+
+  def mp_name(pq)
+    pq.minister && pq.minister.name
+  end
+
+  def mp_email(pq)
+    pq.minister && pq.minister.email
+  end
+
+  def policy_mp_name(pq)
+    pq.policy_minister && pq.policy_minister.name
+  end
+
+  def policy_mp_email(pq)
+    pq.policy_minister && pq.policy_minister.email
+  end
+
+  def press_email(ao)
+    ao.press_desk && ao.press_desk.email_output
+  end
+
+  def internal_deadline(pq)
+    pq.internal_deadline ? "#{pq.internal_deadline.to_s(:date) } - 10am " : ''
   end
 end
