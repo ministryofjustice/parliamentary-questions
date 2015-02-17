1b65d7ea56665ebe4504a46e2055e404bb648645
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-02-17 11:38:15 +0000

    Finished single AO commissioning journey

--- features/commission_question_spec.rb
+++ features/commission_question_spec.rb
@@ -2,7 +2,7 @@ require 'feature_helper'
 
 feature 'Commissioning questions', js: true, suspend_cleaner: true do
   include Features::EmailHelpers
-  
+
   let(:ao)         { ActionOfficer.find_by(email: 'ao1@pq.com') }
   let(:minister)   { Minister.first                             }
 
@@ -29,12 +29,11 @@ feature 'Commissioning questions', js: true, suspend_cleaner: true do
     end
 
     click_on 'Commission'
+
     expect(page).to have_content("#{@pq.uin} commissioned successfully")
-    # Expect PQ status to change to draft pending
-    expect(false).to be true
   end
 
-  scenario 'AO and DD receive an email notification of assigned question' do
+  scenario 'AO and DD should receive an email notification of assigned question' do
     ao_mail, dd_mail = sent_mail.last(2)
     
     expect(ao_mail.to).to include ao.email
@@ -44,7 +43,7 @@ feature 'Commissioning questions', js: true, suspend_cleaner: true do
     expect(dd_mail.text_part.body).to include "#{ao.name} has been allocated the PQ #{@pq.uin}"
   end
 
-  scenario 'Following the link lets the AO accept the question' do
+  scenario 'Following the link should let the AO accept the question' do
     url = extract_url_like('/assignment', sent_mail.first)
     visit url
     choose 'Accept'
@@ -54,20 +53,24 @@ feature 'Commissioning questions', js: true, suspend_cleaner: true do
     expect(page).to have_content("PQ #{@pq.uin}")
   end
 
-  scenario 'The AO and DD receive an email notification confirming the question acceptance' do
-    ao_mail, dd_mail = sent_mail.last(2)
-    binding.pry
+  scenario 'The PQ status should then change to draft pending' do
+    create_pq_session
+    visit dashboard_path
+    click_on 'In progress'
+    click_on 'Draft Pending'
+
+    expect(page).to have_content("#{@pq.uin}")
+  end
+
+  scenario 'The AO should receive an email notification confirming the question acceptance' do
+    ao_mail = sent_mail.last
+
     expect(ao_mail.to).to include ao.email
     expect(ao_mail.text_part.body).to include 
       "You have accepted responsibility for drafting an answer to PQ #{@pq.uin}"
-
-    expect(dd_mail).to include ao.deputy_director.email
-    expect(dd_mail.text_part.body).to include 
   end
 
-  scenario 'If another AO tries to access the accepted question, it shows that it has already been accepted'
-
-  scenario 'Following the link after 3 days have passed shows error page' do
+  scenario 'Following the link after 3 days have passed should show an error page' do
     form_params = {
       pq_id: Pq.last.id,
       minister_id: minister.id,
@@ -77,7 +80,6 @@ feature 'Commissioning questions', js: true, suspend_cleaner: true do
     }
     form = CommissionForm.new(form_params) 
     CommissioningService.new(nil, Date.today - 4.days).commission(form)
-    # Retrieve the AO email, ignore the DD email
     ao_mail, _ = sent_mail.last(2)
     url = extract_url_like('/assignment', ao_mail)
     visit url
