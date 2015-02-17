67815ce2317c14f2202cb8828ec57e79799ac1e5
Andrea Fiore <andrea.fiore@composable.io>
2015-02-17 16:52:45 +0000

    WIP: refactor assignment controller

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -1,61 +1,52 @@
 class AssignmentController < ApplicationController
   before_action AOTokenFilter
-  before_action :set_data
-  before_action :load_service
 
-  def index
-    @question = Pq.find_by(uin: params[:uin])
-    if @question.action_officers_pqs.accepted.present? || @assignment.rejected?
-      return render 'confirmation'
+  def show
+    loading_question_and_assignment do
+      if @question.action_officers_pqs.accepted || @assignment.rejected?
+        render 'confirmation'
+      else
+        @response = AllocationResponse.new
+      end
     end
-    @response = AllocationResponse.new()
   end
 
-  def action
-    @response = AllocationResponse.new(response_params)
-
-    if !@response.valid?
-      return render 'index'
+  def create
+    loading_question_and_assignment do
+      @response = AllocationResponse.new(response_params)
+      unless @response.valid?
+        render 'show'
+      else
+        service = AssignmentService.new
+        case @response.response_action
+        when 'accept'
+          service.accept(@assignment)
+        when 'reject'
+          service.reject(@assignment)
+        else
+          #TODO: log unexpected input
+        end
+        render 'confirmation'
+      end
     end
-
-    response_action = @response.response_action
-    if response_action == 'accept'
-      @assignment_service.accept(@assignment)
-    end
-
-    if response_action == 'reject'
-      @assignment_service.reject(@assignment, @response)
-    end
-
-    @question = Pq.find_by(uin: params[:uin])
-    render 'confirmation'
   end
 
-private
+  private
 
-  def set_data
-    entity = params[:entity].split(':')
-    assignment_id = entity[1]
-
-    if assignment_id.nil?
-      return render :file => 'public/401.html', :status => :unauthorized
-    end
-
-    @assignment = ActionOfficersPq.find(assignment_id)
-
-    if @assignment.nil?
-      return render :file => 'public/401.html', :status => :unauthorized
-    end
-
+  def loading_question_and_assignment
+    _, assignment_id = params[:entity].split(':')
     @question = Pq.find_by(uin: params[:uin])
-    @ao = @assignment.action_officer
+
+    if assignment_id
+      @assignment = @question.action_officers_pqs.find(assignment_id)
+      @ao         = @assignment.action_officer
+      yield
+    else
+      render :file => 'public/404.html', :status => :not_found
+    end
   end
 
   def response_params
     params.require(:allocation_response).permit(:response_action, :reason_option, :reason)
-  end
-
-  def load_service(service = AssignmentService.new)
-    @assignment_service ||= service
   end
 end
--- app/models/allocation_response.rb
+++ app/models/allocation_response.rb
@@ -12,7 +12,7 @@ class AllocationResponse
 
   def initialize(attributes = {})
     attributes.each do |name, value|
-      send("#{name}=", value)
+      public_send("#{name}=", value)
     end
   end
 
