f661c5ab1402a3df98a799bf68df330be77b9549
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-02-18 17:04:55 +0000

    show monday first, and don't fix date on calendar close

--- app/assets/javascripts/pq.js
+++ app/assets/javascripts/pq.js
@@ -56,11 +56,15 @@ var changeBadgeBy = function(id_of_navpill, val) {
 };
 
 $(document).ready(function () {
-  $('.datetimepicker input').datetimepicker({closeOnDateSelect:true,
+  $('.datetimepicker input').datetimepicker({validateOnBlur:false,
+                                             closeOnDateSelect:true,
+                                             dayOfWeekStart: 1,
                                              format:'d/m/Y     H:i'});
 
   $('.datepicker input').datetimepicker({timepicker: false,
+                                         validateOnBlur:false,
                                          closeOnDateSelect:true,
+                                         dayOfWeekStart: 1,
                                          format:'d/m/Y'});
 
   $('.minister-select').select2({width:'250px'});
