0cea7724900f114aea33a8722569af7ca38f31d1
Andrea Fiore <andrea.fiore@composable.io>
2015-02-20 17:01:10 +0000

    Rework trim link validation and try to add e2e tests

--- app/controllers/trim_links_controller.rb
+++ app/controllers/trim_links_controller.rb
@@ -2,42 +2,47 @@ class TrimLinksController < ApplicationController
   before_action :authenticate_user!, PQUserFilter
 
   def create
-    result = {
-        :message => '',
-        :status => 'failure',
-        :link => ''
-    }
-    if trim_link_params[:file_data].nil?
-      flash[:error] = 'Please select a trim file (.tr5) before trying to add'
-    else
-      uploaded_io = trim_link_params[:file_data]
-      filename = uploaded_io.original_filename
-      if ['.tr5'].include? File.extname(filename)
-        data = uploaded_io.read
-        @trim_link = TrimLink.new(:data => data, :filename => filename, :size => data.size, :pq_id => trim_link_params[:pq_id])
-        if @trim_link.save
-          result = {
-              :message => 'Trim link was successfully created.',
-              :status => 'success',
-              :link => "#{url_for trim_link_path(@trim_link)}"
-          }
-        else
-          result[:message]="Could not add Trim link. #{@trim_link.errors}"
-        end
+    upload = trim_link_params[:file_data]
+
+    data =
+      unless Trim::Validator.valid_upload?(upload)
+        failure_data('Missing or invalid trim file!')
       else
-        result[:message]='Could not add Trim link. File must be tr5'
+        io   = upload.read
+        trim = TrimLink.create!(data: io,
+                                filename: upload.original_filename,
+                                size: upload.size,
+                                pq_id: trim_link_params[:pq_id])
+        link_url  = url_for trim_link_path(trim.id)
+        success_data('Trim link was successfully created', link_url)
       end
-    end
-    return render json: result.as_json
+
+    render json: data
   end
 
   def show
-    @upload = TrimLink.find(params[:id])
-    @data = @upload.data
-    send_data(@data, :type => 'application/json', :filename => @upload.filename, :disposition => 'download')
+    upload = TrimLink.find(params[:id])
+    send_data(upload.data, type: 'application/octet-stream',
+                           filename: upload.filename,
+                           disposition: 'download')
   end
 
-private
+  private
+
+  def failure_data(msg)
+    {
+      message: msg,
+      status: 400
+    }
+  end
+
+  def success_data(msg, link)
+    {
+      message: msg,
+      link: link,
+      status: 200
+    }
+  end
 
   def trim_link_params
     params.require(:trim_link).permit(:file_data, :pq_id)
