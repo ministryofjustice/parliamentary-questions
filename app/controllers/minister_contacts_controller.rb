9ca91390a737c4e4a28bd799ff4bdc1ed825fcc4
Andrea Fiore <andrea.fiore@composable.io>
2015-03-02 17:33:54 +0000

    Allow editing minister contacts

--- app/controllers/minister_contacts_controller.rb
+++ app/controllers/minister_contacts_controller.rb
@@ -1,30 +1,39 @@
 class MinisterContactsController < ApplicationController
   before_action :authenticate_user!, PQUserFilter
-  before_action :set_minister, only: [:new, :create]
 
   def new
-    @minister_contact = MinisterContact.new(minister_id: @minister.id, deleted: false)
+    @minister_contact = MinisterContact.new(minister_id: params[:id])
+    @minister         = @minister_contact.minister
   end
 
   def create
     @minister_contact = MinisterContact.new(minister_contacts_params)
+    @minister         = @minister_contact.minister
 
-      if @minister_contact.save
-        redirect_to @minister, notice: 'Contact was successfully created.'
-      else
-        render action: 'new'
-      end
-  end
-
-private
-
-  def set_minister
-    if params[:id].nil?
-      @minister = Minister.find(params[:minister_contact][:minister_id])
+    if @minister_contact.save
+      redirect_to @minister, notice: 'Contact was successfully created.'
     else
-      @minister = Minister.find(params[:id])
+      render action: 'new'
     end
   end
+
+  def edit
+    @minister_contact = MinisterContact.find(params[:id])
+    @minister         = @minister_contact.minister
+  end
+
+  def update
+    @minister_contact = MinisterContact.find(params[:id])
+    @minister         = @minister_contact.minister
+
+    if @minister_contact.update(minister_contacts_params)
+      redirect_to @minister_contact.minister, notice: 'Contact was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
 
   def minister_contacts_params
     params.require(:minister_contact).permit(:name, :email, :phone, :deleted, :minister_id)
