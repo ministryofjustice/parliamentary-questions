7add2c4bdee894b37868a263f69bdf1ab73d91e5
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-03-04 15:36:48 +0000

    more compact

--- app/controllers/action_officer_reminder_controller.rb
+++ app/controllers/action_officer_reminder_controller.rb
@@ -2,31 +2,25 @@ class ActionOfficerReminderController  < ApplicationController
   before_action :authenticate_user!, PQUserFilter
 
   def accept_reject
-    loading_records do |pq, ao, ao_pq|
+    loading_records_and_rendering do |pq, ao, ao_pq|
       PqMailer.acceptance_reminder_email(ao, pq).deliver
-
       ao_pq.increment(:reminder_accept).save()
-
-      flash[:success] = "reminder sent"
-      render partial: 'shared/flash_messages'
     end
   end
 
   def send_draft
-    loading_records do |pq, ao, ao_pq|
+    loading_records_and_rendering do |pq, ao, ao_pq|
       PqMailer.draft_reminder_email(ao, pq).deliver
-
       ao_pq.increment(:reminder_draft).save()
-
-      flash[:success] = "reminder sent"
-      render partial: 'shared/flash_messages'
     end
   end
 
   private
 
-  def loading_records
+  def loading_records_and_rendering
     ao_pq = ActionOfficersPq.find(params[:id])
     yield(ao_pq.pq, ao_pq.action_officer, ao_pq)
+    flash[:success] = "reminder sent"
+    render partial: 'shared/flash_messages'
   end
 end
