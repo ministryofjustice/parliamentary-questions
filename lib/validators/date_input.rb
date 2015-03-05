3571eb1ec46913d1ed13793bd45088c4316f0183
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-03-05 10:11:34 +0000

    Finished date validator

--- lib/validators/date_input.rb
+++ lib/validators/date_input.rb
@@ -1,20 +1,29 @@
 module Validators
   module DateInput
-
     WINDOW  = 100.years
     MAX_LEN = 100
 
+    def parse_date(date_s)
+      @date_class = Date
+      secure_parse(date_s)
+    end
+
+    def parse_datetime(date_s)
+      @date_class = DateTime
+      secure_parse(date_s)
+    end
+
+    private
+
     def secure_parse(date_s)
       raise DateTimeInputError unless date_s.size < MAX_LEN
-      d = DateTime.parse(date_s)
+      d = @date_class.parse(date_s)
       raise DateTimeInputError unless d.between?(min_date, max_date)
       d
 
     rescue ArgumentError
       raise DateTimeInputError
     end
-
-    private
 
     class DateTimeInputError < StandardError
       def initialize
@@ -23,11 +32,11 @@ module Validators
     end
 
     def max_date
-      DateTime.now + WINDOW
+      @date_class.current + WINDOW
     end
 
     def min_date
-      DateTime.now - WINDOW
+      @date_class.current - WINDOW
     end
   end
 end
