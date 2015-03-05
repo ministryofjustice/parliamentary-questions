5239bc08faeffb1225f0a84ebfb4af5f3ef33bcf
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-03-05 18:27:14 +0000

    Added test coverage for finance user invitation

--- features/finance_officer_spec.rb
+++ features/finance_officer_spec.rb
@@ -1,53 +1,80 @@
 require 'feature_helper'
 
-# Pending test Senario 1
-feature "Parli-branch invites a new finance officer" do
-  xscenario "Parli-branch user invites a new user to be a finance officer in the PQ tool" do
+feature 'Creating finance officers', js: true, suspend_cleaner: true do
+  include Features::EmailHelpers
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  let(:email) { 'fo@pq.com'       }
+  let(:name)  { 'finance offer 1' }
+  let(:pass)  { '123456789'       }
+ 
+  scenario 'Parli-branch can invite a new user to be a finance officer' do
+    create_pq_session
+    visit users_path
+    click_on 'Invite new user'
+
+    fill_in 'Email', with: email
+    fill_in 'Name', with: name
+    select 'FINANCE', from: 'Role'
+    click_on 'Send an invitation'
+
+    expect(page).to have_content "An invitation email has been sent to #{email}"
+  end 
+
+  scenario 'New user receives an email invitation to become a finance officer' do
+    invitation = sent_mail_to(email).last
+
+    expect(invitation.subject).to eq 'Invitation instructions'
+    expect(invitation.body).to include 'You have been invited to use the PQ Tracker'
+  end
+
+  scenario 'Clicking the link allows the user to set their password' do
+    invitation = sent_mail_to(email).last
+    url = extract_url_like('/users/invitation/accept', invitation)
+    
+    visit url
+    fill_in 'Password', with: pass
+    fill_in 'Password confirmation', with: pass
+    click_on 'Set my password'
+
+    expect(page).to have_content 'Your password was set successfully. You are now signed in'
+  end
+
+  scenario 'New finance officer can login to view a list of questions' do
+    visit new_user_session_path
+
+    fill_in 'Email', with: email
+    fill_in 'Password', with: pass
+    click_on 'Sign in'
+
+    expect(page).to have_content "New PQ's today"
   end
 end
 
-# Pending test Senario 2 
-feature "New user recieves an email from Parli-branch and clicks on the accept the invitation link" do
-  xscenario "On clicking the link the user is taken to a page on the tool to set their password" do
-  end
-  xscenario "After the password has been set by the Finance officer they are redirected to the list of questions" do
-  end
-end 
-
-# Pending test Senario 3
-feature "List of questions for the day have been set up" do
-  xscenario "As a finance office i need to be able to log in and see the list of questions" do
-  end
-  xscenario "As a Finance officer I can sign out successfully of the PQ tool" do
-  end
-end
-# 
-
-# Actual testing Senario 4
-feature "As a finance officer, I want to be able to register interest in a question", js: true do
+feature 'Registering interest in PQs as a Finance Officer', js: true do
   before do
     DBHelpers.load_feature_fixtures
     @pqs = PQA::QuestionLoader.new.load_and_import(3)
     create_finance_session
   end
 
-# These tests are actioned on the following URL //finance/questions
-
-  scenario "Finance officer tries to access the dashboard page" do
+  scenario 'FO cannot access the dashboard page' do
     visit dashboard_path
+    
     expect(page).to have_content(/unauthorized/i)
   end
 
-  context "Finance officer views daily questions" do
-    scenario "Finance officer registers interest in some" do
-      @pqs.each do |pq|
-        expect(page).to have_content(pq.text)
-      end
-
-      check "pq[2][finance_interest]"
-      click_link_or_button 'btn_finance_visibility'
-      expect(page).to have_content(/successfully registered interest/i)
+  scenario 'FO can register interest in PQs' do
+    @pqs.each do |pq|
+      expect(page).to have_content(pq.text)
     end
+
+    check 'pq[2][finance_interest]'
+    click_link_or_button 'btn_finance_visibility'
+    expect(page).to have_content(/successfully registered interest/i)
   end
 end
 
