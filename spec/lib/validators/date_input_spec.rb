2866bc5b2876a7923ba99eb123d18f1617c08944
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-03-05 10:11:34 +0000

    Finished date validator

--- spec/lib/validators/date_input_spec.rb
+++ spec/lib/validators/date_input_spec.rb
@@ -10,25 +10,36 @@ describe Validators::DateInput do
   it 'should raise an error if the input is above the maximum size' do
     buffer = "1" *  (max_len + 1)
 
-    expect { secure_parse(buffer) }.to raise_error(Validators::DateInput::DateTimeInputError)
+    expect { parse_date(buffer) }.to raise_error(Validators::DateInput::DateTimeInputError)
   end
 
   it 'should raise an error if the input cannot be parsed as a date' do
     buffer = "\x90" *  (max_len + 1)
 
-    expect { secure_parse(buffer) }.to raise_error(Validators::DateInput::DateTimeInputError)
+    expect { parse_date(buffer) }.to raise_error(Validators::DateInput::DateTimeInputError)
   end
 
   it 'should raise an error if date is outside the expected window' do
     date_a = DateTime.now + window
     date_b = DateTime.now - window
 
-    expect { secure_parse(date_a.to_s) }.to raise_error(Validators::DateInput::DateTimeInputError)
-    expect { secure_parse(date_b.to_s) }.to raise_error(Validators::DateInput::DateTimeInputError)
+    expect { parse_datetime(date_a.to_s) }.to raise_error(Validators::DateInput::DateTimeInputError)
+    expect { parse_datetime(date_b.to_s) }.to raise_error(Validators::DateInput::DateTimeInputError)
   end
 
-  it 'should return a date time if input is correct' do
-    expect(secure_parse(Date.today.to_s)).to eq Date.today
+  context '#parse_datetime' do
+    it 'should return a date time if input is correct' do
+      dt = DateTime.now.midnight
+      expect(parse_datetime(dt.to_s)).to be_an_instance_of DateTime
+      expect(parse_datetime(dt.to_s)).to eq dt
+    end
   end
 
+  context '#parse_date' do
+    it 'should return a date if input is correct' do
+      d = Date.today
+      expect(parse_date(d.to_s)).to be_an_instance_of Date
+      expect(parse_date(d.to_s)).to eq d
+    end
+  end
 end
