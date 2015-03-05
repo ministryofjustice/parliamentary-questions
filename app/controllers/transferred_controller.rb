117fc22c1f56f77d4b93a69d35be1c656b0856e1
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-03-05 17:20:43 +0000

    WIP: test coverage for date validation UI errors

--- app/controllers/transferred_controller.rb
+++ app/controllers/transferred_controller.rb
@@ -1,24 +1,23 @@
 class TransferredController < ApplicationController
+  include Validators::DateInput
+
   before_action :authenticate_user!, PQUserFilter
 
   def new
-    loading_ogds do
-      @pq = Pq.new
-    end
+    @ogd_list = Ogd.all
+    @pq       = Pq.new
   end
 
   def create
-    loading_ogds do
-      @pq = Pq.new(pq_params)
+    @pq       = Pq.new
+    @ogd_list = Ogd.all
 
-      if @pq.update(transferred: true,
-                    raising_member_id: '0',
-                    progress: Progress.unassigned)
-
+    with_valid_dates do
+      if @pq.update(transferred_pq_params)
         flash[:success] = 'Transferred PQ was successfully created.'
-        redirect_to controller: 'dashboard', action: 'index'
+        redirect_to dashboard_path
       else
-        flash[:error] = 'There was an error creating the transfer PQ.'
+        flash.now[:error] = 'There was an error creating the transfer PQ.'
         render :new
       end
     end
@@ -26,23 +25,36 @@ class TransferredController < ApplicationController
 
   private
 
-  def pq_params
+  def transferred_pq_params
     params
       .require(:pq)
-      .permit(:uin,
-              :question,
-              :house_name,
-              :member_name,
-              :member_constituency,
-              :date_for_answer,
-              :registered_interest,
-              :question_type,
-              :transfer_in_ogd_id,
-              :transfer_in_date)
+      .permit(
+        :uin,
+        :question,
+        :house_name,
+        :member_name,
+        :member_constituency,
+        :date_for_answer,
+        :registered_interest,
+        :question_type,
+        :transfer_in_ogd_id,
+        :transfer_in_date
+      )
+      .merge({ 
+        transferred:        true,
+        raising_member_id:  '0',
+        progress:           Progress.unassigned 
+      })
   end
 
-  def loading_ogds
-    @ogd_list = Ogd.all
+  def with_valid_dates
+    [
+      :date_for_answer,
+      :transfer_in_date
+    ].each { |date_key| parse_date(params[:pq][date_key]) }
     yield
+    rescue DateTimeInputError
+      flash.now[:error] = 'Invalid date input!'
+      render(:new, status: 422)
   end
 end
