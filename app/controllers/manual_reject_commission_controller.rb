ebd3c2880657445b9c1813323a3ab690d7040c27
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-03-09 11:27:24 +0000

    Refactor manual reject and search controllers

--- app/controllers/manual_reject_commission_controller.rb
+++ app/controllers/manual_reject_commission_controller.rb
@@ -1,23 +1,26 @@
 class ManualRejectCommissionController < ApplicationController
   before_action :authenticate_user!, PQUserFilter
-  before_action :load_service
 
   def reject_manual
     ao_pq = ActionOfficersPq.find(params[:id])
-    if ao_pq.nil?
-      return redirect_to({controller: 'dashboard', action: 'index'}, {notice: 'Commission data not found'})
+
+    unless ao_pq
+      redirect_to(
+        { controller: 'dashboard', action: 'index' }, 
+        { notice: 'Commission data not found' }
+      )
+    else
+      response = AllocationResponse.new(
+        reason_option: 'Other Reason', 
+        reason: "This question was rejected manually by #{current_user.email}"
+      )
+      AssignmentService.new.reject(ao_pq, response)
+      
+      uin = ao_pq.pq.uin
+      redirect_to(
+        { controller: 'pqs', action: 'show', id: uin }, 
+        { notice: "#{uin} manually rejected, check if you have to do the commissioning again" }
+      )
     end
-
-    response = AllocationResponse.new(reason_option: 'Other Reason', reason: "This question was rejected manually by #{current_user.email}")
-    @assignment_service.reject(ao_pq, response)
-
-    pq = Pq.find(ao_pq.pq_id)
-    redirect_to({controller: 'pqs', action: 'show', id: pq.uin }, {notice: "#{pq.uin} manually rejected, check if you have to do the commissioning again"})
-  end
-
-private
-
-  def load_service(service = AssignmentService.new)
-    @assignment_service ||= service
   end
 end
