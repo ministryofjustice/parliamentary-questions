d1a68558972e65415818a4c5912b53a2d2943fe5
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-03-10 17:02:54 +0000

    Change title of 404 and 500 pages to include Error Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :set_am_host
+  before_action :set_page_title
 
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    page_not_found
+  rescue_from StandardError do |exception|
+    case exception.class
+    when ActiveRecord::RecordNotFound
+      page_not_found
+    else
+      server_error
+    end
   end
 
   def set_am_host
@@ -21,18 +27,25 @@ class ApplicationController < ActionController::Base
   end
 
   def page_not_found
+    @page_title = 'Error Page 404'
     show_error_page_and_increment_statsd(404)
   end
 
   def unauthorized
+    @page_title = 'Error Page 401'
     show_error_page_and_increment_statsd(401)
   end
 
   def server_error
+    @page_title = 'Error Page 500'
     show_error_page_and_increment_statsd(500)
   end
 
 protected
+
+  def set_page_title
+    @page_title = 'MOJ Parliamentary Questions'
+  end
 
   def request_url
     "#{request.protocol}#{request.host_with_port}#{request.fullpath}"
