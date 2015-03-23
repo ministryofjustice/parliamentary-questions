74464b9da1907fbaaad436d56999b8e848bdbd18
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-03-23 10:06:31 +0000

    - refactor settings.rb to remove method_missing - convert tabs to spaces

--- spec/mailers/pq_mailers_spec.rb
+++ spec/mailers/pq_mailers_spec.rb
@@ -2,36 +2,34 @@ require 'spec_helper'
 
 describe PqMailer do
 
+  describe 'commission_email' do
 
-
-	describe 'commission_email' do
-
-		before(:each) do
-			pq = FactoryGirl.create :pq
-			@template_params = {
-				:uin 									=> pq.uin,
-				:question 						=> pq.question,
-				:ao_name 							=> "Colin Bruce",
-				:member_constituency 	=> "Kingston upon Hull North",
-				:member_name 					=> "Diana Johnson",
-				:house_name 					=> "House of Commons",
-				:answer_by 						=> "Damian Green (MP)",
-				:internal_deadline 		=> 3.days.from_now,
-				:date_to_parliament 	=> 5.days.from_now,
-				:email 								=> "colin.bruce@digital.justice.gov.uk",
-				:token 								=> "KL7g57Y-7ZFgBBGvWq9o",
-				:entity 							=> "assignment:4"}
-		end
+    before(:each) do
+      pq = FactoryGirl.create :pq
+      @template_params = {
+        :uin                  => pq.uin,
+        :question             => pq.question,
+        :ao_name              => "Colin Bruce",
+        :member_constituency  => "Kingston upon Hull North",
+        :member_name          => "Diana Johnson",
+        :house_name           => "House of Commons",
+        :answer_by            => "Damian Green (MP)",
+        :internal_deadline    => 3.days.from_now,
+        :date_to_parliament   => 5.days.from_now,
+        :email                => "colin.bruce@digital.justice.gov.uk",
+        :token                => "KL7g57Y-7ZFgBBGvWq9o",
+        :entity               => "assignment:4"}
+    end
     describe 'deliver' do
       it 'should have correct addressing info' do
-      	PqMailer.commission_email(@template_params).deliver
-      	 mail = ActionMailer::Base.deliveries.first
-      	 expect(mail.to).to eq ["colin.bruce@digital.justice.gov.uk"]
-      	 expect(mail.from).to eq ["pqs@justice.gsi.gov.uk"]
-      	 expect(mail.reply_to).to eq ["pqs@justice.gsi.gov.uk"]
+        PqMailer.commission_email(@template_params).deliver
+         mail = ActionMailer::Base.deliveries.first
+         expect(mail.to).to eq ["colin.bruce@digital.justice.gov.uk"]
+         expect(mail.from).to eq ["pqs@justice.gsi.gov.uk"]
+         expect(mail.reply_to).to eq ["pqs@justice.gsi.gov.uk"]
 
-      	 expect(mail.to_s).to include 'From: PQ Team <pqs@justice.gsi.gov.uk>'
-      	 expect(mail.to_s).to include 'Reply-To: pqs@justice.gsi.gov.uk'
+         expect(mail.to_s).to include 'From: PQ Team <pqs@justice.gsi.gov.uk>'
+         expect(mail.to_s).to include 'Reply-To: pqs@justice.gsi.gov.uk'
       end
     end
   end
