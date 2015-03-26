7ddc129c3c15b01a8f1f21a53d854b10b091822e
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-03-26 17:33:16 +0000

    refactor log stash recording of token errors & add TODO file for branch

--- app/controllers/ao_token_filter.rb
+++ app/controllers/ao_token_filter.rb
@@ -6,6 +6,9 @@ class AOTokenFilter
     end
   end
 
+
+  private
+
   def self.validate_token(controller)
     result = :invalid
     token_service = TokenService.new
@@ -19,4 +22,38 @@ class AOTokenFilter
 
     return result
   end
+
+  
+  def self.log_and_redirect(controller, token_state)
+    params         = OpenStruct.new
+    params.uri     = controller.env['REQUEST_URI']
+    params.referer = controller.request.referer
+    params.uin     = extract_uin(controller)  
+    params.user    = controller.current_user.name
+    params.user_id = controller.current_user.id
+    log_error(token_state, params)
+    controller.render :file => "shared/token_#{token_state.to_s}.html.slim", status: :unauthorized
+  end
+
+
+  def self.log_error(token_state, params)
+    LogStuff.error(:token_error, 
+                    type: "#{token_state}_token", 
+                    uri: params.uri, 
+                    referer: params.referer, 
+                    uin: params.uin, 
+                    user: params.user, 
+                    user_id: params.user_id) { "Access Token Error - #{token_state.to_s.humanize} Token" }
+  end
+
+  def self.extract_uin(controller)
+    request_path  = controller.env['REQUEST_PATH']
+    if request_path =~ /^\/assignment\//
+      controller.env['REQUEST_PATH'].split('/').last
+    else
+      nil
+    end
+  end
+
+
 end
