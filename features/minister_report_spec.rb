12ff70de3d8d2feb75bbd6a4cfb32a3dc4623415
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-03-30 10:58:59 +0100

    minister report specs for journeys ending in PODS cleared

--- features/minister_report_spec.rb
+++ features/minister_report_spec.rb
@@ -19,25 +19,76 @@ feature 'Minister Report', js: true, suspend_cleaner: true do
   let(:policy_minister)         { Minister.find_by_name('Damian Green (MP)') }
   let(:draft_pending_progress)  { Progress.find_by_name('Draft Pending') }
   let(:with_pod_progress)       { Progress.find_by_name('With POD') }
+  let(:pod_query)               { Progress.find_by_name('POD Query') }
+  let(:pod_cleared)             { Progress.find_by_name('POD Cleared') }
 
 
   scenario 'Acceptance of a question by an AO should show in report as draft pending' do
     pq = Pq.first
     progress_pq_to_draft_pending(pq)
-    visit reports_ministers_by_progress_path
-    el = find_table_element_by_minister_and_progress(minister, draft_pending_progress)
-    expect(el.text).to eq '1'
+    expect_ministers_report_to_have(minister, draft_pending_progress, '1')
   end
 
   scenario 'Setting date for answer to parliament should show in report as With POD' do
     pq = Pq.all[1]
     progress_pq_to_draft_pending(pq)
     progress_pq_from_draft_pending_to_with_pod(pq)
-    visit reports_ministers_by_progress_path
-    el = find_table_element_by_minister_and_progress(minister, with_pod_progress)
-    expect(el.text).to eq '1'
+    expect_ministers_report_to_have(minister, with_pod_progress, '1')
   end
+
+
+  scenario 'setting pod query flag should show in report as POD query' do
+    pq = Pq.all[2]
+    progress_pq_to_draft_pending(pq)
+    progress_pq_from_draft_pending_to_with_pod(pq)
+    progress_pq_from_with_pod_to_pod_query(pq)
+    expect_ministers_report_to_have(minister, pod_query, '1')
+  end
+
+  scenario 'setting pod_cleared on with pod and pod query should set both to pod cleared' do
+    pq3 = Pq.all[3]
+    pq4 = Pq.all[4]
+    
+    progress_pq_to_draft_pending(pq3)
+    progress_pq_from_draft_pending_to_with_pod(pq3)
+    progress_pq_from_with_pod_to_pod_query(pq3)
+    progress_pq_to_pod_cleared(pq3)
+
+    clear_sent_mail
+    progress_pq_to_draft_pending(pq4)
+    progress_pq_from_draft_pending_to_with_pod(pq4)
+    progress_pq_to_pod_cleared(pq4)
+
+    expect_ministers_report_to_have(minister, pod_cleared, '2')
+  end
+
 end 
+
+
+def progress_pq_to_pod_cleared(pq)
+  visit pq_path(pq)
+  in_pq_detail(pq.uin, 'POD check')  do
+    fillin_date('#pod_clearance') 
+    click_on 'Save'
+  end
+end
+
+
+def expect_ministers_report_to_have(minister, progress, expected_text)
+  visit reports_ministers_by_progress_path
+  el = find_table_element_by_minister_and_progress(minister, progress)
+  expect(el.text).to eq expected_text
+end
+
+
+
+def progress_pq_from_with_pod_to_pod_query(pq)
+  visit pq_path(pq)
+  in_pq_detail(pq.uin, 'POD check')  do
+    check 'POD query flag'
+    click_on 'Save'
+  end
+end
 
 
 def progress_pq_to_draft_pending(pq)
