ec4e8195c225684056b372db254e3416ba44a13e
Andrea Fiore <andrea.fiore@composable.io>
2015-03-31 12:07:56 +0100

    wip: bring Progress model back from the land of death

---
+++ app/models/progress.rb
@@ -0,0 +1,146 @@
+class Progress < ActiveRecord::Base
+  has_paper_trail
+  has_many :pqs
+
+  def classname
+    self.name.downcase.sub ' ', '-'
+  end
+
+  def self.find_by_status(status)
+    Progress.find_by_name(status)
+  end
+
+  def self.unassigned
+    find_by_status(self.UNASSIGNED)
+  end
+
+  def self.no_response
+    find_by_status(self.NO_RESPONSE)
+  end
+
+  def self.draft_pending
+    find_by_status(self.DRAFT_PENDING)
+  end
+
+  def self.rejected
+    find_by_status(self.REJECTED)
+  end
+
+  def self.with_pod
+    find_by_status(self.WITH_POD)
+  end
+
+  def self.pod_query
+    find_by_status(self.POD_QUERY)
+  end
+
+  def self.pod_cleared
+    find_by_status(self.POD_CLEARED)
+  end
+
+  def self.with_minister
+    find_by_status(self.WITH_MINISTER)
+  end
+
+  def self.ministerial_query
+    find_by_status(self.MINISTERIAL_QUERY)
+  end
+
+  def self.minister_cleared
+    find_by_status(self.MINISTER_CLEARED)
+  end
+
+  def self.transfer
+    find_by_status(self.TRANSFER)
+  end
+
+  def self.answered
+    find_by_status(self.ANSWERED)
+  end
+  def self.transferred_out
+    find_by_status(self.TRANSFERRED_OUT)
+  end
+
+  def self.UNASSIGNED
+    'Unassigned'
+  end
+
+  def self.NO_RESPONSE
+    'No response'
+  end
+
+  def self.DRAFT_PENDING
+    'Draft Pending'
+  end
+
+  def self.REJECTED
+    'Rejected'
+  end
+
+  def self.WITH_POD
+    'With POD'
+  end
+
+  def self.POD_QUERY
+    'POD Query'
+  end
+
+  def self.POD_CLEARED
+    'POD Cleared'
+  end
+
+  def self.WITH_MINISTER
+    'With Minister'
+  end
+
+  def self.MINISTERIAL_QUERY
+    'Ministerial Query'
+  end
+
+  def self.MINISTER_CLEARED
+    'Minister Cleared'
+  end
+
+  def self.ANSWERED
+    'Answered'
+  end
+
+  def self.TRANSFERRED_OUT
+    'Transferred out'
+  end
+
+  def self.TRANSFER
+    'Transfer'
+  end
+
+  def self.new_questions
+    [
+      Progress.UNASSIGNED,
+      Progress.NO_RESPONSE,
+      Progress.REJECTED
+    ]
+  end
+
+  def self.in_progress_questions
+    [
+      Progress.DRAFT_PENDING,
+      Progress.WITH_POD,
+      Progress.POD_QUERY,
+      Progress.POD_CLEARED,
+      Progress.WITH_MINISTER,
+      Progress.MINISTERIAL_QUERY,
+      Progress.MINISTER_CLEARED
+    ]
+  end
+
+  def self.closed_questions
+    [
+      Progress.ANSWERED,
+      Progress.TRANSFERRED_OUT
+    ]
+  end
+
+  def self.visible
+    [new_questions, in_progress_questions].flatten
+  end
+end
