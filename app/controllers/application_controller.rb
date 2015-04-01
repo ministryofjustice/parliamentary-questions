fb77140a6b3221a2c85ff5d8bb753e90c571cd06
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-04-01 14:09:49 +0100

    don't trap errors other than ActiveRecord::RecordNotFound in order to preserve backtrace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  if Rails.env.production? || Rails.env.test?
-    rescue_from StandardError do |exception|
-      if exception.is_a?(ActiveRecord::RecordNotFound)
-        page_not_found
-      else
-        server_error
-      end
-    end
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    page_not_found
   end
 
 
