38d47095f0b69402883e49cc8c86a14d2fc8c8b3
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-04-07 16:18:37 +0100

    add annotations to models, factories and specs

--- app/models/press_desk.rb
+++ app/models/press_desk.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: press_desks
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  deleted    :boolean          default(FALSE)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class PressDesk < ActiveRecord::Base
   extend  SoftDeletion::Collection
   include SoftDeletion::Record
--- spec/models/directorate_spec.rb
+++ spec/models/directorate_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: directorates
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  deleted    :boolean          default(FALSE)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Directorate do
