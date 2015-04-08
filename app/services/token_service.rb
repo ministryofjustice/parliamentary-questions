fcaf4397092f614b36552b074780e9f43594f4a4
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-04-08 14:22:19 +0100

    Debugging messages

--- app/services/token_service.rb
+++ app/services/token_service.rb
@@ -10,20 +10,32 @@ class TokenService
 
 
   def valid?(token, path, entity)
+    logit  "DEBUG TOKEN SERVICE VALID?    #{__FILE__}::#{__LINE__}"
     token_dec = Devise.token_generator.digest(Token, :token_digest, token)
     token = Token.find_by(path: path, entity: entity)
-    token && Devise.secure_compare(token_dec, token.token_digest) ? true : false
+    result = token && Devise.secure_compare(token_dec, token.token_digest) ? true : false
+    logit  "DEBUG valid: #{result}    #{__FILE__}::#{__LINE__}"
+    result
   end
 
 
   def expired?(token, path, entity)
+    logit "DEBUG TOKEN SERVICE EXPIRED?    #{__FILE__}::#{__LINE__} "
     token_dec = Devise.token_generator.digest(Token, :token_digest, token)
     token = Token.find_by(path: path, entity: entity)
-    token.expire < DateTime.now
+    logit "DEBUG Token id: #{token.id}  path: #{token.path} expire: #{token.expire}   #{__FILE__}::#{__LINE__} "
+    result = token.expire < DateTime.now
+    logit "DEBUG expired: #{result}    #{__FILE__}::#{__LINE__} "
+    result
   end
 
   def delete_expired
     now_no_time_zone = DateTime.now
     Token.where('expire < ?', now_no_time_zone).destroy_all
+  end
+
+
+  def logit(message)
+    LogStuff.error(:debug, message: message) { "Access Token Error - Debug message" }
   end
 end
