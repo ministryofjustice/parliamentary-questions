fa7f96c6384ff181ef11d80d4180681528b41547
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-04-15 10:05:26 +0100

    Tidied redeploy code

--- redeploy/pq_database_fixer.rb
+++ redeploy/pq_database_fixer.rb
@@ -1,21 +1,23 @@
 require "#{Rails.root}/redeploy/pq_state_comparer.rb"
 require "#{Rails.root}/redeploy/pq_fixer.rb"
 
-class PqDatabaseFixer
+module Redeploy
+  class PqDatabaseFixer
 
-  def run
-    comparer = PqStateComparer.new
-    comparer.update_all_states!
-    results = comparer.run
-    results.each do |result|
-      pq_fixer = PqFixer.new(result.id)
-      pq_fixer.fix!
+    def run
+      comparer = PqStateComparer.new
+      comparer.update_all_states!
+      results = comparer.run
+      results.each do |result|
+        pq_fixer = PqFixer.new(result.id)
+        pq_fixer.fix!
+      end
+
+      # Now see if there are any still in the wrong state
+      comparer = PqStateComparer.new
+      comparer.run
+      comparer.display_results
+
     end
-
-    # Now see if there are any still in the wrong state
-    comparer = PqStateComparer.new
-    comparer.run
-    comparer.display_results
-
   end
 end
--- redeploy/pq_state_comparer.rb
+++ redeploy/pq_state_comparer.rb
@@ -1,36 +1,29 @@
-# require "#{Rails.root}/redeploy/pq_state_comparer.rb"
+module Redeploy
+  class PqStateComparer
 
-# psc = PqStateComparer.new
-# psc.update_all_states         # runs update_state! on every record in the database
-# psc.run                       # returns an array of open structs of records needing attention
-
-
-
-class PqStateComparer
-
-  def initialize
-    @results = []
-  end
-
-  def update_all_states!
-    Pq.all.each { |q| q.update_state! }
-  end
-
-  def run(run_update = false)
-    record_set = Pq.joins(:progress).
-      where("replace(upper(pqs.state), ' ', '_') != replace(upper(progresses.name), ' ', '_')").
-      order(:id)
-
-    record_set.each do |r|
-      @results << OpenStruct.new(id: r.id, uin: r.uin, state: r.state, progress: r.progress.name)
+    def initialize
+      @results = []
     end
-    @results
-  end
 
-  def display_results
-    @results.each do |r|
-      puts "#ID: #{r.id}, UIN: #{r.uin}, STATE: #{r.state}  PROGRESS: #{r.progress}"
+    def update_all_states!
+      Pq.all.each { |q| q.update_state! }
+    end
+
+    def run
+      record_set = Pq.joins(:progress).
+        where("replace(upper(pqs.state), ' ', '_') != replace(upper(progresses.name), ' ', '_')").
+        order(:id)
+
+      record_set.each do |r|
+        @results << OpenStruct.new(id: r.id, uin: r.uin, state: r.state, progress: r.progress.name)
+      end
+      @results
+    end
+
+    def display_results
+      @results.each do |r|
+        puts "#ID: #{r.id}, UIN: #{r.uin}, STATE: #{r.state}  PROGRESS: #{r.progress}"
+      end
     end
   end
 end
-
