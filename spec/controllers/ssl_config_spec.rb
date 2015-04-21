95ef0279853a7f15cd4417f24e6ead3e956d2593
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-04-21 16:17:49 +0100

    change the code to decide whether or not we force ssl to a before filter

--- spec/controllers/ssl_config_spec.rb
+++ spec/controllers/ssl_config_spec.rb
@@ -20,6 +20,8 @@ describe DashboardController, type: :controller do
 
   describe 'ssl configuration in production' do
     it 'should force ssl' do
+      # allow(PQUserFilter).to receive(:before).and_return(true)
+      # allow(controller).to receive(:authenticate_user!).and_return(true)
       expect(Rails.env).to receive(:production?).and_return(true)
         
       expect(controller).to receive(:force_ssl)
@@ -33,7 +35,7 @@ describe DashboardController, type: :controller do
       expect(Rails.env).to receive(:production?).and_return(false)
       
       expect(controller).not_to receive(:force_ssl)
-      expect(controller).to receive(:before).with(:reset_session)
+      # expect(controller).to receive(:before).with(:reset_session)      ============> I don't think we expect this, do we?
       get :index
     end
   end
@@ -48,9 +50,10 @@ describe PingController, type: :controller do
   describe 'ssl configuration in production' do
     it 'should not force ssl on excepted routes, but reset the session' do
       expect(Rails.env).to receive(:production?).and_return(true)
+      allow(request).to receive(:fullpath).and_return('ping.json')
         
       expect(controller).not_to receive(:force_ssl)
-      expect(controller).to receive(:before).with(:reset_session)
+      expect(controller).to receive(:reset_session)
       get :index
     end
   end
