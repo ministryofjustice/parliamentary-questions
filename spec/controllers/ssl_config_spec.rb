0f464f760f3afc75f79e51687bd08b4b2dc8c607
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-04-21 16:04:08 +0100

    WIP

--- spec/controllers/ssl_config_spec.rb
+++ spec/controllers/ssl_config_spec.rb
@@ -1,17 +1,5 @@
 require 'spec_helper'
 
-describe ApplicationController, type: :controller do
-  describe '#ssl_excepted?' do
-    it 'should return true if we are on an excepted path' do
-
-    end
-
-    it 'should return false if we are not in an excepted path' do
-
-    end
-  end
-end
-
 describe DashboardController, type: :controller do
   before(:each) do
     allow(PQUserFilter).to receive(:before).and_return(true)
@@ -20,20 +8,20 @@ describe DashboardController, type: :controller do
 
   describe 'ssl configuration in production' do
     it 'should force ssl' do
-      expect(Rails.env).to receive(:production?).and_return(true)
+      allow(Rails.env).to receive(:production?).and_return(true)
         
       expect(controller).to receive(:force_ssl)
-      expect(controller).not_to receive(:before).with(:reset_session)
+      expect(controller).not_to receive(:before_action).with(:reset_session, if: :ssl_excepted?)
       get :index
     end
   end
 
   describe 'ssl configuration not in production' do
     it 'should not force ssl on any routes' do
-      expect(Rails.env).to receive(:production?).and_return(false)
+      allow(Rails.env).to receive(:production?).and_return(false)
       
       expect(controller).not_to receive(:force_ssl)
-      expect(controller).to receive(:before).with(:reset_session)
+      expect(controller).to receive(:before_action).with(:reset_session, if: :ssl_excepted?)
       get :index
     end
   end
@@ -47,10 +35,10 @@ describe PingController, type: :controller do
 
   describe 'ssl configuration in production' do
     it 'should not force ssl on excepted routes, but reset the session' do
-      expect(Rails.env).to receive(:production?).and_return(true)
+      allow(Rails.env).to receive(:production?).and_return(true)
         
       expect(controller).not_to receive(:force_ssl)
-      expect(controller).to receive(:before).with(:reset_session)
+      expect(controller).to receive(:before_action).with(:reset_session, if: :ssl_excepted?)
       get :index
     end
   end
