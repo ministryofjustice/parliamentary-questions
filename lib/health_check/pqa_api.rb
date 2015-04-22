32627c00a73d4d3793e0e0d9d1995fde0fa98ed5
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-04-22 15:18:06 +0100

    - only carry out PQA API health check once every 15 mins (configurable)  - fix bug in Healthcheck service  - write tests for Healtcheck service and controller  - rewrite Database healthcheck  - separate model specs from feature specs

--- lib/health_check/pqa_api.rb
+++ lib/health_check/pqa_api.rb
@@ -1,5 +1,8 @@
 module HealthCheck
   class PqaApi < Component
+
+    TIMESTAMP_FILE = "#{Rails.root}/tmp/pqa_api_healthcheck_timestamp"
+
     ERRS_TO_CATCH = 
     [
       Net::ReadTimeout, 
@@ -8,9 +11,17 @@ module HealthCheck
       HTTPClient::FailureResponse
      ]
 
+    def self.time_to_run?
+      interval = get_minimum_interval_in_seconds
+      time_last_run = get_time_last_run
+      Time.now.to_i > interval + time_last_run
+    end
+
+
     def initialize
       @api = PQA::ApiClient.from_settings
       super
+      record_time_ran
     end
 
     def available?
@@ -38,6 +49,29 @@ module HealthCheck
     end
 
     private
+
+    def self.get_minimum_interval_in_seconds
+      Settings.healthceck_pqa_api_interval * 60
+    end
+
+    def self.get_time_last_run
+      if File.exist?(TIMESTAMP_FILE)
+        File.open(TIMESTAMP_FILE, 'r') do |fp|
+          interval = fp.gets.chomp.to_i
+        end
+      else
+        0
+      end
+    end
+
+
+    def record_time_ran
+      tmpdir = "#{Rails.root}/tmp"
+      Dir.mkdir(tmpdir) unless Dir.exist?(tmpdir
+      File.open(TIMESTAMP_FILE, 'w') do |fp|
+        fp.puts Time.now.to_i
+      end
+    end
 
     def log_error(type, e)
       @errors << "PQA API #{type} Error: #{e.message}"
