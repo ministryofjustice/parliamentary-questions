1b68913e413f1360df5842dd7d96642c266d6801
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-04-28 17:13:01 +0100

    HOTFIX: add missing migration component

--- db/migrate/20150428133026_clean_stats_data.rb
+++ db/migrate/20150428133026_clean_stats_data.rb
@@ -5,5 +5,12 @@ class CleanStatsData < ActiveRecord::Migration
       "SET updated_at = created_at " +
       "WHERE action_officer_id = 497;"
     )
+    
+    ActiveRecord::Base.connection.execute(
+      "UPDATE action_officers_pqs " +
+      "SET created_at = pqs.created_at FROM pqs " +
+      "WHERE action_officers_pqs.action_officer_id = 497 " +
+      "AND action_officers_pqs.pq_id = pqs.id;"
+    )
   end
 end
