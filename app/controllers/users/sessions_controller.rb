357efc2d610fdd1eff3d3a8b42fcde7113639d60
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-05-06 10:58:48 +0100

    fix 500 error on failed login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,8 @@
 class Users::SessionsController < Devise::SessionsController
   def create
+    csrf = session['_csrf_token']
     reset_session
+    session['_csrf_token'] = csrf
     super
   end
 end
