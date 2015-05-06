98d8538ec1c3100a943e76e954e61d7ded9ce7a0
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-05-06 17:13:40 +0100

    PQA Import to only collect questions since last successful import, and keep record of runs

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -4,14 +4,19 @@ class ImportWorker
   end
 
   def perform
+    date_from = PqaImportRun.last_import_time_utc
+    date_to = 5.minutes.from_now
+    start_time = Time.now
     LogStuff.tag(:import) do
       begin
-        LogStuff.info { "Import: starting scheduled import" }
-        report = @import.run(Date.yesterday, Date.tomorrow)
+        LogStuff.info { "Import: starting scheduled import from #{date_from} to #{date_to}" }
+        report = @import.run(date_from, date_to)
         LogStuff.info { "Import: completed scheduled import" }
         ImportMailer.notify_success(report).deliver
+        PqaImportRun.record_success(start_time, report)
 
       rescue => err
+        PqaImportRun.record_failure(start_time, "#{err.class}: #{err.message}")
         case err
         when HTTPClient::FailureResponse, Net::ReadTimeout, Errno::ECONNREFUSED, SocketError
           LogStuff.error { err.message }
---
+++ spec/workers/import_worker_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe ImportWorker do
+
+  let(:worker)                { ImportWorker.new }
+  let(:importer)              { worker.instance_variable_get(:@import) }
+  let(:freeze_time)           { Time.new(2015, 5, 7, 12, 45, 17).utc }
+  let(:last_import_time)      { Time.new(2015, 7, 9, 8, 30, 26).utc }
+  let(:five_mins_from_now)    { Time.new(2015, 5, 7, 12, 50, 17).utc }
+  let(:three_days_ago)        { Time.new(2015, 5, 4, 12, 45, 17).utc }
+
+  let(:ok_report) do
+    {
+      total:    18,
+      created:  15,
+      updated:  3,
+      errors:   {}
+    }
+  end
+
+
+  describe '#perform' do
+    it 'should record collect questions from 3 days ago if the pqa_import_runs table is empty' do
+      Timecop.freeze freeze_time do
+        expect(PqaImportRun.count).to eq(0)
+        expect(importer).to receive(:run).with(three_days_ago, five_mins_from_now).and_return(ok_report)
+
+        worker.perform
+      end
+    end
+
+    it 'should collect questions from the start time of the previous import' do
+      Timecop.freeze freeze_time do
+        allow(PqaImportRun).to receive(:last_import_time_utc).and_return(last_import_time)
+        expect(importer).to receive(:run).with(last_import_time, five_mins_from_now).and_return(ok_report)
+
+        worker.perform
+      end
+    end
+
+    it 'should add a record to the pqa_runs_table with the time of running' do
+      Timecop.freeze freeze_time do
+        allow(PqaImportRun).to receive(:last_import_time_utc).and_return(last_import_time)
+        expect(importer).to receive(:run).with(last_import_time, five_mins_from_now).and_return(ok_report)
+
+        worker.perform
+        pir = PqaImportRun.last
+        expect(pir.start_time).to eq(freeze_time)
+      end
+    end
+
+
+
+  end
+
+end
+
