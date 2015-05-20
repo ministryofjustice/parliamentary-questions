d172e103d19eda17551c2ac46c336eea5302073e
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-05-20 14:41:45 +0100

    mark watch list token as acknowledged the first time the link is clicked

--- app/controllers/watchlist_dashboard_controller.rb
+++ app/controllers/watchlist_dashboard_controller.rb
@@ -4,11 +4,19 @@ class WatchlistDashboardController < ApplicationController
 
   def index
   	update_page_title('Watchlist preview')
+    token = Token.entity(watchlist_params[:entity])
+    token.accept unless token.acknowledged?
     @questions = Pq.allocated_since(DateTime.now.at_beginning_of_day)
   end
 
   def preview
     index
     render 'index'
+  end
+
+  private
+
+  def watchlist_params
+    params.permit(:entity)
   end
 end
