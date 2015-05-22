076bc6a2ff9dbfc3effb79a819113a2e5beefe71
Jeremy Fox <jeremy.fox@digital.justice.gov.uk>
2015-05-22 09:44:49 +0100

    WIP

--- lib/smoke_test/base.rb
+++ lib/smoke_test/base.rb
@@ -68,7 +68,7 @@ module SmokeTest
       store = OpenSSL::X509::Store.new
 
       if SSL_CERT_DIR
-        store.add_path(SSL_CERT_DIR)
+        store.set_default_paths
       else
         store.add_file(SSL_CERT_FILE)
       end
