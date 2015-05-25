6ca952674dc6ce6b4475685b386cc28354f4176c
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-05-25 12:35:04 +0100

    Various results and sql snippets for AO and DD de-duping

--- lib/bugfix/dupe_deputy_director_fixer.rb
+++ lib/bugfix/dupe_deputy_director_fixer.rb
@@ -2,7 +2,7 @@ class DupeDeputyDirectorFixer
 
   def initialize(dupes)
     @dupes = dupes
-    @reports = []
+    @report = []
   end
 
   def dedupe!
@@ -12,9 +12,15 @@ class DupeDeputyDirectorFixer
         old_dd = ao.deputy_director
         new_dd = DeputyDirector.find(@dupes[old_dd.id])
         ao.update_attribute(:deputy_director, new_dd)
-        @reports << "AO:#{ao.id} #{ao.name} has changed DD from #{old_dd.id}:#{old_dd.name} to #{new_dd.id}:#{new_dd.name}."
+        @report << "AO:#{ao.id} #{ao.name} has changed DD from #{old_dd.id}:#{old_dd.name} to #{new_dd.id}:#{new_dd.name}."
       end
     end
   end
+
+  def report
+    @report.each { |r| puts r }
+  end
+
+
 
 end
