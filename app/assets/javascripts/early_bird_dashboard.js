05e24863b1e41710f58dc7af7d64f02556b675fa
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2015-08-18 10:55:06 +0100

    Fixed question list column widths in response to PB's device resolution statistics.

--- app/assets/javascripts/early_bird_dashboard.js
+++ app/assets/javascripts/early_bird_dashboard.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function () {
+    "use strict";
+    //Check Initial browser window size - apply sticky filter
+    var win = $(this);
+    if (win.width() > 991) {
+        $('#filters').addClass('js-stick-at-top-when-scrolling');
+    }
+
+    // On browser window resize add/remove sticky filter
+    $(window).on('resize', function () {
+        if (win.width() > 991) {
+            $('#filters').addClass('js-stick-at-top-when-scrolling');
+        }
+        else {
+            $('#filters').removeClass('js-stick-at-top-when-scrolling');
+        }
+    });
+});
+
