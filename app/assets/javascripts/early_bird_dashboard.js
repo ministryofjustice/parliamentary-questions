7bab088a097e1b826d18cbe9ce29cab56afb6f52
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2015-08-19 12:38:02 +0100

    Fixes to the sticky filter behaviour and widow size responsiveness.

--- app/assets/javascripts/early_bird_dashboard.js
+++ app/assets/javascripts/early_bird_dashboard.js
@@ -2,21 +2,8 @@
 // All this logic will automatically be available in application.js.
 
 $(document).ready(function () {
-    "use strict";
-    //Check Initial browser window size - apply sticky filter
-    var win = $(this);
-    if (win.width() > 991) {
-        $('#filters').addClass('js-stick-at-top-when-scrolling');
-    }
-
-    // On browser window resize add/remove sticky filter
-    $(window).on('resize', function () {
-        if (win.width() > 991) {
-            $('#filters').addClass('js-stick-at-top-when-scrolling');
-        }
-        else {
-            $('#filters').removeClass('js-stick-at-top-when-scrolling');
-        }
-    });
+    GOVUK.stickAtTopWhenScrolling.init();
+    GOVUK.stopScrollingAtFooter.addEl($(footer), $(footer).height());
 });
+
 
