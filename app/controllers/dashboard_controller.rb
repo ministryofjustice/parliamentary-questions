a25966b30896fbd1d9cc2baa61867d751ac42c88
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2015-09-14 11:07:31 +0100

    Follow-Up IWW identified by IWW flag not question type in UI

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -68,7 +68,7 @@ class DashboardController < ApplicationController
     #@questions       = paginate_collection(yield) if block_given?
     @questions       = (yield) if block_given?
     @statuses = @questions.all.map {|q| q.state}.uniq
-    @question_types = @questions.all.map {|q| q.question_type}.uniq
+    @question_types = @questions.where("question_type != 'Follow-up IWW'").map {|q| q.question_type}.uniq
     @transfers = @questions.all.map {|q| q.transferred}.uniq
     @i_will_writes = @questions.all.map {|q| q.i_will_write}.uniq
     @answering_minister_names = @questions.where('minister_id > 0').map {|q| q.minister.name}.uniq
