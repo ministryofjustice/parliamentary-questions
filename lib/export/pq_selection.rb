fef9e2eb0f0b5fba2c94a7683679c42ca8cadd72
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2015-10-27 16:10:13 +0000

    Format for Quick Export Style

--- lib/export/pq_selection.rb
+++ lib/export/pq_selection.rb
@@ -1,20 +1,65 @@
 module Export
   class PqSelection < Base
+
+    HEADINGS = [
+      'PIN',
+    'MP',
+    'Draft due to Parly Branch',
+    'Date Due in Parliament',
+    'Full_PQ_subject',
+    'Minister',
+    'Status',
+    'Action Officer',
+    'Division',
+    'Type of Question',
+    'Date First Appeared in Parliament',
+    'Date Draft Returned to PB',
+    'Date delivered to Minister',
+    'Returned signed from Minister',
+    'Date response answered by Parly (dept)'
+    ]
+
+    def csv_fields(pq, ao)
+      [ pq.uin,                                                         # 'PIN',
+        pq.member_name,                                                 # 'MP',
+        pq.internal_deadline,                                           # 'Draft due to Parly Branch',
+        pq.date_for_answer,                                             # 'Date Due in Parliament',
+        pq.question,                                                    # 'Full_PQ_subject',
+        pq.minister && pq.minister.name,                                # 'Minister',
+        pq.state,
+        ao && ao.name,                                                   # 'Action Officer',
+        pq.original_division && pq.original_division.name,                                # 'Division',
+        pq.question_type,                                               # 'Type of Question',
+        pq.tabled_date,                                                 # 'Date First Appeared in Parliament',
+        pq.draft_answer_received,                                       # 'Date Draft Returned to PB',
+        pq.sent_to_answering_minister,                                  # 'Date delivered to Minister',
+        pq.cleared_by_answering_minister,                                # 'Returned signed from Minister',
+        pq.answer_submitted                                            # 'Date response answered by Parly (dept)',
+      ]
+    end
+
+
+    def to_csv
+      CSV.generate do |csv|
+        csv << HEADINGS
+        pqs.each { |pq| csv << csv_fields(pq, pq.action_officer_accepted) }
+      end
+    end
+
     private
     def pqs
         pqs_array = Array.new
         @pqs_list.split(',').map { |p|
           if Pq.find_by(uin: p).nil?
             puts 'Failed to validate ' || p
-            errors.add(p, "False PQ UIN")
-            return false
+            return pqs_array
           else
             puts 'Validated ' || p
             x = Pq.find_by(uin: p)
             puts x
             pqs_array.push(x)
           end
-        }
+        } unless @pqs_list.nil?
         pqs_array
 
     end
--- spec/lib/export/pq_selection_spec.rb
+++ spec/lib/export/pq_selection_spec.rb
@@ -5,7 +5,7 @@ describe Export::PqSelection do
   include CSVHelpers
 
   let(:export) {
-    Export::PqSelection.new(Date.yesterday - 2.days, Date.today)
+    Export::PqSelection.new(Date.yesterday - 2.days, Date.today, 'uin-1,uin-3')
   }
 
   context "when no records are present" do
@@ -16,19 +16,16 @@ describe Export::PqSelection do
 
   context "when some records are present" do
     before(:each) do
-      # Expected exclusions
-      mk_pq('uin-1', tabled_date: Date.today + 5)
+      mk_pq('uin-1', answer_submitted: nil, date_for_answer: Date.yesterday - 3)   # Expected result
       mk_pq('uin-2', tabled_date: Date.today - 5)
-      mk_pq('uin-3', answer_submitted: Date.today)
+      mk_pq('uin-3', answer_submitted: Date.today, answer_submitted: nil, date_for_answer: Date.yesterday)   # Expected result
       mk_pq('uin-4', pod_clearance: Date.today)
-
-      # Expected inclusions
       mk_pq('uin-z', answer_submitted: nil, date_for_answer: Date.today)
       mk_pq('uin-c', answer_submitted: nil, date_for_answer: Date.yesterday)
       mk_pq('uin-a', answer_submitted: nil, date_for_answer: Date.yesterday - 3)
     end
 
-    it "returns unanswered, and non transfered-out pqs, within the supplied date range ordered by date for answer" do
+    it "returns specified questions" do
       exported_pqs = decode_csv(export.to_csv).map do |h|
         [
           h['PIN'],
@@ -39,9 +36,8 @@ describe Export::PqSelection do
       end
 
       expect(exported_pqs).to eq([
-                                   ['uin-a', 'uin-a body text', date_s(Date.yesterday), date_s(Date.yesterday - 3)],
-                                   ['uin-c', 'uin-c body text', date_s(Date.yesterday), date_s(Date.yesterday)],
-                                   ['uin-z', 'uin-z body text', date_s(Date.yesterday), date_s(Date.today)]
+                                   ['uin-1', 'uin-1 body text', date_s(Date.yesterday), date_s(Date.yesterday - 3)],
+                                   ['uin-3', 'uin-3 body text', date_s(Date.yesterday), date_s(Date.yesterday)]
                                  ])
     end
   end
