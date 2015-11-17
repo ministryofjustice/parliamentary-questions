2eb2a5d312514320015ec300786ad086a4614a1d
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2015-11-17 13:42:27 +0000

    More data for report

--- lib/gecko_report.rb
+++ lib/gecko_report.rb
@@ -13,18 +13,18 @@ class GeckoReport
   def write_report
     add_item('total_questions_ytd', Pq.total_questions_ytd.count)
 
-    add_item('commons_last_week_ordinary', Pq.commons.imported_last_week.ordinary.count)
-    add_item('commons_prev_week_ordinary', Pq.commons.imported_prev_week.ordinary.count)
+    add_item('import_commons_last_week_ordinary', Pq.commons.imported_last_week.ordinary.count)
+    add_item('import_commons_prev_week_ordinary', Pq.commons.imported_prev_week.ordinary.count)
 
-    add_item('commons_last_week_named_day', Pq.commons.imported_last_week.named_day.count)
-    add_item('commons_prev_week_named_day', Pq.commons.imported_prev_week.named_day.count)
+    add_item('import_commons_last_week_named_day', Pq.commons.imported_last_week.named_day.count)
+    add_item('import_commons_prev_week_named_day', Pq.commons.imported_prev_week.named_day.count)
 
-    add_item('commons_last_week', Pq.commons.imported_last_week.count)
-    add_item('commons_prev_week', Pq.commons.imported_prev_week.count)
+    add_item('import_commons_last_week', Pq.commons.imported_last_week.count)
+    add_item('import_commons_prev_week', Pq.commons.imported_prev_week.count)
 
 
-    add_item('lords_last_week_ordinary', Pq.lords.imported_last_week.ordinary.count)
-    add_item('lords_prev_week_ordinary', Pq.lords.imported_prev_week.ordinary.count)
+    add_item('import_lords_last_week_ordinary', Pq.lords.imported_last_week.ordinary.count)
+    add_item('import_lords_prev_week_ordinary', Pq.lords.imported_prev_week.ordinary.count)
 
     add_item('hoc_ord_ans_by_dead_last_week', Pq.commons.ordinary.answered_by_deadline_last_week.count)
     add_item('hoc_nmd_ans_by_dead_last_week', Pq.commons.named_day.answered_by_deadline_last_week.count)
