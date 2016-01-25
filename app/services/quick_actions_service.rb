dc136fc5736e268f243c91f16dd16a69c3d96582
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2016-01-25 15:58:57 +0000

    Start Service Test

---
+++ app/services/quick_actions_service.rb
@@ -0,0 +1,8 @@
+class QuickActionsService
+
+  def valid?(pq_list, internal_deadline, date_for_answer)
+    false
+  end
+
+
+end
---
+++ spec/services/quick_actions_service_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+
+describe 'valid' do
+
+  it 'should check pqs' do
+
+    true.should == false
+  end
+end
