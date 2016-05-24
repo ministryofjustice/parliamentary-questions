ca1e7026825e077e1174f4c13ad2a6958873f9a7
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2016-05-24 11:48:24 +0100

    no message

--- features/dashboard_inprogress_filters_spec.rb
+++ features/dashboard_inprogress_filters_spec.rb
@@ -20,6 +20,93 @@ feature "dashboard/in_progress filtering:", js: true, suspend_cleaner: true do
     DatabaseCleaner.clean
   end
 
+  def setup_questions
+    pq1 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-1', date_for_answer: Date.today+16, internal_deadline: Date.today+14, minister_id: 3, policy_minister_id: 6, question_type: 'NamedDay' )
+    pq2 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-2', date_for_answer: Date.today+15, internal_deadline: Date.today+13, minister_id: 3, policy_minister_id: 4, question_type: 'NamedDay' )
+    pq3 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-3', date_for_answer: Date.today+14, internal_deadline: Date.today+12, minister_id: 3, policy_minister_id: 4, question_type: 'Ordinary' )
+    pq4 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-4', date_for_answer: Date.today+13, internal_deadline: Date.today+11, minister_id: 5, policy_minister_id: 4, question_type: 'NamedDay' )
+    pq5 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-5', date_for_answer: Date.today+12, internal_deadline: Date.today+10, minister_id: 5, policy_minister_id: 4, question_type: 'Ordinary')
+    pq6 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-6', date_for_answer: Date.today+11, internal_deadline: Date.today+9, minister_id: 3, policy_minister_id: 4, question_type: 'NamedDay' )
+    pq7 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-7', date_for_answer: Date.today+10, internal_deadline: Date.today+8, minister_id: 5, policy_minister_id: 6, question_type: 'Ordinary' )
+    pq8 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-8', date_for_answer: Date.today+9, internal_deadline: Date.today+7, minister_id: 3, policy_minister_id: 6, question_type: 'Ordinary' )
+    pq9 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-9', date_for_answer: Date.today+8, internal_deadline: Date.today+6, minister_id: 5, policy_minister_id: 6, question_type: 'Ordinary' )
+    pq10 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-10', date_for_answer: Date.today+7, internal_deadline: Date.today+5, minister_id: 5, policy_minister_id: 4, question_type: 'Ordinary' )
+    pq11 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-11', date_for_answer: Date.today+6, internal_deadline: Date.today+4, minister_id: 3, policy_minister_id: 6, question_type: 'Ordinary' )
+    pq12 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-12', date_for_answer: Date.today+5, internal_deadline: Date.today+3, minister_id: 5, policy_minister_id: 4, question_type: 'NamedDay' )
+    pq13 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-13', date_for_answer: Date.today+4, internal_deadline: Date.today+2, minister_id: 5, policy_minister_id: 6, question_type: 'NamedDay' )
+    pq14 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-14', date_for_answer: Date.today+3, internal_deadline: Date.today+1, minister_id: 3, policy_minister_id: 4, question_type: 'Ordinary' )
+    pq15 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-15', date_for_answer: Date.today+2, internal_deadline: Date.today, minister_id: 5, policy_minister_id: 6, question_type: 'NamedDay' )
+    pq16 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-16', date_for_answer:  Date.today+1, internal_deadline: Date.today-1, minister_id: 3, policy_minister_id: 6, question_type: 'NamedDay' )
+  end
+
+  def test_date (filterBox, id, date)
+    within(filterBox+'.filter-box'){ fill_in id, :with => date }
+  end
+
+  def test_checkbox(filterBox, category, term)
+    within(filterBox+'.filter-box'){
+      find_button(category).trigger('click')
+      choose(term)
+      within('.notice') { expect(page).to have_text('1 selected') }
+    }
+  end
+
+  def test_keywords(term)
+    fill_in 'keywords', :with => term
+  end
+
+  def clear_filter(filterName)
+    within(filterName+'.filter-box') {
+      find_button('Clear').trigger('click')
+      expect(page).not_to have_text('1 selected')
+    }
+  end
+
+  def all_pqs_visible
+    within('.questions-list'){
+      find('li#pq-frame-16').visible?
+      find('li#pq-frame-16').visible?
+      find('li#pq-frame-15').visible?
+      find('li#pq-frame-14').visible?
+      find('li#pq-frame-13').visible?
+      find('li#pq-frame-12').visible?
+      find('li#pq-frame-11').visible?
+      find('li#pq-frame-10').visible?
+      find('li#pq-frame-9').visible?
+      find('li#pq-frame-8').visible?
+      find('li#pq-frame-7').visible?
+      find('li#pq-frame-6').visible?
+      find('li#pq-frame-5').visible?
+      find('li#pq-frame-4').visible?
+      find('li#pq-frame-3').visible?
+      find('li#pq-frame-2').visible?
+      find('li#pq-frame-1').visible?
+    }
+  end
+
+  def all_pqs_hidden
+    within('.questions-list'){
+      expect(page).not_to have_selector('li#pq-frame-16')
+      expect(page).not_to have_selector('li#pq-frame-15')
+      expect(page).not_to have_selector('li#pq-frame-14')
+      expect(page).not_to have_selector('li#pq-frame-13')
+      expect(page).not_to have_selector('li#pq-frame-12')
+      expect(page).not_to have_selector('li#pq-frame-11')
+      expect(page).not_to have_selector('li#pq-frame-10')
+      expect(page).not_to have_selector('li#pq-frame-9')
+      expect(page).not_to have_selector('li#pq-frame-8')
+      expect(page).not_to have_selector('li#pq-frame-7')
+      expect(page).not_to have_selector('li#pq-frame-6')
+      expect(page).not_to have_selector('li#pq-frame-5')
+      expect(page).not_to have_selector('li#pq-frame-4')
+      expect(page).not_to have_selector('li#pq-frame-3')
+      expect(page).not_to have_selector('li#pq-frame-2')
+      expect(page).not_to have_selector('li#pq-frame-1')
+    }
+  end
+
+  # ======================================================================== #
+
   scenario '1) Test Date for Answer: From today -10 days.' do
     within('#count'){expect(page).to have_text('16 parliamentary questions')}
     all_pqs_visible
@@ -2034,89 +2121,5 @@ feature "dashboard/in_progress filtering:", js: true, suspend_cleaner: true do
     all_pqs_visible
   end
 
-  def setup_questions
-    pq1 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-1', date_for_answer: Date.today+16, internal_deadline: Date.today+14, minister_id: 3, policy_minister_id: 6, question_type: 'NamedDay' )
-    pq2 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-2', date_for_answer: Date.today+15, internal_deadline: Date.today+13, minister_id: 3, policy_minister_id: 4, question_type: 'NamedDay' )
-    pq3 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-3', date_for_answer: Date.today+14, internal_deadline: Date.today+12, minister_id: 3, policy_minister_id: 4, question_type: 'Ordinary' )
-    pq4 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-4', date_for_answer: Date.today+13, internal_deadline: Date.today+11, minister_id: 5, policy_minister_id: 4, question_type: 'NamedDay' )
-    pq5 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-5', date_for_answer: Date.today+12, internal_deadline: Date.today+10, minister_id: 5, policy_minister_id: 4, question_type: 'Ordinary')
-    pq6 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-6', date_for_answer: Date.today+11, internal_deadline: Date.today+9, minister_id: 3, policy_minister_id: 4, question_type: 'NamedDay' )
-    pq7 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-7', date_for_answer: Date.today+10, internal_deadline: Date.today+8, minister_id: 5, policy_minister_id: 6, question_type: 'Ordinary' )
-    pq8 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-8', date_for_answer: Date.today+9, internal_deadline: Date.today+7, minister_id: 3, policy_minister_id: 6, question_type: 'Ordinary' )
-    pq9 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-9', date_for_answer: Date.today+8, internal_deadline: Date.today+6, minister_id: 5, policy_minister_id: 6, question_type: 'Ordinary' )
-    pq10 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-10', date_for_answer: Date.today+7, internal_deadline: Date.today+5, minister_id: 5, policy_minister_id: 4, question_type: 'Ordinary' )
-    pq11 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-11', date_for_answer: Date.today+6, internal_deadline: Date.today+4, minister_id: 3, policy_minister_id: 6, question_type: 'Ordinary' )
-    pq12 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-12', date_for_answer: Date.today+5, internal_deadline: Date.today+3, minister_id: 5, policy_minister_id: 4, question_type: 'NamedDay' )
-    pq13 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-13', date_for_answer: Date.today+4, internal_deadline: Date.today+2, minister_id: 5, policy_minister_id: 6, question_type: 'NamedDay' )
-    pq14 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-14', date_for_answer: Date.today+3, internal_deadline: Date.today+1, minister_id: 3, policy_minister_id: 4, question_type: 'Ordinary' )
-    pq15 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-15', date_for_answer: Date.today+2, internal_deadline: Date.today, minister_id: 5, policy_minister_id: 6, question_type: 'NamedDay' )
-    pq16 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-16', date_for_answer:  Date.today+1, internal_deadline: Date.today-1, minister_id: 3, policy_minister_id: 6, question_type: 'NamedDay' )
-  end
-
-  def test_date (filterBox, id, date)
-    within(filterBox+'.filter-box'){ fill_in id, :with => date }
-  end
-
-  def test_checkbox(filterBox, category, term)
-    within(filterBox+'.filter-box'){
-      find_button(category).trigger('click')
-      choose(term)
-      within('.notice') { expect(page).to have_text('1 selected') }
-    }
-  end
-
-  def test_keywords(term)
-    fill_in 'keywords', :with => term
-  end
-
-  def clear_filter(filterName)
-    within(filterName+'.filter-box') {
-      find_button('Clear').trigger('click')
-      expect(page).not_to have_text('1 selected')
-    }
-  end
-
-  def all_pqs_visible
-    within('.questions-list'){
-      find('li#pq-frame-16').visible?
-      find('li#pq-frame-16').visible?
-      find('li#pq-frame-15').visible?
-      find('li#pq-frame-14').visible?
-      find('li#pq-frame-13').visible?
-      find('li#pq-frame-12').visible?
-      find('li#pq-frame-11').visible?
-      find('li#pq-frame-10').visible?
-      find('li#pq-frame-9').visible?
-      find('li#pq-frame-8').visible?
-      find('li#pq-frame-7').visible?
-      find('li#pq-frame-6').visible?
-      find('li#pq-frame-5').visible?
-      find('li#pq-frame-4').visible?
-      find('li#pq-frame-3').visible?
-      find('li#pq-frame-2').visible?
-      find('li#pq-frame-1').visible?
-    }
-  end
-
-  def all_pqs_hidden
-    within('.questions-list'){
-      expect(page).not_to have_selector('li#pq-frame-16')
-      expect(page).not_to have_selector('li#pq-frame-15')
-      expect(page).not_to have_selector('li#pq-frame-14')
-      expect(page).not_to have_selector('li#pq-frame-13')
-      expect(page).not_to have_selector('li#pq-frame-12')
-      expect(page).not_to have_selector('li#pq-frame-11')
-      expect(page).not_to have_selector('li#pq-frame-10')
-      expect(page).not_to have_selector('li#pq-frame-9')
-      expect(page).not_to have_selector('li#pq-frame-8')
-      expect(page).not_to have_selector('li#pq-frame-7')
-      expect(page).not_to have_selector('li#pq-frame-6')
-      expect(page).not_to have_selector('li#pq-frame-5')
-      expect(page).not_to have_selector('li#pq-frame-4')
-      expect(page).not_to have_selector('li#pq-frame-3')
-      expect(page).not_to have_selector('li#pq-frame-2')
-      expect(page).not_to have_selector('li#pq-frame-1')
-    }
-  end
 
 end
