e81c97109772638c4ebf1c3059d4c2ce1c8a4fae
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2016-05-27 11:15:17 +0100

    Travis failure test 2

--- features/in_progress_filtering_tests_spec.rb
+++ features/in_progress_filtering_tests_spec.rb
@@ -31,6 +31,41 @@ feature "In progress page filtering:", js: true, suspend_cleaner: true do
     all_pqs(16, 'visible')
   end
 
+  scenario '2) by Date for Answer (From: 9 days time).' do
+    test_date('#date-for-answer', 'answer-from', Date.today+9)
+    within('#count'){expect(page).to have_text('8 parliamentary questions out of 16.')}
+    within('.questions-list'){
+      expect(page).not_to have_selector('li#pq-frame-16')
+      expect(page).not_to have_selector('li#pq-frame-15')
+      expect(page).not_to have_selector('li#pq-frame-14')
+      expect(page).not_to have_selector('li#pq-frame-13')
+      expect(page).not_to have_selector('li#pq-frame-12')
+      expect(page).not_to have_selector('li#pq-frame-11')
+      expect(page).not_to have_selector('li#pq-frame-10')
+      expect(page).not_to have_selector('li#pq-frame-9')
+      find('li#pq-frame-8').visible?
+      find('li#pq-frame-7').visible?
+      find('li#pq-frame-6').visible?
+      find('li#pq-frame-5').visible?
+      find('li#pq-frame-4').visible?
+      find('li#pq-frame-3').visible?
+      find('li#pq-frame-2').visible?
+      find('li#pq-frame-1').visible?
+    }
+    clear_filter('#date-for-answer')
+    within('#count'){expect(page).to have_text('16 parliamentary questions out of 16.')}
+    all_pqs(16, 'visible')
+  end
+
+  scenario '3) by Date for Answer (From: 20 days time).' do
+    test_date('#date-for-answer', 'answer-from', Date.today+20)
+    within('#count'){expect(page).to have_text('0 parliamentary questions out of 16.')}
+    all_pqs(16, 'hidden')
+    clear_filter('#date-for-answer')
+    within('#count'){expect(page).to have_text('16 parliamentary questions out of 16.')}
+    all_pqs(16, 'visible')
+  end
+
   def setup_questions
     pq1 = FactoryGirl.create( :with_pod_pq, uin: 'UIN-1', date_for_answer: Date.today+16, internal_deadline: Date.today+14, minister_id: 3, policy_minister_id: 6, question_type: 'NamedDay' )
     pq2 = FactoryGirl.create( :draft_pending_pq, uin: 'UIN-2', date_for_answer: Date.today+15, internal_deadline: Date.today+13, minister_id: 3, policy_minister_id: 4, question_type: 'NamedDay' )
