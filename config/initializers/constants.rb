7c5f5100d1d5da0b4fac87d18e18dac367543657
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2016-09-29 13:07:48 +0100

    Refactor - move Start of Parliamentary Session to a more appropriate initialiser

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,2 @@
 LARGEST_POSTGRES_INTEGER = 2147483647
-def parliament_session_start
-  Date.strptime("{ 2016, 5, 19 }", "{ %Y, %m, %d }")
-end
+
---
+++ config/initializers/geckoboard_stats_start.rb
@@ -0,0 +1,3 @@
+def parliament_session_start
+  Date.strptime("{ 2016, 5, 19 }", "{ %Y, %m, %d }")
+end
