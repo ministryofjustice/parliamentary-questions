e80c02657e23537c154552cd27d36a12220d7214
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2018-08-21 12:07:52 +0100

    Fix Travis CI failures
    - Forced git to track the vendor directory.
    - Stopped passing $phase to alpha/beta banners and the phase-tag mixin.

--- spec/controllers/ao_token_filter_spec.rb
+++ spec/controllers/ao_token_filter_spec.rb
@@ -9,10 +9,10 @@ describe 'AOTokenFilter' do
   describe '.validate_token' do
     it 'the filter should say no access if you dont have a valid token' do
       controller = double('ApplicationController')
-      request = double('request')
+      request    = double('request')
 
       allow(request).to receive(:path) { '/test/this' }
-      allow(controller).to receive(:params) { {token: 'mytoken', entity: 'test'} }
+      allow(controller).to receive(:params) { { token: 'mytoken', entity: 'test' } }
       allow(controller).to receive(:request) { request }
 
       expect(AOTokenFilter.validate_token(controller)).to eq :invalid
@@ -20,13 +20,13 @@ describe 'AOTokenFilter' do
 
     it 'the filter should pass if you have a valid token' do
       controller = double('ApplicationController')
-      request = double('request')
-      path = '/my/valid/path'
-      entity = 'ao@justice.com'
-      token = @token_service.generate_token(path, entity, DateTime.now.at_end_of_day)
+      request    = double('request')
+      path       = '/my/valid/path'
+      entity     = 'ao@justice.com'
+      token      = @token_service.generate_token(path, entity, DateTime.now.at_end_of_day)
 
       allow(request).to receive(:path) { path }
-      allow(controller).to receive(:params) { {token: token, entity: entity} }
+      allow(controller).to receive(:params) { { token: token, entity: entity } }
       allow(controller).to receive(:request) { request }
 
       expect(AOTokenFilter.validate_token(controller)).to eq :valid
@@ -35,13 +35,13 @@ describe 'AOTokenFilter' do
 
     it 'the filter should fail if the token is expired' do
       controller = double('ApplicationController')
-      request = double('request')
-      path = '/my/valid/path'
-      entity = 'ao@justice.com'
-      token = @token_service.generate_token(path, entity, 20.minutes.ago)
+      request    = double('request')
+      path       = '/my/valid/path'
+      entity     = 'ao@justice.com'
+      token      = @token_service.generate_token(path, entity, 20.minutes.ago)
 
       allow(request).to receive(:path) { path }
-      allow(controller).to receive(:params) { {token: token, entity: entity} }
+      allow(controller).to receive(:params) { { token: token, entity: entity } }
       allow(controller).to receive(:request) { request }
 
       expect(AOTokenFilter.validate_token(controller)).to eq :expired
@@ -49,7 +49,7 @@ describe 'AOTokenFilter' do
 
     it 'the filter should say no access if you provide empty token' do
       controller = double('ApplicationController')
-      request = double('request')
+      request    = double('request')
 
       allow(request).to receive(:path) { '/test/this' }
       allow(controller).to receive(:params) { { entity: 'test'} }
@@ -60,7 +60,7 @@ describe 'AOTokenFilter' do
 
     it 'the filter should say no access if you provide empty entity' do
       controller = double('ApplicationController')
-      request = double('request')
+      request    = double('request')
 
       allow(request).to receive(:path) { '/test/this' }
       allow(controller).to receive(:params) { { token: 'test'} }
@@ -69,13 +69,13 @@ describe 'AOTokenFilter' do
 
     it 'the filter should say no access if you have a are in the wrong path' do
       controller = double('ApplicationController')
-      request = double('request')
-      path = '/my/valid/path'
-      entity = 'ao@justice.com'
-      token = @token_service.generate_token(path, entity, DateTime.now.at_end_of_day)
+      request    = double('request')
+      path       = '/my/valid/path'
+      entity     = 'ao@justice.com'
+      token      = @token_service.generate_token(path, entity, DateTime.now.at_end_of_day)
 
       allow(request).to receive(:path) { '/other' }
-      allow(controller).to receive(:params) { {token: token, entity: entity} }
+      allow(controller).to receive(:params) { { token: token, entity: entity } }
       allow(controller).to receive(:request) { request }
 
       expect(AOTokenFilter.validate_token(controller)).to eq :invalid
@@ -83,9 +83,9 @@ describe 'AOTokenFilter' do
   end
 
   describe '.before' do
-    let(:controller)      { double('controller') }
-    let(:user)            { double(User) }
-    let(:request)         { double('request') }
+    let(:controller) { double('controller') }
+    let(:user)       { double(User) }
+    let(:request)    { double('request') }
 
     it 'should not write an error log if valid' do
       expect(AOTokenFilter).to receive(:validate_token).and_return(:valid)
