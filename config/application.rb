6fb27a50906d7d7a1d99e436896b7a9c86ba6569
Daniel Penny <danielatdigital@users.noreply.github.com>
2018-11-15 15:17:44 +0000

    Add further folders

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,12 @@ module ParliamentaryQuestions
 
     #config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.eager_load_paths << Rails.root.join("app", "assets", "fonts")
+    config.eager_load_paths += %w[ app/assets/images ]
+    config.eager_load_paths += %w[ app/assets/javascripts ]
+    config.eager_load_paths += %w[ app/assets/stylesheets ]
+    config.eager_load_paths += %w[ app/assets/stylesheets/moj ]
+    config.eager_load_paths += %w[ app/assets/stylesheets/vendor ]
+
 
     config.generators do |g|
       g.template_engine :erb
