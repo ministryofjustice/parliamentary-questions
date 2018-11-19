6adca18b0ed3735854e499713627b6b38dd6ab59
Daniel Penny <danielatdigital@users.noreply.github.com>
2018-11-19 11:56:46 +0000

    Add file types to precompilation
    For Rails 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ ParliamentaryQuestions::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(.svg .eot .woff .ttf)
-
+  config.assets.precompile += %w(.svg .eot .woff .ttf *.js ^[^_]*.css)
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
