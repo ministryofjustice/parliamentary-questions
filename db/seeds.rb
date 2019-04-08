398c76e25ef7730f36ae20e1572f3c6fe99e3422
npreddy <n_preddy@hotmail.com>
2019-04-08 16:50:50 +0100

    Added random names to DB test junk data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,12 +33,12 @@ progresses = Progress.create([
                              ])
 
 ministers = Minister.create!([
-                               { name: 'Chris Grayling', title: 'Secretary of State and Lord High Chancellor of Great Britain' },
-                               { name: 'Damian Green (MP)', title: 'Minister of State' },
-                               { name: 'Jeremy Wright (MP)', title: 'Parliamentary Under-Secretary of State; Minister for Prisons and Rehabilitation' },
-                               { name: 'Shailesh Vara (MP)', title: 'Parliamentary Under-Secretary of State' },
-                               { name: 'Simon Hughes (MP)',  title: 'Minister of State for Justice & Civil Liberties' },
-                               { name: 'Lord Faulks QC', title: 'Lord Faulks QC, Minister of State' }
+                               { name: 'Dimple Machado', title: 'Secretary of State and Lord High Chancellor of Great Britain' },
+                               { name: 'Herma Goble (MP)', title: 'Minister of State' },
+                               { name: 'Teddy Pool (MP)', title: 'Parliamentary Under-Secretary of State; Minister for Prisons and Rehabilitation' },
+                               { name: 'Ligia Dowdy (MP)', title: 'Parliamentary Under-Secretary of State' },
+                               { name: 'Son Putnam (MP)',  title: 'Minister of State for Justice & Civil Liberties' },
+                               { name: 'Belkis Naranjo QC', title: 'Minister of State' }
                              ])
 
 directorates = Directorate.create!([
@@ -73,25 +73,25 @@ divisions = Division.create!([
                              ])
 
 deputy_directors = DeputyDirector.create!([
-                                            { division_id: 1, email: 'dd1@email.com', name: 'Craig Watkins' },
-                                            { division_id: 2, email: 'dd2@email.com', name: 'Rebecca Endean' },
-                                            { division_id: 3, email: 'dd3@email.com', name: 'Procurement' },
-                                            { division_id: 4, email: 'dd4@email.com', name: 'Jonathon Sedgwick' },
-                                            { division_id: 5, email: 'dd5@email.com', name: 'Helen Judge' },
-                                            { division_id: 6, email: 'dd6@email.com', name: 'Paul Kett' },
-                                            { division_id: 7, email: 'dd7@email.com', name: 'Ian Poree' },
-                                            { division_id: 8, email: 'dd8@email.com', name: 'Darren Tierney' },
-                                            { division_id: 9, email: 'dd9@email.com', name: 'Mark Sweeney' },
-                                            { division_id: 10, email: 'dd10@email.com',  name: 'Vacant' },
-                                            { division_id: 11, email: 'dd11@email.com',  name: 'Pat Lloyd' },
-                                            { division_id: 12, email: 'dd12@email.com',  name: 'Pam Teare' },
-                                            { division_id: 13, email: 'dd13@email.com',  name: 'Carol Carpenter' },
-                                            { division_id: 14, email: 'dd14@email.com',  name: 'Phil Copple' },
-                                            { division_id: 15, email: 'dd15@email.com',  name: 'Kevin Sadler' },
-                                            { division_id: 16, email: 'dd16@email.com',  name: 'Guy Tompkins' },
-                                            { division_id: 17, email: 'dd17@email.com',  name: 'Paul Shipley' },
-                                            { division_id: 18, email: 'dd18@email.com',  name: 'Gerry Smith' },
-                                            { division_id: 19, email: 'dd19@email.com',  name: 'Nick Ramsey' }
+                                            { division_id: 1, email: 'dd1@email.com', name: 'Dollie Christenson' },
+                                            { division_id: 2, email: 'dd2@email.com', name: 'Logan Lavoie' },
+                                            { division_id: 3, email: 'dd3@email.com', name: 'Aja Song' },
+                                            { division_id: 4, email: 'dd4@email.com', name: 'Rory Otis' },
+                                            { division_id: 5, email: 'dd5@email.com', name: 'Myrtice Mattox' },
+                                            { division_id: 6, email: 'dd6@email.com', name: 'Ozella Yoo' },
+                                            { division_id: 7, email: 'dd7@email.com', name: 'Francoise Mcalister' },
+                                            { division_id: 8, email: 'dd8@email.com', name: 'Romeo Pipkin' },
+                                            { division_id: 9, email: 'dd9@email.com', name: 'Kyra Orr' },
+                                            { division_id: 10, email: 'dd10@email.com',  name: 'Kaycee Bonner' },
+                                            { division_id: 11, email: 'dd11@email.com',  name: 'Issac Otto' },
+                                            { division_id: 12, email: 'dd12@email.com',  name: 'Nohemi Mullen' },
+                                            { division_id: 13, email: 'dd13@email.com',  name: 'Bree Fries' },
+                                            { division_id: 14, email: 'dd14@email.com',  name: 'Ashton Valerio' },
+                                            { division_id: 15, email: 'dd15@email.com',  name: 'Jesus Staton' },
+                                            { division_id: 16, email: 'dd16@email.com',  name: 'Jere Huggins' },
+                                            { division_id: 17, email: 'dd17@email.com',  name: 'Buddy Kent' },
+                                            { division_id: 18, email: 'dd18@email.com',  name: 'Benito Canales' },
+                                            { division_id: 19, email: 'dd19@email.com',  name: 'Hyman Quinones' }
                                           ])
 
 PressDesk.create!([
@@ -100,21 +100,21 @@ PressDesk.create!([
                   ])
 
 PressOfficer.create!([
-                       { name: 'press officer one', email: 'one@press.office.com', press_desk_id: 1 },
-                       { name: 'press officer two', email: 'two@press.office.com', press_desk_id: 1 },
-                       { name: 'press officer three', email: 'three@press.office.com', press_desk_id: 2 },
-                       { name: 'press officer four', email: 'four@press.office.com', press_desk_id: 2 }
+                       { name: 'Vertie Engel ', email: 'po-one@press.office.com', press_desk_id: 1 },
+                       { name: 'Bok Mcgehee', email: 'po-two@press.office.com', press_desk_id: 1 },
+                       { name: 'Lawanna Whitcomb', email: 'po-three@press.office.com', press_desk_id: 2 },
+                       { name: 'Delmer Gaskin', email: 'po-four@press.office.com', press_desk_id: 2 }
                      ])
 
 action_officers = ActionOfficer.create!(
   [
-    { deputy_director_id: 17, name: 'Colin Bruce', email: 'colin.bruce@digital.justice.gov.uk', press_desk_id: 1 },
+    { deputy_director_id: 17, name: 'Nick Preddy', email: 'nick.preddy@digital.justice.gov.uk', press_desk_id: 1 },
     { deputy_director_id: 19, name: 'Daniel Penny', email: 'daniel.penny@digital.justice.gov.uk', press_desk_id: 1 },
-    { deputy_director_id: 6, name: 'David Hernandez', email: 'david.hernandez@digital.justice.gov.uk', press_desk_id: 1 },
-    { deputy_director_id: 7, name: 'Tehseen Udin', email: 'tehseen.udin@digital.justice.gov.uk', press_desk_id: 1 },
-    { deputy_director_id: 8, name: 'Tom Wynne-Morgan', email: 'tom.wynne-morgan@digital.justice.gov.uk', press_desk_id: 2 },
-    { deputy_director_id: 9, name: 'Tom Norman', email: 'tom.norman@digital.justice.gov.uk', press_desk_id: 2 },
-    { deputy_director_id: 10, name: 'Mary Henley', email: 'mary.henley@digital.justice.gov.uk', press_desk_id: 2 }
+    { deputy_director_id: 6, name: 'Opal Moody', email: 'ao-six@email.com', press_desk_id: 1 },
+    { deputy_director_id: 7, name: 'Doug Sherman', email: 'ao-seven@email.com', press_desk_id: 1 },
+    { deputy_director_id: 8, name: 'Loretta Floyd', email: 'ao-eight@email.com', press_desk_id: 2 },
+    { deputy_director_id: 9, name: 'Clyde Medina', email: 'ao-nine@email.com', press_desk_id: 2 },
+    { deputy_director_id: 10, name: 'Marsha Huff ', email: 'ao-ten@email.com', press_desk_id: 2 }
   ]
 )
 
