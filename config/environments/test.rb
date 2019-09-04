44ed5bac6b8dbec799440e4edeec7880419167a0
beccapearce <rebecca.pearce.92@gmail.com>
2019-09-04 11:00:05 +0100

    add SMTP from variable to test env for circle

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ ParliamentaryQuestions::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_options = { from: 'localhost' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
