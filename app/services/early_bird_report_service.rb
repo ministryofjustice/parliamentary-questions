03da28e1f9b4897788754af127d4caab7cef6b81
beccapearce <rebecca.pearce.92@gmail.com>
2020-01-06 15:59:11 +0000

    Send earlybird and watchlist iterating with .each instead of cc

--- app/services/early_bird_report_service.rb
+++ app/services/early_bird_report_service.rb
@@ -13,19 +13,22 @@ class EarlyBirdReportService
   def notify_early_bird
     end_of_day = @current_time.end_of_day
     token      = @token_service.generate_token(early_bird_dashboard_path, entity, end_of_day)
-    cc         = EarlyBirdMember.active.pluck(:email).join(';')
+    recipients = EarlyBirdMember.active.pluck(:email)
     template   = {
       name: 'early_bird',
       entity: entity,
       email: 'pqtest@digital.justice.gov.uk',
       token: token,
-      cc: cc
+      recipients: recipients
     }
 
-    $statsd.increment "#{StatsHelper::TOKENS_GENERATE}.earlybird"
-    LogStuff.tag(:mailer_early_bird) do
-      LogStuff.info { "Early bird  email to #{template[:email]} (name #{template[:name]}) [CCd to #{template[:cc]}]" }
-      NotifyPqMailer.early_bird_email(email: cc, token: token, entity: entity).deliver_now
+    recipients.each do |recipient|
+
+      $statsd.increment "#{StatsHelper::TOKENS_GENERATE}.earlybird"
+      LogStuff.tag(:mailer_early_bird) do
+        LogStuff.info { "Early bird  email to #{template[:email]} (name #{template[:name]}) [CCd to #{template[:recipient]}]" }
+        NotifyPqMailer.early_bird_email(email: recipient, token: token, entity: entity).deliver_now
+      end
     end
     token
   end
