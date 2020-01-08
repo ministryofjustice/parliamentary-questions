278e058bb0af9cf10a424db09bb0a1cf2c463ff0
beccapearce <rebecca.pearce.92@gmail.com>
2020-01-08 15:56:57 +0000

    tidy iterative email sending

--- app/services/early_bird_report_service.rb
+++ app/services/early_bird_report_service.rb
@@ -13,20 +13,12 @@ class EarlyBirdReportService
   def notify_early_bird
     end_of_day = @current_time.end_of_day
     token      = @token_service.generate_token(early_bird_dashboard_path, entity, end_of_day)
-    recipients = EarlyBirdMember.active.pluck(:email)
-    template   = {
-      name: 'early_bird',
-      entity: entity,
-      email: 'pqtest@digital.justice.gov.uk',
-      token: token,
-      recipients: recipients
-    }
+    recipients = EarlyBirdMember.active.pluck(:email) << 'pqtest@digital.justice.gov.uk'
 
     recipients.each do |recipient|
-
       $statsd.increment "#{StatsHelper::TOKENS_GENERATE}.earlybird"
       LogStuff.tag(:mailer_early_bird) do
-        LogStuff.info { "Early bird  email to #{template[:email]} (name #{template[:name]}) [CCd to #{template[:recipient]}]" }
+        LogStuff.info { "Early bird  email to pqtest@digital.justice.gov.uk} (name early_bird) [CCd to #{recipients.join(';')}]" }
         NotifyPqMailer.early_bird_email(email: recipient, token: token, entity: entity).deliver_now
       end
     end
