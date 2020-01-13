596f1bca4add9dc2f4b7fbde5f230993f06c906a
njpreddy <nick.preddy@digital.justice.gov.uk>
2020-01-13 17:34:04 +0000

    Removed spaces and changed cron job start time.

--- lib/rake_task_helpers/db_trim.rb
+++ lib/rake_task_helpers/db_trim.rb
@@ -1,6 +1,6 @@
 module RakeTaskHelpers
   class DBTrim
-    MY_SQLS = [ "DELETE FROM pqs WHERE id NOT IN (SELECT id FROM pqs ORDER BY id DESC LIMIT 50);" ]
+    MY_SQLS = ['DELETE FROM pqs WHERE id NOT IN (SELECT id FROM pqs ORDER BY id DESC LIMIT 50);']
     def run!
       MY_SQLS.map do |sql|
         ActiveRecord::Base.connection.execute sql
