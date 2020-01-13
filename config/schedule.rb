721e080348ac018520822d15178ac577d7684532
njpreddy <nick.preddy@digital.justice.gov.uk>
2020-01-13 17:24:47 +0000

    Trim and anonymise staging questions
    This cron jobs calls three rake task helpers which:
    - Trims the database to the 50 mosts recent questions.
    -  Anonymises all the database email addresses.
    - Generates a test user.

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,46 +4,6 @@
 # http://en.wikipedia.org/wiki/Cron
 # Learn more: http://github.com/javan/whenever
 #
-set :output, 'log/schedule.log'
-job_type :rake,  'cd :path && RAILS_ENV=production bundle exec rake :task :output'
-#
-# PQ API Nightly Import
-#
-every 1.day, at: '4:00 am' do
-  rake 'pqa:nightly_import'
-end
+# set :output, 'log/schedule.log'
+# job_type :rake,  'cd :path && RAILS_ENV=production bundle exec rake :task :output'
 
-every 1.day, at: '6:00 am' do
-  rake 'pqa:nightly_import'
-end
-#
-# PQ Early Bird email (schedule after the first of the nightly imports)
-#
-every :weekday, at: '5:30 am' do
-  rake 'pqa:early_bird'
-  if ENV['ENV'] =~ /prod/
-    rake 'pqa:early_bird'
-    `echo "Running Earlybird, environment is PROD $(date)" >> /tmp/cron_check`
-  else
-    `echo "Not running Earlybird, environment is not PROD $(date)" >> /tmp/cron_check`
-  end
-end
-#
-# Sanitize imported staging data
-# Follows import job staging -> production run via crontab
-#
-every 1.day, at: '1:00 am' do
-  rake 'db:staging:sync'
-end
-#
-# Trigger mail queue poll and new email sending
-#
-every 1.minute do
-  rake 'email:process_queue'
-end
-#
-# Run smoke tests every hour
-#
-every 1.hour do
-  rake 'smoke_test:run'
-end
