dcfbfa687e6471fc173a4458df00c8ce2a6b00c3
njpreddy <nick.preddy@digital.justice.gov.uk>
2020-01-14 15:23:38 +0000

    Re-enable smoke-test schedule.

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,5 +4,11 @@
 # http://en.wikipedia.org/wiki/Cron
 # Learn more: http://github.com/javan/whenever
 #
-# set :output, 'log/schedule.log'
-# job_type :rake,  'cd :path && RAILS_ENV=production bundle exec rake :task :output'
+set :output, 'log/schedule.log'
+job_type :rake,  'cd :path && RAILS_ENV=production bundle exec rake :task :output'
+#
+# Run smoke tests every hour
+#
+every 1.hour do
+  rake 'smoke_test:run'
+end
