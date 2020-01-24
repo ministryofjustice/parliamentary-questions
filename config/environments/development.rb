11fa32ab03ac4a09e88a4ab08d47963f88ff0af3
beccapearce <rebecca.pearce.92@gmail.com>
2020-01-24 16:01:03 +0000

    Fix url in email and sending host regex
    The URL generated for the emails included a port number from the
    ActionBaseMailer settings in the config environments, this removes
    where it is set.
    
    Also fixes an issue where HostEnv.is_live? returns true for development
    due to the regex ignoring the development prefix in the url. This caused
    the migrations to error when deploying to development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ ParliamentaryQuestions::Application.configure do
     ActionMailer::Base.delivery_method = :sendmail
     ActionMailer::Base.default from: Settings.mail_from
     ActionMailer::Base.default reply_to: Settings.mail_reply_to
-    ActionMailer::Base.default_url_options = { host: sending_host, protocol: 'http', port: '3000' }
+    ActionMailer::Base.default_url_options = { host: sending_host, protocol: 'http' }
     ActionMailer::Base.smtp_settings = {
       address: ENV['SMTP_HOSTNAME'] || 'localhost',
       authentication: :login,
