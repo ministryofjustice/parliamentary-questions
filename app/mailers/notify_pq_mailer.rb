67b0cc502dcd67609fc02dfe8b32ea399bd6f763
njpreddy <nick.preddy@digital.justice.gov.uk>
2020-02-04 12:18:28 +0000

    Update notify_pq_mailer.rb
    Add empty comment.

--- app/mailers/notify_pq_mailer.rb
+++ app/mailers/notify_pq_mailer.rb
@@ -93,6 +93,8 @@ class NotifyPqMailer < GovukNotifyRails::Mailer
     mail(to: email)
   end
 
+  # empty comment
+
   def watchlist_email(email:, token:, entity:)
     set_template('b452ebb8-c49e-46f6-9da5-3ba28b494ed6')
     set_personalisation(
