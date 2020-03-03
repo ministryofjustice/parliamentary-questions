e6a3a2ac02e2da74d5da520b5a6b65a09e5a19f7
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2020-03-03 12:28:02 +0000

    Update database.rb (#543)
    * Update database.rb
    
    Set 'available?' method to always return true.
    
    * Update database.rb
    * Comment out some tests that expect DB connection failure.

--- spec/health_check/database_spec.rb
+++ spec/health_check/database_spec.rb
@@ -8,10 +8,10 @@ describe HealthCheck::Database do
       expect(db).to be_available
     end
 
-    it 'returns false if the database is not available' do
-      allow(ActiveRecord::Base).to receive(:connected?).and_return(false)
-      expect(db).not_to be_available
-    end
+    # it 'returns false if the database is not available' do
+    #   allow(ActiveRecord::Base).to receive(:connected?).and_return(false)
+    #   expect(db).not_to be_available
+    # end
   end
 
   context '#accessible?' do
@@ -19,29 +19,29 @@ describe HealthCheck::Database do
       expect(db).to be { :accessible? }
     end
 
-    it 'returns false if the database is not accessible with our credentials' do
-      allow(db).to receive(:execute_simple_select_on_database).and_raise(PG::ConnectionBad.new('Database has gone away'))
-      result = db.accessible?
-      expect(result).to be false
-    end
+    # it 'returns false if the database is not accessible with our credentials' do
+    #   allow(db).to receive(:execute_simple_select_on_database).and_raise(PG::ConnectionBad.new('Database has gone away'))
+    #   result = db.accessible?
+    #   expect(result).to be false
+    # end
   end
 
-  context '#error_messages' do
-    it 'returns the exception messages if there is an error accessing the database' do
-      allow(ActiveRecord::Base).to receive(:connected?).and_return(false)
-      db.available?
+  # context '#error_messages' do
+  #   it 'returns the exception messages if there is an error accessing the database' do
+  #     allow(ActiveRecord::Base).to receive(:connected?).and_return(false)
+  #     db.available?
 
-      expect(db.error_messages).to eq([
-                                        'Database Error: could not connect to parliamentary-questions_test ' \
-                                        'on localhost using postgresql'
-                                      ])
-    end
+  #     expect(db.error_messages).to eq([
+  #                                       'Database Error: could not connect to parliamentary-questions_test ' \
+  #                                       'on localhost using postgresql'
+  #                                     ])
+  #   end
 
-    it 'returns an error an backtrace for errors not specific to a component' do
-      allow(ActiveRecord::Base).to receive(:connected?).and_raise(StandardError)
-      db.available?
+  #   it 'returns an error an backtrace for errors not specific to a component' do
+  #     allow(ActiveRecord::Base).to receive(:connected?).and_raise(StandardError)
+  #     db.available?
 
-      expect(db.error_messages.first).to match(/Error: StandardError\nDetails/)
-    end
-  end
+  #     expect(db.error_messages.first).to match(/Error: StandardError\nDetails/)
+  #   end
+  # end
 end
