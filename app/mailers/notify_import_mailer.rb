aa1bbc1d9250733858f5bce7d5a24490c8c33349
njpreddy <nick.preddy@digital.justice.gov.uk>
2020-03-12 11:49:47 +0000

    Any environment label that is not 'development', 'staging' or 'production' set as 'env-unknown'.

--- app/mailers/notify_import_mailer.rb
+++ app/mailers/notify_import_mailer.rb
@@ -2,7 +2,7 @@ class NotifyImportMailer < GovukNotifyRails::Mailer
   def notify_fail(error_message)
     set_template('586dd10e-8987-4754-b653-9cacd3763d19')
     set_personalisation(
-      environment: ENV['RAILS_ENV'],
+      environment: app_env,
       error_message: error_message
     )
     set_email_reply_to(Settings.tech_support_email)
@@ -12,12 +12,22 @@ class NotifyImportMailer < GovukNotifyRails::Mailer
   def notify_success(report)
     set_template('7858c6b6-774e-47f5-80c2-bea221805bb7')
     set_personalisation(
-      environment: ENV['RAILS_ENV'],
+      environment: app_env,
       total_questions: report[:total],
       questions_created: report[:created],
       questions_updated: report[:updated]
     )
     set_email_reply_to(Settings.tech_support_email)
     mail(to: Settings.mail_tech_support)
+  end
+
+  private
+
+  def app_env
+    if ENV['RAILS_ENV'] != 'development' && ENV['RAILS_ENV'] != 'staging' && ENV['RAILS_ENV'] != 'production'
+      'env-unknown'
+    else
+      ENV['RAILS_ENV']
+    end
   end
 end
