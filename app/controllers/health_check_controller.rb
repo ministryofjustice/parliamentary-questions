4494550be878c9d148317703d803ee66f4129038
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2021-05-25 22:42:47 +0100

    Turn off silence for healtcheck

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -2,14 +2,12 @@ class HealthCheckController < ApplicationController
   respond_to :json
 
   def index
-    Rails.logger.silence do
-      report = HealthCheckService.new.report
+    report = HealthCheckService.new.report
 
-      if report.status == '200'
-        render(json: report)
-      else
-        render(json: report, status: '500')
-      end
+    if report.status == '200'
+      render(json: report)
+    else
+      render(json: report, status: '500')
     end
   end
 end
