60a90efab6732d164c4a223fa82b10ad257f4717
Ian Anderson <ian.anderson@digital.justice.gov.uk>
2021-05-26 12:54:29 +0100

    CT-3650 investigate pod restarts and instability  (#725)
    * Turn off silence for healtcheck
    
    * Use Sentry in all namespaces if DSN is configured
    
    * Stop using /healthcheck for probes and set timeout explicitly

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
-if Rails.env.production? && ENV['SENTRY_DSN'].present?
+if ENV['SENTRY_DSN'].present?
   Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
     config.breadcrumbs_logger = [:active_support_logger]
