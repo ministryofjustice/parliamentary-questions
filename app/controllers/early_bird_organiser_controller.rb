d80c4af80fdd0a9b5b940ee92575fd1af09a7e01
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-06-24 13:47:56 +0100

    Add message that informs user that early bird is deactivated

--- app/controllers/early_bird_organiser_controller.rb
+++ app/controllers/early_bird_organiser_controller.rb
@@ -3,6 +3,16 @@ class EarlyBirdOrganiserController < ApplicationController
 
   def new
     @early_bird_organiser = EarlyBirdOrganiser.new
+
+    @previous_early_bird = EarlyBirdOrganiser.last
+
+    if @previous_early_bird
+      if Time.zone.today < @previous_early_bird.date_to
+        date_from = @previous_early_bird.date_from
+        date_to = @previous_early_bird.date_to
+        flash[:message] = "The early bird is currently turned off from #{date_from} until #{date_to}."
+      end
+    end
   end
 
   def create
