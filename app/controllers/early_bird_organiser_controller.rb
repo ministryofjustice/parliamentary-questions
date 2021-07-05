46f4e4cad7d3003dd3d7b07c4a0d33c56224fba1
willmcb <william.mcbrien@digital.justice.gov.uk>
2021-07-05 11:19:09 +0100

    Update copy to match updated ticket

--- app/controllers/early_bird_organiser_controller.rb
+++ app/controllers/early_bird_organiser_controller.rb
@@ -10,7 +10,7 @@ class EarlyBirdOrganiserController < ApplicationController
       if Time.zone.today < @previous_early_bird.date_to
         date_from = @previous_early_bird.date_from
         date_to = @previous_early_bird.date_to
-        flash[:message] = "The early bird is currently turned off from #{date_from} until #{date_to}."
+        flash[:message] = "The early bird is currently turned off between #{date_from} and #{date_to}."
       end
     end
   end
@@ -21,7 +21,7 @@ class EarlyBirdOrganiserController < ApplicationController
     if @early_bird_organiser.save
       date_from = @early_bird_organiser.date_from
       date_to = @early_bird_organiser.date_to
-      flash[:success] = "You have successfully scheduled the early bird to be turned off from #{date_from} until #{date_to}"
+      flash[:success] = "You have successfully scheduled the early bird to be turned off between #{date_from} and #{date_to}"
       redirect_to admin_path
     else
       render action: 'new'
