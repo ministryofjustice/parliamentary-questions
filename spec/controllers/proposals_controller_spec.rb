2e036efdbcf01d935cc0aebbf3767fe276bfc9da
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-01-19 13:45:07 +0000

    controller test action officer and pq created

--- spec/controllers/proposals_controller_spec.rb
+++ spec/controllers/proposals_controller_spec.rb
@@ -2,12 +2,20 @@ require 'spec_helper'
 
 describe ProposalsController, type: :controller do
   describe 'Get new' do
+    let!(:pq) { FactoryBot.create(:pq) }
     it 'Assigns action officers' do
-      get :new, params: { pq_id: 1 }
-      # expect(assigns(:action_officers).map(&:name)).to eq expected_order_of_action_officers
-      expect(assigns(:action_officers)).to be_an_instance_of Array
+      action_officer = FactoryBot.create(:action_officer)
+      get :new, params: { pq_id: pq.id }
+      expect(assigns(:action_officers)).to eq([action_officer])
+    end
+
+    it "renders the new template" do
+      get :new, params: { pq_id: pq.id }
+      expect(response).to render_template("new")
     end
   end
 
+  describe 'Get propose' do
 
-end    
+  end
+end
