d05fc97c112498741bc851f4b0aa3dde068fb07a
N.Preddy <nick.preddy@digital.justice.gov.uk>
2023-07-10 09:53:45 +0100

    Fixed RSpec/ExpectActual offences.

--- spec/routes/routes_spec.rb
+++ spec/routes/routes_spec.rb
@@ -1,12 +1,16 @@
 require "spec_helper"
 
 describe "routes", type: :routing do
+  dashboard_path    = "/dashboard"
+  dummy_path        = "/gobbledygook"
+  in_progress_path  = "/dashboard/in_progress"
+
   it "routes to the expected controller action" do
-    expect(get: "/dashboard").to route_to(controller: "dashboard", action: "index")
-    expect(get: "/dashboard/in_progress").to route_to(controller: "dashboard", action: "in_progress")
+    expect(get: dashboard_path).to route_to(controller: "dashboard", action: "index")
+    expect(get: in_progress_path).to route_to(controller: "dashboard", action: "in_progress")
   end
 
   it "routes unknown routes to the page_not_found action" do
-    expect(get: "/gobbledygook").to route_to(:controller => "application", action: "page_not_found", "path" => "gobbledygook")
+    expect(get: dummy_path).to route_to(:controller => "application", action: "page_not_found", "path" => "gobbledygook")
   end
 end
