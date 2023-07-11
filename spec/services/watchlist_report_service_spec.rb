121799ecdb82f7040b8e6aaf90f83f59f5bb5979
N.Preddy <nick.preddy@digital.justice.gov.uk>
2023-07-11 11:34:59 +0100

    Fixed 1 RSpec/InstanceVariable offense.

--- spec/services/watchlist_report_service_spec.rb
+++ spec/services/watchlist_report_service_spec.rb
@@ -1,19 +1,16 @@
 require "spec_helper"
 
 describe "WatchlistReportService" do
+  let(:report_service) { WatchlistReportService.new }
   let!(:watchlist_one) { create(:watchlist_member, name: "member 1", email: "m1@ao.gov", deleted: false) }
   let!(:watchlist_two) { create(:watchlist_member, name: "member 2", email: "m2@ao.gov", deleted: false) }
   let!(:watchlist_deleted) { create(:watchlist_member, name: "member 3", email: "m3@ao.gov", deleted: true) }
   let(:testid) { "watchlist-#{Time.zone.now.utc}" }
 
-  before do
-    @report_service = WatchlistReportService.new
-  end
-
   it "has generated a valid token" do
-    @report_service.notify_watchlist
+    report_service.notify_watchlist
 
-    token = Token.find_by(entity: @report_service.entity, path: "/watchlist/dashboard")
+    token = Token.find_by(entity: report_service.entity, path: "/watchlist/dashboard")
     expect(token.token_digest).not_to be nil
 
     end_of_day = Time.current.end_of_day
@@ -27,7 +24,7 @@ describe "WatchlistReportService" do
   it "calls the mailer" do
     allow(NotifyPqMailer).to receive_message_chain(:watchlist_email, :deliver_later)
 
-    token = @report_service.notify_watchlist
+    token = report_service.notify_watchlist
 
     expect(NotifyPqMailer).to have_received(:watchlist_email).with(email: "m1@ao.gov", token:, entity: testid)
     expect(NotifyPqMailer).to have_received(:watchlist_email).with(email: "m2@ao.gov", token:, entity: testid)
