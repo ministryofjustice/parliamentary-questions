23d350f57cde40f517196a72e2eb91eedd47c536
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-07-13 14:00:15 +0100

    Always return json

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,9 +1,7 @@
 class PingController < ApplicationController
-  respond_to :json
-
   def index
     Rails.logger.silence do
-      respond_with Deployment.info
+      render json: Deployment.info
     end
   end
 end
