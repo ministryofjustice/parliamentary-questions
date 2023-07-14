3c0cd2556390ab2a6361da4e1079f79f44cfe5bb
N.Preddy <nick.preddy@digital.justice.gov.uk>
2023-07-14 15:57:59 +0100

    Disabled RSpec/StubbedMock & /AnyInstance checks.

--- spec/services/health_check_service_spec.rb
+++ spec/services/health_check_service_spec.rb
@@ -2,15 +2,15 @@ require "spec_helper"
 
 describe HealthCheckService do
   context "when the PQA API is run" do
+    # rubocop:disable RSpec/StubbedMock
+    # rubocop:disable RSpec/AnyInstance
     it "calls accessible and avaiable on all checks" do
       allow(HealthCheck::PqaApi).to receive(:time_to_run?).and_return(true)
       expect(HealthCheck::PqaApi).to receive(:time_to_run?)
-      # rubocop:disable RSpec/StubbedMock
       expect_any_instance_of(HealthCheck::Database).to receive(:available?).and_return(true)
       expect_any_instance_of(HealthCheck::PqaApi).to receive(:available?).and_return(true)
       expect_any_instance_of(HealthCheck::Database).to receive(:accessible?).and_return(true)
       expect_any_instance_of(HealthCheck::PqaApi).to receive(:accessible?).and_return(true)
-      # rubocop:enable RSpec/StubbedMock
 
       result = described_class.new.report
 
@@ -21,12 +21,10 @@ describe HealthCheckService do
     it "does not call accessible and available on PQA API check if not time to run" do
       allow(HealthCheck::PqaApi).to receive(:time_to_run?).and_return(false)
       expect(HealthCheck::PqaApi).to receive(:time_to_run?)
-      # rubocop:disable RSpec/StubbedMock
       expect_any_instance_of(HealthCheck::Database).to receive(:available?).and_return(true)
       expect_any_instance_of(HealthCheck::Database).to receive(:accessible?).and_return(true)
       expect_any_instance_of(HealthCheck::PqaApi).not_to receive(:available?)
       expect_any_instance_of(HealthCheck::PqaApi).not_to receive(:accessible?)
-      # rubocop:enable RSpec/StubbedMock
 
       result = described_class.new.report
 
@@ -37,14 +35,12 @@ describe HealthCheckService do
     it "collects error messages if any checks fail" do
       allow(HealthCheck::PqaApi).to receive(:time_to_run?).and_return(true)
       expect(HealthCheck::PqaApi).to receive(:time_to_run?)
-      # rubocop:disable RSpec/StubbedMock
       expect_any_instance_of(HealthCheck::Database).to receive(:available?).and_return(false)
       expect_any_instance_of(HealthCheck::Database).to receive(:accessible?).and_return(false)
       expect_any_instance_of(HealthCheck::Database).to receive(:error_messages).and_return(["DB message 1", "DB Message 2"])
       expect_any_instance_of(HealthCheck::PqaApi).to receive(:available?).and_return(false)
       expect_any_instance_of(HealthCheck::PqaApi).to receive(:accessible?).and_return(false)
       expect_any_instance_of(HealthCheck::PqaApi).to receive(:error_messages).and_return(["API message 1", "API Message 2"])
-      # rubocop:enable RSpec/StubbedMock
 
       result = described_class.new.report
 
@@ -56,5 +52,7 @@ describe HealthCheckService do
         "API Message 2",
       ].sort)
     end
+    # rubocop:enable RSpec/StubbedMock
+    # rubocop:enable RSpec/AnyInstance
   end
 end
