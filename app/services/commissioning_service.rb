a9570f6a097d868c6b06f738e27169c93a9285a9
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-10-17 14:39:31 +0100

    display accepted and rejected history in the relevant sections

--- app/services/commissioning_service.rb
+++ app/services/commissioning_service.rb
@@ -19,7 +19,7 @@ class CommissioningService
             action_officer_id: ao_id,
           )
         end
-      pq.action_officers_pqs << ao_pqs.last
+      pq.action_officers_pqs << ao_pqs
       pq.update_state!
 
       ao_pqs.each do |ao_pq|
