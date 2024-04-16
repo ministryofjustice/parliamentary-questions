f9ff503ea5c7683fbb9e98ff9f8027da9bf39772
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-04-16 16:06:07 +0100

    Rails 7.1 upgrade (#1022)
    * Update to Rail 7.0
    
    * rubocop fixes
    
    * Use terser
    
    * re-order
    
    * Use updated task name
    
    * Enable some rails 7.0 config
    
    * Upgrade to rails 7.1 and set defaults
    
    * Update gemfile.lock
    
    * add comma
    
    * Fix devise deprecation
    
    * autoload smoke_test
    
    * Update paper_trail
    
    * Disable setting
    
    * Update devise

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,11 @@ module ParliamentaryQuestions
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -64,6 +69,8 @@ module ParliamentaryQuestions
     config.eager_load_paths += %w[app/assets/stylesheets]
     config.eager_load_paths += %w[app/assets/stylesheets/moj]
     config.eager_load_paths += %w[app/assets/stylesheets/vendor]
+
+    config.add_autoload_paths_to_load_path = false
 
     config.generators do |g|
       g.template_engine :erb
