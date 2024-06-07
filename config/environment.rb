86988994349c2906e49f3dd481e7dcf29aa6134a
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-06-07 10:40:31 +0100

    Update sidekiq

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,9 @@ Rails.configuration.active_job.queue_adapter     = :sidekiq
 Rails.configuration.active_job.queue_name_prefix = "parliamentary_questions"
 Rails.application.config.action_mailer.deliver_later_queue_name = "mailers"
 
+# Use MailDeliveryJob
+config.action_mailer.delivery_job = "ActionMailer::MailDeliveryJob"
+
 # No need anymore as we use notification api to do it
 # smtpconfig = ActionMailer::Base.smtp_settings.reject { |x| x['password'] }
 # LogStuff.info "ActionMailer smtp_settings #{smtpconfig.inspect}"
