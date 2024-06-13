7c1de6eeedbcbecd2c30c265afe53f061a8c5fe9
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-06-13 17:04:46 +0100

    Fix action officers features

--- features/action_officer_spec.rb
+++ features/action_officer_spec.rb
@@ -28,8 +28,8 @@ describe "Managing action officers", js: true, suspend_cleaner: true do
     click_on "Save"
   end
 
-  let(:ao_email) { "action_officer@pq.com"  }
-  let(:ao_name)  { "action officer"         }
+  let(:ao_email) { "action_officer@pq.com" }
+  let(:ao_name)  { "action officer 1"      }
 
   it "Parli-branch can create a new finance officer" do
     create_ao(ao_name, ao_email)
@@ -45,19 +45,20 @@ describe "Managing action officers", js: true, suspend_cleaner: true do
     click_on ao_name
     click_on "Edit"
 
-    expect(page.title).to have_content("Edit action officer")
+    expect(page).to have_title "Edit action officer"
     fill_in "Name", with: "another action officer"
     click_on "Save"
 
-    expect(page.title).to have_content("Action officer details")
+    expect(page).to have_title "Action officer details"
     expect(page).to have_content "Action officer was successfully updated"
     expect(page).to have_content "another action officer"
   end
 
   it "Parli-branch cannot duplicate AO email for the same deputy director" do
     create_ao(ao_name, ao_email)
+    create_ao(ao_name, ao_email)
 
-    expect(page.title).to include("Action officers")
+    expect(page).to have_title "Add action officer"
     expect(page).to have_content "an action officer cannot be assigned twice to the same deputy director"
     expect(page).not_to have_content "Action officer was successfully created"
   end
