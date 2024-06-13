213f7f8e1289ebb552b1510daf315cb7e329dfca
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-06-13 17:04:52 +0100

    make headless

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -15,7 +15,7 @@ Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
 
   unless ENV["CHROME_DEBUG"]
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--start-maximized")
