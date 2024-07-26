f390cb7adb1a1f5d02d299620243681f04f786c3
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-07-26 09:59:26 +0100

    Use simple URL

--- app/controllers/early_bird_dashboard_controller.rb
+++ app/controllers/early_bird_dashboard_controller.rb
@@ -3,14 +3,14 @@ class EarlyBirdDashboardController < ApplicationController
   before_action :save_early_bird_credentials, only: [:index]
   before_action :authenticate_user!, PqUserFilter, only: [:preview]
 
-  NEW      = "New".freeze
   PER_PAGE = 200
+  QUESTIONS_URL = "https://questions-statements.parliament.uk/".freeze
 
   def index
     update_page_title("Early bird preview")
-    @now            = Time.zone.now.strftime("%d/%m/%Y")
-    @questions      = Pq.new_questions.order(:uin)
-    @parliament_url = PQA::RecentQuestionsUrl.url(Time.zone.today)
+    @now = Time.zone.now.strftime("%d/%m/%Y")
+    @questions = Pq.new_questions.order(:uin)
+    @parliament_url = QUESTIONS_URL
   end
 
   def preview
