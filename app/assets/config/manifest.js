34195e70c0aed5ce3999d26a2a5074ff854d2c2c
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-10-30 15:47:27 +0000

    Fix assets for production

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,9 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link images/favicon.ico
+//= link images/favicon.svg
+//= link images/govuk-icon-mask.svg
+//= link images/govuk-icon-180.png
+//= link images/govuk-crest.svg
+//= link images/govuk-opengraph-image.png
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,3 @@
-Rails.application.config.assets.precompile += %w[images/favicon.ico]
-Rails.application.config.assets.precompile += %w[images/favicon.svg]
-Rails.application.config.assets.precompile += %w[images/govuk-icon-mask.svg]
-Rails.application.config.assets.precompile += %w[images/govuk-icon-180.png]
-Rails.application.config.assets.precompile += %w[images/govuk-opengraph-image.png]
-Rails.application.config.assets.precompile += %w[gov.uk_logotype_crown.png]
-Rails.application.config.assets.precompile += %w[font-awesome.css]
-Rails.application.config.assets.precompile += %w[select2.css]
-Rails.application.config.assets.precompile += %w[select2-bootstrap.css]
-Rails.application.config.assets.precompile += %w[vendor/jquery.datetimepicker.css]
-Rails.application.config.assets.precompile += %w[vendor/pq-select2.css]
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
