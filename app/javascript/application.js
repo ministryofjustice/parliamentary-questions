e107edd2fdda5ff1dd09d13b4f09e2ce450051b6
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-11-01 10:26:46 +0000

    Fix ajax

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,9 @@ import "./moj/pq"
 import "./moj/stop-scrolling-at-footer"
 import "./moj/validation_event"
 import "./moj/early_bird_dashboard"
-import { initAll } from "govuk-frontend";
 
+import { initAll } from "govuk-frontend";
 initAll();
+
+import Rails from "@rails/ujs"
+Rails.start()
