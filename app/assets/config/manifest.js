3c3b6e8e55d3a08143b15fa6995d510ad4c1922b
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-11-01 12:13:38 +0000

    Add correct paths

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,8 @@
 //= link_tree ../images
 //= link_tree ../builds
 //= link_tree ../../../node_modules/govuk-frontend/dist/govuk/assets/images
-//= link jquery
-//= link select2
-//= link moment
+//= link jquery/dist/jquery.js
+//= link moment/moment.js
+//= link select2/dist/js/select2.js
 //= link select2/dist/css/select2.css
 //= link font-awesome/css/font-awesome.css
