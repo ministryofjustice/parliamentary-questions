721046092e9eee6a13f49df0c3a7dbc19c6c629e
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2025-03-17 15:27:54 +0000

    Remove firefox

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -19,23 +19,7 @@ Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
 
-Capybara.register_driver :headless_firefox do |app|
-  options = Selenium::WebDriver::Firefox::Options.new
-
-  unless ENV["FIREFOX_DEBUG"]
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--start-maximized")
-    options.add_argument("--window-size=1980,2080")
-    options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
-  end
-
-  Capybara::Selenium::Driver.new(app, browser: :firefox, options:)
-end
-
-# Chrome having issues so curently using firefox
-Capybara.javascript_driver = :headless_firefox
+Capybara.javascript_driver = :headless_chrome
 
 RSpec.configure do |config|
   # Helper modules to load
