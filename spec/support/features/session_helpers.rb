6991e00e1a350b682809f7912721d4b7643f172e
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2025-04-15 17:12:42 +0100

    Set  driver version to 133 & add some sleep states.

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -3,11 +3,13 @@ module Features
     def create_pq_session
       user = DbHelpers.users.find(&:pq_user?)
       sign_in(user.email, DbHelpers::USER_PASSWORD)
+      sleep 1
     end
 
     def create_admin_session
       user = DbHelpers.users.find(&:admin?)
       sign_in(user.email, DbHelpers::USER_PASSWORD)
+      sleep 1
     end
 
     def sign_in(email, password)
@@ -16,6 +18,7 @@ module Features
       fill_in "Email", with: email
       fill_in "Password", with: password
       click_button "Sign in"
+      sleep 1
     end
   end
 end
