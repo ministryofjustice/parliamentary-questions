c2e97619d66a351048f65c975590dd03f39e6460
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2025-04-22 15:35:58 +0100

    Update feature_helper.rb

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -4,22 +4,36 @@ require "./spec/support/features/pq_helpers"
 require "capybara/rspec"
 require "capybara/rails"
 
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
+# Capybara.register_driver :headless_chrome do |app|
+#   options = Selenium::WebDriver::Chrome::Options.new
+#
+#   unless ENV["CHROME_DEBUG"]
+#     # options.add_argument("--headless")
+#     options.add_argument("--disable-gpu")
+#     options.add_argument("--no-sandbox")
+#     options.add_argument("--start-maximized")
+#     options.add_argument("--window-size=1980,2080")
+#     options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
+#   end
+#
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+# end
 
-  unless ENV["CHROME_DEBUG"]
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--start-maximized")
-    options.add_argument("--window-size=1980,2080")
-    options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
-  end
+# Capybara.javascript_driver = :headless_chrome
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+Capybara.register_driver :selenium_firefox do |app|
+  options = Selenium::WebDriver::Firefox::Options.new
+  options.add_argument("--headless")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--start-maximized")
+  options.add_argument("--window-size=1980,2080")
+  options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
+
+  Capybara::Selenium::Driver.new(app, browser: :firefox, options:)
 end
 
-Capybara.javascript_driver = :headless_chrome
+# Capybara::Selenium::Driver.new(app, browser: :firefox, browser_version: "latest")
 
 RSpec.configure do |config|
   # Helper modules to load
